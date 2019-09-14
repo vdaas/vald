3733b9946d197c0434588a25d8c8acfc1fe1295f
kpango <i.can.feel.gravity@gmail.com>
2019-09-14 10:35:13 +0900

    fix
    Signed-off-by: kpango <i.can.feel.gravity@gmail.com>

--- hack/license/gen/main.go
+++ hack/license/gen/main.go
@@ -47,7 +47,8 @@ var (
 {{.Escape}} WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 {{.Escape}} See the License for the specific language governing permissions and
 {{.Escape}} limitations under the License.
-{{.Escape}}`))
+{{.Escape}}
+`))
 	slushEscape = "//"
 	sharpEscape = "#"
 )
