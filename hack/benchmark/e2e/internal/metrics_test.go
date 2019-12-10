a99213abe9535b0966d6eadbcb6f485ff80b27cf
Kosuke Morimoto <kou.morimoto@gmail.com>
2019-12-10 16:20:55 +0900

    fix metrics
    Signed-off-by: Kosuke Morimoto <kou.morimoto@gmail.com>

--- hack/benchmark/e2e/internal/metrics_test.go
+++ hack/benchmark/e2e/internal/metrics_test.go
@@ -13,4 +13,31 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 //
-package ngt
+package internal
+
+import "testing"
+
+func TestRecall(t *testing.T) {
+	x := []string{
+		"foo", "bar", "hoge", "huga",
+	}
+	y := []string{
+		"foo", "baz", "hoge", "huga", "moge",
+	}
+
+	tests := []struct {
+		k      int
+		recall float64
+	}{
+		{1, 1.0},
+		{2, 0.5},
+		{3, 2.0 / 3.0},
+		{4, 3.0 / 4.0},
+	}
+
+	for _, tt := range tests {
+		if Recall(x, y, tt.k) != tt.recall {
+			t.Errorf("Recall@%d is %f", tt.k, tt.recall)
+		}
+	}
+}
