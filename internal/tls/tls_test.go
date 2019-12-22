6170844683c131230091bfe489737c77d0bf5b65
kevindiu <kevin_diu@yahoo.com.hk>
2019-12-22 23:29:13 +0900

    add missing test data and add test

--- internal/tls/tls_test.go
+++ internal/tls/tls_test.go
@@ -65,7 +65,6 @@ func TestNew(t *testing.T) {
 				return nil
 			},
 		},
-
 		{
 			name: "return value CurvePreferences test.",
 			args: defaultArgs,
@@ -111,33 +110,6 @@ func TestNew(t *testing.T) {
 				return nil
 			},
 		},
-		/*
-			{
-				name: "return value Certificates test.",
-				args: defaultArgs,
-				want: func() *Config {
-					conf := defaultConfig
-					cert, _ := tls.LoadX509KeyPair(dummyCertPath, dummyKeyPath)
-					conf.Certificates = []tls.Certificate{cert}
-					return conf
-				}(),
-				checkFunc: func(got, want *tls.Config) error {
-					for _, wantVal := range want.Certificates {
-						var notExist = false
-						for _, gotVal := range got.Certificates {
-							if gotVal.PrivateKey == wantVal.PrivateKey {
-								notExist = true
-								break
-							}
-						}
-						if notExist {
-							return fmt.Errorf("Certificates PrivateKey not Matched :\twant %s", wantVal.PrivateKey)
-						}
-					}
-					return nil
-				},
-			},
-		*/
 		{
 			name: "return value ClientAuth test.",
 			args: defaultArgs,
@@ -194,6 +166,139 @@ func TestNew(t *testing.T) {
 		})
 	}
 }
+
+func TestNewClientConfig(t *testing.T) {
+	type args struct {
+		opts []Option
+	}
+	dummyCertPath := "./testdata/dummyServer.crt"
+	dummyKeyPath := "./testdata/dummyServer.key"
+	dummyCaPath := "./testdata/dummyCa.pem"
+	defaultArgs := args{
+		opts: []Option{
+			WithCert(dummyCertPath),
+			WithKey(dummyKeyPath),
+			WithCa(dummyCaPath),
+		},
+	}
+	tests := []struct {
+		name      string
+		args      args
+		checkFunc func(*Config, *Config) error
+		want      *Config
+		wantErr   error
+	}{
+		{
+			name: "return value MinVersion test.",
+			args: defaultArgs,
+			want: func() *Config {
+				conf := defaultConfig
+				cert, _ := tls.LoadX509KeyPair(dummyCertPath, dummyKeyPath)
+				conf.Certificates = []tls.Certificate{cert}
+				return conf
+			}(),
+			checkFunc: func(got, want *tls.Config) error {
+				if got.MinVersion != want.MinVersion {
+					return fmt.Errorf("MinVersion not Matched :\tgot %d\twant %d", got.MinVersion, want.MinVersion)
+				}
+				return nil
+			},
+		},
+
+		{
+			name: "return value CurvePreferences test.",
+			args: defaultArgs,
+			want: func() *Config {
+				conf := defaultConfig
+				cert, _ := tls.LoadX509KeyPair(dummyCertPath, dummyKeyPath)
+				conf.Certificates = []tls.Certificate{cert}
+				return conf
+			}(),
+			checkFunc: func(got, want *tls.Config) error {
+				if len(got.CurvePreferences) != len(want.CurvePreferences) {
+					return fmt.Errorf("CurvePreferences not Matched length:\tgot %d\twant %d", len(got.CurvePreferences), len(want.CurvePreferences))
+				}
+				for _, actualValue := range got.CurvePreferences {
+					var match bool
+					for _, expectedValue := range want.CurvePreferences {
+						if actualValue == expectedValue {
+							match = true
+							break
+						}
+					}
+
+					if !match {
+						return fmt.Errorf("CurvePreferences not Find :\twant %s", string(want.MinVersion))
+					}
+				}
+				return nil
+			},
+		},
+		{
+			name: "return value SessionTicketsDisabled test.",
+			args: defaultArgs,
+			want: func() *Config {
+				conf := defaultConfig
+				cert, _ := tls.LoadX509KeyPair(dummyCertPath, dummyKeyPath)
+				conf.Certificates = []tls.Certificate{cert}
+				return conf
+			}(),
+			checkFunc: func(got, want *tls.Config) error {
+				if got.SessionTicketsDisabled != want.SessionTicketsDisabled {
+					return fmt.Errorf("SessionTicketsDisabled not matched :\tgot %v\twant %v", got.SessionTicketsDisabled, want.SessionTicketsDisabled)
+				}
+				return nil
+			},
+		},
+		{
+			name: "return value ClientAuth test.",
+			args: defaultArgs,
+			want: func() *Config {
+				conf := defaultConfig
+				cert, _ := tls.LoadX509KeyPair(dummyCertPath, dummyKeyPath)
+				conf.Certificates = []tls.Certificate{cert}
+				return conf
+			}(),
+			checkFunc: func(got, want *tls.Config) error {
+				if got.ClientAuth != want.ClientAuth {
+					return fmt.Errorf("ClientAuth not Matched :\tgot %d \twant %d", got.ClientAuth, want.ClientAuth)
+				}
+				return nil
+			},
+		},
+		{
+			name: "cert file not found return error",
+			args: args{
+				[]Option{
+					WithCert(""),
+					WithKey(dummyKeyPath),
+					WithCa(dummyCaPath),
+				},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, gotErr := NewClientConfig(tt.args.opts...)
+			if tt.checkFunc != nil {
+				if err := tt.checkFunc(got, tt.want); err != nil {
+					t.Errorf("NewTLSConfig() error = %v", err)
+					return
+				}
+			}
+			if gotErr != nil {
+				if tt.wantErr == nil {
+					t.Errorf("NewTLSConfig() error = %v, wantErr = %v", gotErr, tt.wantErr)
+				} else if gotErr.Error() != tt.wantErr.Error() {
+					t.Errorf("NewTLSConfig() error = %v, wantErr = %v", gotErr, tt.wantErr)
+				}
+			} else if tt.wantErr != nil {
+				t.Errorf("NewTLSConfig() error = %v, wantErr = %v", gotErr, tt.wantErr)
+			}
+		})
+	}
+}
+
 func TestNewX509CertPool(t *testing.T) {
 	type args struct {
 		path string
