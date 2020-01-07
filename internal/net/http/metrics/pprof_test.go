dc567621d1254c3a400d79ad02aa36b5dbf092d9
hlts2 <hiroto.funakoshi.hiroto@gmail.com>
2020-01-07 11:23:56 +0900

    fix

--- internal/net/http/metrics/pprof_test.go
+++ internal/net/http/metrics/pprof_test.go
@@ -17,7 +17,7 @@ func TestNewPProfHandler(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			got := NewPProfHandler()
 			if (got != nil) != tt.initialized {
-				t.Error("New() is wrong.")
+				t.Error("NewPProfHandler() is wrong.")
 			}
 		})
 	}
