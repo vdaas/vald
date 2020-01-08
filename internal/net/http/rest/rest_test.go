96d0242659fc2962ee97b5231935b67df4393fbb
hlts2 <hiroto.funakoshi.hiroto@gmail.com>
2020-01-08 13:02:03 +0900

    fix

--- internal/net/http/rest/rest_test.go
+++ internal/net/http/rest/rest_test.go
@@ -1,10 +1,11 @@
 package rest
 
 import (
-	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"testing"
+
+	"github.com/vdaas/vald/internal/errors"
 )
 
 func TestHandlerToRestFunc(t *testing.T) {
@@ -28,15 +29,15 @@ func TestHandlerToRestFunc(t *testing.T) {
 				checkFunc: func(fn Func) error {
 					code, err := fn(httptest.NewRecorder(), new(http.Request))
 					if err != nil {
-						return fmt.Errorf("err is not nil. err: %v", err)
+						return errors.Errorf("err is not nil. err: %v", err)
 					}
 
 					if code != http.StatusOK {
-						return fmt.Errorf("status code is wrong. want: %v, got: %v", http.StatusOK, code)
+						return errors.Errorf("status code is wrong. want: %v, got: %v", http.StatusOK, code)
 					}
 
 					if cnt != 1 {
-						return fmt.Errorf("called count is wrong. want: %v, got: %v", 1, cnt)
+						return errors.Errorf("called count is wrong. want: %v, got: %v", 1, cnt)
 					}
 
 					return nil
