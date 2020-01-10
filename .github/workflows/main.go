6b94e96e379486362e9792d4cc11e8ae73fa0282
hlts2 <hiroto.funakoshi.hiroto@gmail.com>
2020-01-10 19:42:06 +0900

    fix

---
+++ .github/workflows/main.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	f := "aaaaaaaaaaaaaaaa"
+	f = "bbbbbbbbbbbbbbb"
+	fmt.Println(f)
+}
---
+++ .github/workflows/main_test.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestHoge(b *testing.T) {
+	f := "aaaaaaaaaaa"
+	f = "aaaaaaaaa"
+
+	fmt.Println(f)
+}
