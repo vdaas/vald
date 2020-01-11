7b097a4216b5fa27c70429de6179b032a0c4aa65
Hiroto Funakoshi <hiroto.funakoshi.hiroto@gmail.com>
2020-01-11 16:43:54 +0900

    test case for `internal/net/http/metric` (#146)
    * added test case
    
    * fix
    
    Co-authored-by: Yusuke Kato <i.can.feel.gravity@gmail.com>

---
+++ internal/net/http/metrics/pprof_test.go
@@ -0,0 +1,24 @@
+package metrics
+
+import "testing"
+
+func TestNewPProfHandler(t *testing.T) {
+	tests := []struct {
+		name        string
+		initialized bool
+	}{
+		{
+			name:        "initialize success",
+			initialized: true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := NewPProfHandler()
+			if (got != nil) != tt.initialized {
+				t.Error("NewPProfHandler() is wrong.")
+			}
+		})
+	}
+}
