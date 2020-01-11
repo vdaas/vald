e07906bf1f278e0852b2e28dba8df877a5eb9b3e
Hiroto Funakoshi <hiroto.funakoshi.hiroto@gmail.com>
2020-01-11 16:39:45 +0900

    added test case for `internal/net/http/rest` (#130)
    * added test case for rest
    
    * fix
    
    * refactor
    
    * fix
    
    Co-authored-by: Yusuke Kato <i.can.feel.gravity@gmail.com>

---
+++ internal/net/http/rest/rest_test.go
@@ -0,0 +1,57 @@
+package rest
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/vdaas/vald/internal/errors"
+)
+
+func TestHandlerToRestFunc(t *testing.T) {
+	type test struct {
+		name      string
+		hfn       http.HandlerFunc
+		checkFunc func(Func) error
+	}
+
+	tests := []test{
+		func() test {
+			cnt := 0
+
+			hfn := http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+				cnt++
+			})
+
+			return test{
+				name: "returns 200 status code",
+				hfn:  hfn,
+				checkFunc: func(fn Func) error {
+					code, err := fn(httptest.NewRecorder(), new(http.Request))
+					if err != nil {
+						return errors.Errorf("err is not nil. err: %v", err)
+					}
+
+					if code != http.StatusOK {
+						return errors.Errorf("status code is wrong. want: %v, got: %v", http.StatusOK, code)
+					}
+
+					if cnt != 1 {
+						return errors.Errorf("called count is wrong. want: %v, got: %v", 1, cnt)
+					}
+
+					return nil
+				},
+			}
+		}(),
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			fn := HandlerToRestFunc(tt.hfn)
+			if err := tt.checkFunc(fn); err != nil {
+				t.Error(err)
+			}
+		})
+	}
+}
