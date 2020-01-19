f6fd0e1d800191e61c0b2db011842f79a7a5ab04
kpango <i.can.feel.gravity@gmail.com>
2020-01-19 12:02:45 +0900

    fix
    Signed-off-by: kpango <i.can.feel.gravity@gmail.com>

--- pkg/discoverer/k8s/service/discover.go
+++ pkg/discoverer/k8s/service/discover.go
@@ -113,8 +113,8 @@ func (d *discoverer) GetServers(name, nodeName string) (srvs *payload.Info_Serve
 				(name == "" && nodeName == p.NodeName) ||
 				(metaname == name && nodeName == "") ||
 				(metaname == name && nodeName == p.NodeName) {
-				cpu := 100.0*p.CPURequest/p.CPULimit
-				mem := 100.0*p.MemRequest/p.MemLimit
+				cpu := 100.0 * p.CPURequest / p.CPULimit
+				mem := 100.0 * p.MemRequest / p.MemLimit
 				metrics, ok := d.podMetrics.Load(name)
 				if ok {
 					m := metrics.(mpod.Pod)
