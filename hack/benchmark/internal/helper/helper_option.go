e406da9b30a171db4e26003ef160e194c53cdc71
hlts2 <hiroto.funakoshi.hiroto@gmail.com>
2020-02-13 17:01:03 +0900

    Added helper option
    Signed-off-by: hlts2 <hiroto.funakoshi.hiroto@gmail.com>

--- hack/benchmark/internal/helper/helper_option.go
+++ hack/benchmark/internal/helper/helper_option.go
@@ -5,3 +5,22 @@ type HelperOption func(*helper)
 var (
 	defaultHelperOption = []HelperOption{}
 )
+
+func WithTargets(targets []string) HelperOption {
+	return func(h *helper) {
+		if len(targets) != 0 {
+		}
+	}
+}
+
+func WithParallel() HelperOption {
+	return func(h *helper) {
+		h.parallel = true
+	}
+}
+
+func WithSequential() HelperOption {
+	return func(h *helper) {
+
+	}
+}
