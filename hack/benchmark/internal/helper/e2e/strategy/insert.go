804e1012a82ec156e5e61f9637efd5a110a54e18
hlts2 <hiroto.funakoshi.hiroto@gmail.com>
2020-02-19 20:41:05 +0900

    refactoring
    Signed-off-by: hlts2 <hiroto.funakoshi.hiroto@gmail.com>

--- hack/benchmark/internal/helper/e2e/strategy/insert.go
+++ hack/benchmark/internal/helper/e2e/strategy/insert.go
@@ -23,32 +23,36 @@ func NewInsert(opts ...InsertOption) e2e.Strategy {
 	return i
 }
 
-func (isrt *insert) Run(ctx context.Context, b *testing.B, client client.Client, dataset assets.Dataset) {
+func (isrt *insert) Run(ctx context.Context, b *testing.B, c client.Client, dataset assets.Dataset) {
 	if isrt.parallel {
-		isrt.runParallel(ctx, b, client, dataset)
+		isrt.runParallel(ctx, b, c, dataset)
 		return
 	}
-	isrt.run(ctx, b, client, dataset)
+	isrt.run(ctx, b, c, dataset)
 }
 
-func (isrt *insert) run(ctx context.Context, b *testing.B, client client.Client, dataset assets.Dataset) {
+func (isrt *insert) run(ctx context.Context, b *testing.B, c client.Client, dataset assets.Dataset) {
 	b.Run("", func(b *testing.B) {
 		for i := 0; i < b.N; i++ {
-			if err := client.Insert(ctx, nil); err != nil {
-				b.Error(err)
-			}
+			isrt.do(ctx, b, c, dataset)
 		}
 	})
 }
 
-func (isrt *insert) runParallel(ctx context.Context, b *testing.B, client client.Client, dataset assets.Dataset) {
+func (isrt *insert) runParallel(ctx context.Context, b *testing.B, c client.Client, dataset assets.Dataset) {
 	b.Run("", func(b *testing.B) {
 		b.RunParallel(func(p *testing.PB) {
 			for p.Next() {
-				if err := client.Insert(ctx, nil); err != nil {
-					b.Error(err)
-				}
+				isrt.do(ctx, b, c, dataset)
 			}
 		})
 	})
+}
+
+func (isrt *insert) do(ctx context.Context, b *testing.B, c client.Client, dataset assets.Dataset) {
+	if err := c.Insert(ctx, &client.ObjectVector{
+		Vector: []float32{},
+	}); err != nil {
+		b.Error(err)
+	}
 }
