e8f6a156612ce26350a828bf9e5a734f56348d6b
hlts2 <hiroto.funakoshi.hiroto@gmail.com>
2020-02-20 14:31:40 +0900

    Added stream insert functionary
    Signed-off-by: hlts2 <hiroto.funakoshi.hiroto@gmail.com>

--- hack/benchmark/internal/helper/e2e/strategy/stream_insert.go
+++ hack/benchmark/internal/helper/e2e/strategy/stream_insert.go
@@ -2,6 +2,8 @@ package strategy
 
 import (
 	"context"
+	"io"
+	"sync/atomic"
 	"testing"
 
 	"github.com/vdaas/vald/hack/benchmark/internal/assets"
@@ -19,9 +21,37 @@ func NewStreamInsert(opts ...StreamInsertOption) e2e.Strategy {
 	return s
 }
 
-func (sisrt *streamInsert) dataProvider() func() *client.ObjectVector {
-	return nil
+func (sisrt *streamInsert) dataProvider(b *testing.B, dataset assets.Dataset) func() *client.ObjectVector {
+	ids, trains := dataset.IDs(), dataset.Train()
+
+	var cnt uint32
+
+	b.StopTimer()
+	b.ReportAllocs()
+	b.ResetTimer()
+	b.StartTimer()
+
+	return func() *client.ObjectVector {
+		n := int(atomic.AddUint32(&cnt, 1))
+		if n > b.N {
+			return nil
+		}
+
+		return &client.ObjectVector{
+			Id:     ids[n%len(ids)],
+			Vector: trains[n%len(trains)],
+		}
+	}
 }
 
-func (sisrt *streamInsert) Run(ctx context.Context, b *testing.B, client client.Client, dataset assets.Dataset) {
+func (sisrt *streamInsert) Run(ctx context.Context, b *testing.B, c client.Client, dataset assets.Dataset) {
+	b.Run("StreamInsert", func(bb *testing.B) {
+		c.StreamInsert(ctx, sisrt.dataProvider(b, dataset), func(err error) {
+			if err != nil {
+				if err != io.EOF {
+					bb.Error(err)
+				}
+			}
+		})
+	})
 }
