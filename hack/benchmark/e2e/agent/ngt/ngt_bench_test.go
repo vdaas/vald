2f074a75a936bd01b3cd58d44686dfc487f1b00a
hlts2 <hiroto.funakoshi.hiroto@gmail.com>
2020-02-21 14:21:12 +0900

    use benchmark framework in the agent ngt package

--- hack/benchmark/e2e/agent/ngt/ngt_bench_test.go
+++ hack/benchmark/e2e/agent/ngt/ngt_bench_test.go
@@ -24,13 +24,13 @@ import (
 	"io/ioutil"
 	"net/http"
 	"strings"
-	"sync"
 	"testing"
 
 	"github.com/vdaas/vald/apis/grpc/payload"
 	"github.com/vdaas/vald/hack/benchmark/e2e/internal"
 	"github.com/vdaas/vald/hack/benchmark/internal/assets"
-	"github.com/vdaas/vald/internal/log"
+	"github.com/vdaas/vald/hack/benchmark/internal/e2e"
+	"github.com/vdaas/vald/hack/benchmark/internal/e2e/strategy"
 )
 
 func BenchmarkAgentNGTRESTSequential(rb *testing.B) {
@@ -214,299 +214,46 @@ func BenchmarkAgentNGTRESTSequential(rb *testing.B) {
 	}
 }
 
-func BenchmarkAgentNGTgRPCSequential(rb *testing.B) {
-	parseArgs(rb)
-	rctx, rcancel := context.WithCancel(context.Background())
-	defer rcancel()
-	rb.ReportAllocs()
-	rb.ResetTimer()
-	for N, name := range targets {
-		address := addresses[N]
-		if address == "" {
-			address = "localhost:8082"
-		}
-
-		if name == "" {
-			continue
-		}
-		rb.Run(name, func(b *testing.B) {
-			data := assets.Data(name)(rb)
-			if data == nil {
-				b.Logf("assets %s is nil", name)
-				return
-			}
-			ids := data.IDs()
-			train := data.Train()
-			query := data.Query()
-
-			b.ReportAllocs()
-			b.ResetTimer()
-			ctx, cancel := context.WithCancel(rctx)
-			defer cancel()
-
-			if strings.Contains(address, "localhost") {
-				internal.StartAgentNGTServer(b, ctx, data)
-			}
-
-			client := internal.NewAgentClient(b, ctx, address)
-
-			i := 0
-			b.Run("Insert objects", func(bb *testing.B) {
-				for bb.N+i >= len(ids) {
-					ids = append(ids, assets.CreateSequentialIDs(len(train))...)
-				}
-
-				bb.ReportAllocs()
-				bb.ResetTimer()
-				for n := 0; n < bb.N; n++ {
-					_, err := client.Insert(ctx, &payload.Object_Vector{
-						Id:     ids[i],
-						Vector: train[i%len(train)],
-					})
-					if err != nil {
-						bb.Error(err)
-					}
-					i++
-				}
-			})
-			for ; i < len(train); i++ {
-				_, err := client.Insert(ctx, &payload.Object_Vector{
-					Id:     ids[i],
-					Vector: train[i],
-				})
-				if err != nil {
-					b.Error(err)
-				}
-			}
-
-			b.Run("CreateIndex", func(bb *testing.B) {
-				bb.ReportAllocs()
-				bb.ResetTimer()
-				_, err := client.CreateIndex(ctx, &payload.Controll_CreateIndexRequest{
-					PoolSize: 10000,
-				})
-				if err != nil {
-					if err == io.EOF {
-						return
-					}
-					bb.Error(err)
-				}
-			})
-
-			i = 0
-			b.Run("Search objects", func(bb *testing.B) {
-				bb.ReportAllocs()
-				bb.ResetTimer()
-				for n := 0; n < bb.N; n++ {
-					_, err := client.Search(ctx, &payload.Search_Request{
-						Vector: query[i%len(query)],
-						Config: searchConfig,
-					})
-					if err != nil {
-						bb.Error(err)
-					}
-					i++
-				}
-			})
-
-			i = 0
-			b.Run("Remove objects", func(bb *testing.B) {
-				bb.ReportAllocs()
-				bb.ResetTimer()
-				for n := 0; n < bb.N; n++ {
-					_, err := client.Remove(ctx, &payload.Object_ID{
-						Id: ids[i%len(ids)],
-					})
-					if err != nil {
-						bb.Error(err)
-					}
-					i++
-				}
-			})
-		})
+func BenchmarkAgentNGT_gRPC_Sequential(b *testing.B) {
+	for _, name := range targets {
+		bench := e2e.New(
+			b,
+			e2e.WithName(name),
+			e2e.WithClient(nil),
+			e2e.WithStrategy(
+				strategy.NewInsert(),
+				strategy.NewCreateIndex(
+					strategy.WithCreateIndexClient(nil),
+					strategy.WithCreateIndexPoolSize(10000),
+				),
+				strategy.NewSearch(
+					strategy.WithSearchConfig(searchConfig),
+				),
+				strategy.NewRemove(),
+			),
+		)
+		bench.Run(context.Background(), b)
 	}
 }
 
-func BenchmarkAgentNGTgRPCStream(rb *testing.B) {
-	parseArgs(rb)
-	rctx, rcancel := context.WithCancel(context.Background())
-	defer rcancel()
-	rb.ReportAllocs()
-	rb.ResetTimer()
-	for N, name := range targets {
-		address := addresses[N]
-		if address == "" {
-			address = "localhost:8082"
-		}
-
-		if name == "" {
-			continue
-		}
-		rb.Run(name, func(b *testing.B) {
-			data := assets.Data(name)(rb)
-			if data == nil {
-				b.Fatalf("assets %s is nil", name)
-			}
-			b.Logf("benchmark %s", name)
-			ids := data.IDs()
-			train := data.Train()
-			query := data.Query()
-
-			b.ReportAllocs()
-			b.ResetTimer()
-			ctx, cancel := context.WithCancel(rctx)
-			defer cancel()
-
-			if strings.Contains(address, "localhost") ||
-				strings.Contains(address, "127.0.0.1") ||
-				strings.Contains(address, "0.0.0.0") {
-				internal.StartAgentNGTServer(b, ctx, data)
-			}
-
-			client := internal.NewAgentClient(b, ctx, address)
-
-			sti, err := client.StreamInsert(ctx)
-			if err != nil {
-				b.Error(err)
-			}
-			wg := &sync.WaitGroup{}
-			i := 0
-			b.Run("Insert objects", func(bb *testing.B) {
-				for bb.N+i >= len(ids) {
-					ids = append(ids, assets.CreateSequentialIDs(len(train))...)
-				}
-
-				bb.ReportAllocs()
-				bb.ResetTimer()
-				for n := 0; n < bb.N; n++ {
-					err := sti.Send(&payload.Object_Vector{
-						Id:     ids[i],
-						Vector: train[i%len(train)],
-					})
-					if err != nil {
-						if err == io.EOF {
-							log.Error(err)
-							return
-						}
-						bb.Error(err)
-					}
-					wg.Add(1)
-					go func() {
-						_, err := sti.Recv()
-						if err != nil {
-							if err != io.EOF {
-								bb.Error(err)
-							}
-						}
-						wg.Done()
-					}()
-					i++
-				}
-			})
-			for ; i < len(train); i++ {
-				err := sti.Send(&payload.Object_Vector{
-					Id:     ids[i],
-					Vector: train[i%len(train)],
-				})
-				if err != nil {
-					if err == io.EOF {
-						log.Error(err)
-						return
-					}
-					b.Error(err)
-				}
-				wg.Add(1)
-				go func() {
-					defer wg.Done()
-					_, err := sti.Recv()
-					if err != nil {
-						if err != io.EOF {
-							b.Error(err)
-						}
-					}
-				}()
-			}
-			wg.Wait()
-			if err := sti.CloseSend(); err != nil {
-				b.Error(err)
-			}
-			b.Run("CreateIndex", func(bb *testing.B) {
-				bb.ReportAllocs()
-				bb.ResetTimer()
-				_, err := client.CreateIndex(ctx, &payload.Controll_CreateIndexRequest{
-					PoolSize: 10000,
-				})
-				if err != nil {
-					if err == io.EOF {
-						return
-					}
-					bb.Error(err)
-				}
-			})
-			st, err := client.StreamSearch(ctx)
-			if err != nil {
-				b.Error(err)
-			}
-
-			i = 0
-			b.Run("Search objects", func(bb *testing.B) {
-				bb.ReportAllocs()
-				bb.ResetTimer()
-				for n := 0; n < bb.N; n++ {
-					err := st.Send(&payload.Search_Request{
-						Vector: query[i%len(query)],
-						Config: searchConfig,
-					})
-					if err != nil {
-						if err == io.EOF {
-							return
-						}
-						bb.Error(err)
-					}
-					_, err = st.Recv()
-					if err != nil {
-						if err != io.EOF {
-							bb.Error(i, err)
-						}
-					}
-					i++
-				}
-			})
-			if err := st.CloseSend(); err != nil {
-				b.Error(err)
-			}
-
-			str, err := client.StreamRemove(ctx)
-			if err != nil {
-				b.Error(err)
-			}
-
-			i = 0
-			b.Run("Remove objects", func(bb *testing.B) {
-				bb.ReportAllocs()
-				bb.ResetTimer()
-				for n := 0; n < bb.N; n++ {
-					err := str.Send(&payload.Object_ID{
-						Id: ids[i%len(ids)],
-					})
-					if err != nil {
-						if err == io.EOF {
-							return
-						}
-						bb.Error(err)
-					}
-					_, err = str.Recv()
-					if err != nil {
-						if err != io.EOF {
-							bb.Error(err)
-						}
-					}
-					i++
-				}
-			})
-			if err := str.CloseSend(); err != nil {
-				b.Error(err)
-			}
-		})
+func BenchmarkAgentNGT_gRPC_Stream(b *testing.B) {
+	for _, name := range targets {
+		bench := e2e.New(
+			b,
+			e2e.WithName(name),
+			e2e.WithClient(nil),
+			e2e.WithStrategy(
+				strategy.NewStreamInsert(),
+				strategy.NewCreateIndex(
+					strategy.WithCreateIndexClient(nil),
+					strategy.WithCreateIndexPoolSize(10000),
+				),
+				strategy.NewStreamSearch(
+					strategy.WithStreamSearchConfig(searchConfig),
+				),
+				strategy.NewStreamRemove(),
+			),
+		)
+		bench.Run(context.Background(), b)
 	}
 }
