50e8e8138b4d8404678fa9b5b473ef1a7545eb58
hlts2 <hiroto.funakoshi.hiroto@gmail.com>
2020-02-24 14:48:41 +0900

    Added load functionary
    Signed-off-by: hlts2 <hiroto.funakoshi.hiroto@gmail.com>

--- example/client/go.mod
+++ example/client/go.mod
@@ -4,5 +4,6 @@ go 1.13
 
 require (
 	github.com/vdaas/vald/apis v0.0.0-20191116110024-e4d8aeefc026
+	gonum.org/v1/hdf5 v0.0.0-20191105085658-fe04b73f3b53
 	google.golang.org/grpc v1.25.1
 )
--- example/client/main.go
+++ example/client/main.go
@@ -8,6 +8,7 @@ import (
 
 	"github.com/vdaas/vald/apis/grpc/vald"
 
+	"gonum.org/v1/hdf5"
 	"google.golang.org/grpc"
 )
 
@@ -22,6 +23,13 @@ func init() {
 	flag.Parse()
 }
 
+func main() {
+	if err := run(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+}
+
 func run() error {
 	conn, err := grpc.DialContext(context.Background(), grpcServerAddr, grpc.WithInsecure())
 	if err != nil {
@@ -34,9 +42,52 @@ func run() error {
 	return nil
 }
 
-func main() {
-	if err := run(); err != nil {
-		fmt.Fprintln(os.Stderr, err)
-		os.Exit(1)
+func load(path string) (train, test [][]float64, err error) {
+	var f *hdf5.File
+	f, err = hdf5.OpenFile(path, hdf5.F_ACC_RDONLY)
+	if err != nil {
+		return nil, nil, err
 	}
+	defer f.Close()
+
+	readFn := func(name string) ([][]float64, error) {
+		d, err := f.OpenDataset(name)
+		if err != nil {
+			return nil, err
+		}
+		defer d.Close()
+
+		sp := d.Space()
+		defer sp.Close()
+
+		dims, _, _ := sp.SimpleExtentDims()
+		row, dim := int(dims[0]), int(dims[1])
+
+		vec := make([]float64, sp.SimpleExtentNPoints())
+		if err := d.Read(&vec); err != nil {
+			return nil, err
+		}
+
+		vecs := make([][]float64, row)
+		for i := 0; i < row; i++ {
+			vecs[i] = make([]float64, dim)
+			for j := 0; j < dim; j++ {
+				vecs[i][j] = vec[i*dim+j]
+			}
+		}
+
+		return vecs, nil
+	}
+
+	train, err = readFn("train")
+	if err != nil {
+		return nil, nil, err
+	}
+
+	test, err = readFn("train")
+	if err != nil {
+		return nil, nil, err
+	}
+
+	return
 }
