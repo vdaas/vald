18051e5ccf634fe411e3a33e460eeacd102ba7d5
hlts2 <hiroto.funakoshi.hiroto@gmail.com>
2020-02-27 14:14:11 +0900

    Added ngtd client
    Signed-off-by: hlts2 <hiroto.funakoshi.hiroto@gmail.com>

--- hack/benchmark/e2e/gateway/vald/vald_bench_test.go
+++ hack/benchmark/e2e/gateway/vald/vald_bench_test.go
@@ -18,15 +18,12 @@ package vald
 import (
 	"context"
 	"flag"
-	"io"
 	"strings"
 	"sync"
 	"testing"
 	"time"
 
 	"github.com/vdaas/vald/apis/grpc/payload"
-	"github.com/vdaas/vald/hack/benchmark/e2e/internal"
-	"github.com/vdaas/vald/hack/benchmark/internal/assets"
 	"github.com/vdaas/vald/hack/benchmark/internal/e2e"
 	"github.com/vdaas/vald/hack/benchmark/internal/e2e/strategy"
 	"github.com/vdaas/vald/internal/log"
@@ -73,6 +70,8 @@ func BenchmarkValdGateway_Sequential(b *testing.B) {
 		bench := e2e.New(
 			b,
 			e2e.WithName(name),
+			// TODO: input vald client.
+			e2e.WithClient(nil),
 			e2e.WithStrategy(
 				strategy.NewInsert(),
 				strategy.NewSearch(
@@ -90,6 +89,8 @@ func BenchmarkValdGateway_Stream(b *testing.B) {
 		bench := e2e.New(
 			b,
 			e2e.WithName(name),
+			// TODO: input vald client.
+			e2e.WithClient(nil),
 			e2e.WithStrategy(
 				strategy.NewStreamInsert(),
 				strategy.NewStreamSearch(
@@ -99,266 +100,5 @@ func BenchmarkValdGateway_Stream(b *testing.B) {
 			),
 		)
 		bench.Run(context.Background(), b)
-	}
-}
-
-func aBenchmarkValdGatewaySequential(rb *testing.B) {
-	parseArgs(rb)
-	rctx, rcancel := context.WithCancel(context.Background())
-	defer rcancel()
-	rb.ReportAllocs()
-	rb.ResetTimer()
-	for N, name := range targets {
-		if name == "" {
-			continue
-		}
-		rb.Run(name, func(b *testing.B) {
-			data := assets.Data(name)(rb)
-			if data == nil {
-				b.Logf("assets %s is nil", name)
-				return
-			}
-			ids := data.IDs()
-			train := data.Train()
-			query := data.Query()
-
-			b.ReportAllocs()
-			b.ResetTimer()
-			ctx, cancel := context.WithCancel(rctx)
-			defer cancel()
-
-			client := internal.NewValdClient(b, ctx, addresses[N])
-
-			i := 0
-			b.Run("Insert objects", func(bb *testing.B) {
-				bb.ReportAllocs()
-				bb.ResetTimer()
-				for n := 0; n < bb.N; n++ {
-					_, err := client.Insert(ctx, &payload.Object_Vector{
-						Id:     ids[i],
-						Vector: train[i],
-					})
-					if err != nil {
-						bb.Error(err)
-					}
-					i++
-				}
-			})
-			for ; i < len(train); i++ {
-				_, err := client.Insert(ctx, &payload.Object_Vector{
-					Id:     ids[i],
-					Vector: train[i],
-				})
-				if err != nil {
-					b.Error(err)
-				}
-			}
-
-			time.Sleep(wait)
-
-			i = 0
-			b.Run("Search objects", func(bb *testing.B) {
-				bb.ReportAllocs()
-				bb.ResetTimer()
-				for n := 0; n < bb.N; n++ {
-					_, err := client.Search(ctx, &payload.Search_Request{
-						Vector: query[i],
-						Config: searchConfig,
-					})
-					if err != nil {
-						bb.Error(err)
-					}
-					i++
-				}
-			})
-
-			i = 0
-			b.Run("Remove objects", func(bb *testing.B) {
-				bb.ReportAllocs()
-				bb.ResetTimer()
-				for n := 0; n < bb.N; n++ {
-					_, err := client.Remove(ctx, &payload.Object_ID{
-						Id: ids[i],
-					})
-					if err != nil {
-						bb.Error(err)
-					}
-					i++
-				}
-			})
-		})
-	}
-}
-
-func aBenchmarkValdGatewayStream(rb *testing.B) {
-	parseArgs(rb)
-	rctx, rcancel := context.WithCancel(context.Background())
-	defer rcancel()
-	rb.ReportAllocs()
-	rb.ResetTimer()
-	for N, name := range targets {
-		if name == "" {
-			continue
-		}
-		rb.Run(name, func(b *testing.B) {
-			data := assets.Data(name)(rb)
-			if data == nil {
-				b.Fatalf("assets %s is nil", name)
-			}
-			b.Logf("benchmark %s", name)
-			ids := data.IDs()
-			train := data.Train()
-			query := data.Query()
-
-			b.ReportAllocs()
-			b.ResetTimer()
-			ctx, cancel := context.WithCancel(rctx)
-			defer cancel()
-
-			client := internal.NewAgentClient(b, ctx, addresses[N])
-
-			sti, err := client.StreamInsert(ctx)
-			if err != nil {
-				b.Error(err)
-			}
-			wg := &sync.WaitGroup{}
-			i := 0
-			b.Run("Insert objects", func(bb *testing.B) {
-				if bb.N+i >= len(ids) {
-					ids = append(ids, assets.CreateRandomIDs(len(train))...)
-				}
-
-				bb.ReportAllocs()
-				bb.ResetTimer()
-				for n := 0; n < bb.N; n++ {
-					err := sti.Send(&payload.Object_Vector{
-						Id:     ids[i],
-						Vector: train[i%len(train)],
-					})
-					if err != nil {
-						if err == io.EOF {
-							log.Error(err)
-							return
-						}
-						bb.Error(err)
-					}
-					wg.Add(1)
-					go func() {
-						_, err := sti.Recv()
-						if err != nil {
-							if err != io.EOF {
-								bb.Error(err)
-							}
-						}
-						wg.Done()
-					}()
-					i++
-				}
-			})
-			for ; i < len(train); i++ {
-				err := sti.Send(&payload.Object_Vector{
-					Id:     ids[i],
-					Vector: train[i%len(train)],
-				})
-				if err != nil {
-					if err == io.EOF {
-						log.Error(err)
-						return
-					}
-					b.Error(err)
-				}
-				wg.Add(1)
-				go func() {
-					defer wg.Done()
-					_, err := sti.Recv()
-					if err != nil {
-						if err != io.EOF {
-							b.Error(err)
-						}
-					}
-				}()
-			}
-			wg.Wait()
-			if err := sti.CloseSend(); err != nil {
-				b.Error(err)
-			}
-			b.Run("CreateIndex", func(bb *testing.B) {
-				bb.ReportAllocs()
-				bb.ResetTimer()
-				_, err := client.CreateIndex(ctx, &payload.Controll_CreateIndexRequest{
-					PoolSize: 10000,
-				})
-				if err != nil {
-					if err == io.EOF {
-						return
-					}
-					bb.Error(err)
-				}
-			})
-			st, err := client.StreamSearch(ctx)
-			if err != nil {
-				b.Error(err)
-			}
-
-			i = 0
-			b.Run("Search objects", func(bb *testing.B) {
-				bb.ReportAllocs()
-				bb.ResetTimer()
-				for n := 0; n < bb.N; n++ {
-					err := st.Send(&payload.Search_Request{
-						Vector: query[i%len(query)],
-						Config: searchConfig,
-					})
-					if err != nil {
-						if err == io.EOF {
-							return
-						}
-						bb.Error(err)
-					}
-					_, err = st.Recv()
-					if err != nil {
-						if err != io.EOF {
-							bb.Error(i, err)
-						}
-					}
-					i++
-				}
-			})
-			if err := st.CloseSend(); err != nil {
-				b.Error(err)
-			}
-
-			str, err := client.StreamRemove(ctx)
-			if err != nil {
-				b.Error(err)
-			}
-
-			i = 0
-			b.Run("Remove objects", func(bb *testing.B) {
-				bb.ReportAllocs()
-				bb.ResetTimer()
-				for n := 0; n < bb.N; n++ {
-					err := str.Send(&payload.Object_ID{
-						Id: ids[i%len(ids)],
-					})
-					if err != nil {
-						if err == io.EOF {
-							return
-						}
-						bb.Error(err)
-					}
-					_, err = str.Recv()
-					if err != nil {
-						if err != io.EOF {
-							bb.Error(err)
-						}
-					}
-					i++
-				}
-			})
-			if err := str.CloseSend(); err != nil {
-				b.Error(err)
-			}
-		})
 	}
 }
