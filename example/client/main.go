f270ce335b2e28fa4906ca1d4c3c577c5897f515
hlts2 <hiroto.funakoshi.hiroto@gmail.com>
2020-03-02 15:20:17 +0900

    Added comment
    Signed-off-by: hlts2 <hiroto.funakoshi.hiroto@gmail.com>

--- example/client/main.go
+++ example/client/main.go
@@ -9,7 +9,6 @@ import (
 	"time"
 
 	"github.com/kpango/fuid"
-	"github.com/kpango/glg"
 	"github.com/vdaas/vald-client-go/gateway/vald"
 	"github.com/vdaas/vald-client-go/payload"
 
@@ -36,7 +35,11 @@ var searchConfig = payload.Search_Config{
 
 func init() {
 	/**
-
+	Registers path, addr and wait option.
+	Path option specifies hdf file by path. By default, `fashion-mnist-784-euclidean.hdf5` is registered.
+	Addr option specifies grpc server address. By default, `127.0.0.1:8080` is registered.
+	Wait option specifies indexing wait time. When using vald, indexing starts automatically after insert. Therefore, to wait until indexing is completed before searching.
+	By default `60` seconds is registered.
 	**/
 	flag.StringVar(&datasetPath, "path", "fashion-mnist-784-euclidean.hdf5", "set dataset path")
 	flag.StringVar(&grpcServerAddr, "addr", "127.0.0.1:8081", "set gRPC server address")
@@ -46,7 +49,8 @@ func init() {
 
 func main() {
 	/**
-
+	Gets training and test data, and id based on the dataset path.
+	Ids and train lengths are equal.
 	**/
 	ids, train, test, err := load(datasetPath)
 	if err != nil {
@@ -56,8 +60,8 @@ func main() {
 	ctx := context.Background()
 
 	/**
-
-
+	Creates a client connection to the given the target.
+	And create a vad client based on this connection.
 	**/
 	conn, err := grpc.DialContext(ctx, grpcServerAddr, grpc.WithInsecure())
 	if err != nil {
@@ -66,12 +70,9 @@ func main() {
 	client := vald.NewValdClient(conn)
 
 	/**
-
-
+	Starts inserting vectors specified by insertCount(400).
 	**/
 	for i := range ids[:insertCont] {
-		glg.Infof("\r")
-
 		_, err := client.Insert(ctx, &payload.Object_Vector{
 			Id:     ids[i],
 			Vector: train[i],
@@ -86,7 +87,8 @@ func main() {
 	time.Sleep(time.Duration(indexingWaitSeconds) * time.Second)
 
 	/**
-
+	Gets an approximate vector based on the value of `SearchConfig` from the indexed tree based on the test data.
+	In this example, gets 10 approximate vectors.
 	**/
 	for _, vec := range test[:testCount] {
 		res, err := client.Search(ctx, &payload.Search_Request{
@@ -102,6 +104,7 @@ func main() {
 	}
 }
 
+// load function loads train and test vector from hdf file. Ids are the same number as the train data, and a random id is generated.
 func load(path string) (ids []string, train, test [][]float32, err error) {
 	var f *hdf5.File
 	f, err = hdf5.OpenFile(path, hdf5.F_ACC_RDONLY)
@@ -110,6 +113,7 @@ func load(path string) (ids []string, train, test [][]float32, err error) {
 	}
 	defer f.Close()
 
+	// readFn function reads vectors of the hierarchy with the given the name.
 	readFn := func(name string) ([][]float32, error) {
 		d, err := f.OpenDataset(name)
 		if err != nil {
@@ -139,16 +143,19 @@ func load(path string) (ids []string, train, test [][]float32, err error) {
 		return vecs, nil
 	}
 
+	// Get vector of train hierarchy.
 	train, err = readFn("train")
 	if err != nil {
 		return nil, nil, nil, err
 	}
 
+	// Get vector of test hierarchy.
 	test, err = readFn("test")
 	if err != nil {
 		return nil, nil, nil, err
 	}
 
+	// Generate as many random ids as train vectors.
 	ids = make([]string, 0, len(train))
 	for i := 0; i < len(train); i++ {
 		ids = append(ids, fuid.String())
