55f7c25181253fe40b2491c0e1f9cf3ae21d9c15
Rintaro Okamura <rintaro.okamura@gmail.com>
2020-03-16 16:14:25 +0900

    Update internal/client/agent/ngt/rest/client.go
    Co-Authored-By: Yusuke Kato <i.can.feel.gravity@gmail.com>

--- internal/client/agent/ngt/rest/client.go
+++ internal/client/agent/ngt/rest/client.go
@@ -13,6 +13,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 //
+
+// Package rest provides agent ngt REST client functions
 package rest
 
 import (
