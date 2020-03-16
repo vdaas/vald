e0d3594fabf45c6a395d17ed180087512d5180b8
Rintaro Okamura <rintaro.okamura@gmail.com>
2020-03-16 16:09:41 +0900

    Update internal/client/gateway/vald/rest/client.go
    Co-Authored-By: Yusuke Kato <i.can.feel.gravity@gmail.com>

--- internal/client/gateway/vald/rest/client.go
+++ internal/client/gateway/vald/rest/client.go
@@ -13,6 +13,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 //
+
+// Package rest provides vald REST client functions
 package rest
 
 import (
