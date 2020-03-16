f6139b49bd83794330f4d37855c40bc23038d5ca
Rintaro Okamura <rintaro.okamura@gmail.com>
2020-03-16 16:09:32 +0900

    Update internal/client/gateway/vald/grpc/option.go
    Co-Authored-By: Yusuke Kato <i.can.feel.gravity@gmail.com>

--- internal/client/gateway/vald/grpc/option.go
+++ internal/client/gateway/vald/grpc/option.go
@@ -13,6 +13,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 //
+
+// Package grpc provides vald gRPC client functions
 package grpc
 
 import "github.com/vdaas/vald/internal/config"
