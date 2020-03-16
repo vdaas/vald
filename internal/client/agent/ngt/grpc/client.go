642b4e9bf66a62add620c63c103c31746e5dc5dd
Rintaro Okamura <rintaro.okamura@gmail.com>
2020-03-16 16:13:40 +0900

    Update internal/client/agent/ngt/grpc/client.go
    Co-Authored-By: Yusuke Kato <i.can.feel.gravity@gmail.com>

--- internal/client/agent/ngt/grpc/client.go
+++ internal/client/agent/ngt/grpc/client.go
@@ -13,6 +13,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 //
+
+// Package grpc provides agent ngt gRPC client functions
 package grpc
 
 import (
