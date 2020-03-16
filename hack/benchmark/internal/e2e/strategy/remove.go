c416a38a4731ebb319f58bf81a3525b589fa2828
Rintaro Okamura <rintaro.okamura@gmail.com>
2020-03-16 16:05:54 +0900

    Update hack/benchmark/internal/e2e/strategy/remove.go
    Co-Authored-By: Yusuke Kato <i.can.feel.gravity@gmail.com>

--- hack/benchmark/internal/e2e/strategy/remove.go
+++ hack/benchmark/internal/e2e/strategy/remove.go
@@ -13,6 +13,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 //
+
+// Package strategy provides strategy for e2e testing functions
 package strategy
 
 import (
