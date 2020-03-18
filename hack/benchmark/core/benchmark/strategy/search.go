c66f6553f6f48d446a0e6dc92ca7eebafb179de0
hlts2 <hiroto.funakoshi.hiroto@gmail.com>
2020-03-18 16:59:02 +0900

    Added search and saveindex strategy
    Signed-off-by: hlts2 <hiroto.funakoshi.hiroto@gmail.com>

--- hack/benchmark/core/benchmark/strategy/search.go
+++ hack/benchmark/core/benchmark/strategy/search.go
@@ -9,7 +9,12 @@ import (
 	"github.com/vdaas/vald/internal/core/ngt"
 )
 
-type search struct{}
+type search struct {
+	size     int
+	epsilon  float32
+	radius   float32
+	preStart PreStart
+}
 
 func NewSearch(opts ...SearchOption) benchmark.Strategy {
 	s := new(search)
@@ -20,9 +25,21 @@ func NewSearch(opts ...SearchOption) benchmark.Strategy {
 }
 
 func (s *search) Run(ctx context.Context, b *testing.B, ngt ngt.NGT, dataset assets.Dataset) {
+	cnt := 0
 	b.Run("Search", func(bb *testing.B) {
-		for i := 0; i < bb.N; i++ {
+		query := dataset.Query()
 
+		bb.StopTimer()
+		bb.ReportAllocs()
+		bb.ResetTimer()
+		bb.StartTimer()
+		for i := 0; i < bb.N; i++ {
+			_, err := ngt.Search(query[cnt%len(query)], s.size, s.epsilon, s.radius)
+			if err != nil {
+				bb.Error(err)
+			}
+			cnt++
 		}
+		bb.StopTimer()
 	})
 }
