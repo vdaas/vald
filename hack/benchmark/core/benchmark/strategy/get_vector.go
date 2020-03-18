b8f0d43c0f4f1ac0bc5bd63e08ffb6ede2658343
hlts2 <hiroto.funakoshi.hiroto@gmail.com>
2020-03-18 15:53:13 +0900

    Added get vector strategy
    Signed-off-by: hlts2 <hiroto.funakoshi.hiroto@gmail.com>

--- hack/benchmark/core/benchmark/strategy/get_vector.go
+++ hack/benchmark/core/benchmark/strategy/get_vector.go
@@ -9,7 +9,10 @@ import (
 	"github.com/vdaas/vald/internal/core/ngt"
 )
 
-type getVector struct{}
+type getVector struct {
+	ids      []uint
+	preStart PreStart
+}
 
 func NewGetVector(opts ...GetVectorOption) benchmark.Strategy {
 	g := new(getVector)
@@ -20,9 +23,21 @@ func NewGetVector(opts ...GetVectorOption) benchmark.Strategy {
 }
 
 func (g *getVector) Run(ctx context.Context, b *testing.B, ngt ngt.NGT, dataset assets.Dataset) {
+	cnt := 0
 	b.Run("GetVector", func(bb *testing.B) {
-		for i := 0; i < bb.N; i++ {
+		g.ids = append(g.ids, g.preStart(ctx, b, ngt, dataset)...)
 
+		bb.StopTimer()
+		bb.ReportAllocs()
+		bb.ResetTimer()
+		bb.StartTimer()
+		for i := 0; i < bb.N; i++ {
+			_, err := ngt.GetVector(g.ids[cnt%len(g.ids)])
+			if err != nil {
+				bb.Error(err)
+			}
+			cnt++
 		}
+		bb.StopTimer()
 	})
 }
--- hack/benchmark/core/benchmark/strategy/get_vector_option.go
+++ hack/benchmark/core/benchmark/strategy/get_vector_option.go
@@ -1,7 +1,33 @@
 package strategy
 
+import (
+	"context"
+	"testing"
+
+	"github.com/vdaas/vald/hack/benchmark/internal/assets"
+	"github.com/vdaas/vald/internal/core/ngt"
+)
+
 type GetVectorOption func(*getVector)
 
 var (
-	defaultGetVectorOptions = []GetVectorOption{}
+	defaultGetVectorOptions = []GetVectorOption{
+		WithGetVectorPreStart(
+			func(ctx context.Context, b *testing.B, ngt ngt.NGT, dataset assets.Dataset) (ids []uint) {
+				ids = (new(preStart)).Func(ctx, b, ngt, dataset)
+				if err := ngt.CreateIndex(10000); err != nil {
+					b.Error(err)
+				}
+				return ids
+			},
+		),
+	}
 )
+
+func WithGetVectorPreStart(fn PreStart) GetVectorOption {
+	return func(g *getVector) {
+		if fn != nil {
+			g.preStart = fn
+		}
+	}
+}
