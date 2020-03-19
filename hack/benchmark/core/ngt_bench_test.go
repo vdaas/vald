77390113cb477e3cb67dcdcb751133d563f4d115
hlts2 <hiroto.funakoshi.hiroto@gmail.com>
2020-03-19 10:36:58 +0900

    Added use core benchmark frame
    Signed-off-by: hlts2 <hiroto.funakoshi.hiroto@gmail.com>

--- hack/benchmark/core/ngt_bench_test.go
+++ hack/benchmark/core/ngt_bench_test.go
@@ -1,5 +1,40 @@
 package ngt
 
-import "testing"
+import (
+	context "context"
+	"flag"
+	"strings"
+	"testing"
 
-func BenchmarkNGT(b *testing.B) {}
+	"github.com/vdaas/vald/hack/benchmark/core/benchmark"
+	"github.com/vdaas/vald/hack/benchmark/core/benchmark/strategy"
+	"github.com/vdaas/vald/internal/log"
+)
+
+var (
+	targets []string
+)
+
+func init() {
+	testing.Init()
+	log.Init()
+
+	var dataset string
+	flag.StringVar(&dataset, "dataset", "", "available dataset(choice with comma)")
+	flag.Parse()
+
+	targets = strings.Split(strings.TrimSpace(dataset), ",")
+}
+
+func BenchmarkNGT(b *testing.B) {
+	for _, target := range targets {
+		benchmark.New(
+			b,
+			benchmark.WithName(target),
+			benchmark.WithNGT(nil),
+			benchmark.WithStrategy(
+				strategy.NewInsert(),
+			),
+		).Run(context.Background(), b)
+	}
+}
