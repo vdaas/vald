0b1aa354179ce95ec4270981b6f1ada4d996d824
hlts2 <hiroto.funakoshi.hiroto@gmail.com>
2020-03-19 14:07:12 +0900

    fixed performance
    Signed-off-by: hlts2 <hiroto.funakoshi.hiroto@gmail.com>

--- hack/benchmark/internal/core/gongt/gongt.go
+++ hack/benchmark/internal/core/gongt/gongt.go
@@ -1,127 +1,78 @@
 package gongt
 
 import (
-	"github.com/vdaas/vald/internal/core/ngt"
-	"github.com/vdaas/vald/internal/errors"
+	"errors"
+	"io/ioutil"
+	"os"
 
 	"github.com/yahoojapan/gongt"
 )
 
-// NGT wrapps gongt.NGT object.
-type NGT struct {
-	indexPath string
-	*gongt.NGT
+type (
+	SearchResult = gongt.SearchResult
+	ObjectType   = gongt.ObjectType
+)
+
+const (
+	ObjectNone ObjectType = iota
+	Uint8
+	Float
+)
+
+type NGT interface {
+	Search(vec []float64, size int, epsilon float64) ([]SearchResult, error)
+	Insert(vec []float64) (int, error)
+	InsertCommit(vec []float64, poolSize int) (int, error)
+	BulkInsert(vecs [][]float64) ([]int, []error)
+	BulkInsertCommit(vecs [][]float64, poolSize int) ([]int, []error)
+	CreateAndSaveIndex(poolSize int) error
+	CreateIndex(poolSize int) error
+	SaveIndex() error
+	Remove(id int) error
+	BulkRemove(ids ...uint) error
+	GetVector(id int) ([]float64, error)
+	Close()
 }
 
 var (
-
-	// ErrNotSupportedMethod is not supported error.
 	ErrNotSupportedMethod = errors.New("not supported method")
 )
 
-// New returns ngt.NGT implementation.
-func New(opts ...Option) ngt.NGT {
-	n := new(NGT)
+type ngt struct {
+	indexPath  string
+	tmpdir     string
+	objectType ObjectType
+	dimension  int
+	*gongt.NGT
+}
+
+func New(opts ...Option) (NGT, error) {
+	n := new(ngt)
 	for _, opt := range append(defaultOptions, opts...) {
 		opt(n)
 	}
-	n.NGT = gongt.New(n.indexPath)
-	return n
+
+	tmpdir, err := ioutil.TempDir("", n.indexPath)
+	if err != nil {
+		return nil, err
+	}
+	n.tmpdir = tmpdir
+
+	n.NGT = gongt.New(tmpdir).
+		SetObjectType(n.objectType).
+		SetDimension(n.dimension).
+		Open()
+
+	return n, nil
 }
 
-func (n *NGT) Search(vec []float32, size int, epsilon, radius float32) ([]ngt.SearchResult, error) {
-	results, err := n.NGT.Search(tofloat64(vec), size, float64(epsilon))
-	return toResults(results), err
-}
-
-func (n *NGT) Insert(vec []float32) (uint, error) {
-	id, err := n.NGT.Insert(tofloat64(vec))
-	return uint(id), err
-}
-
-func (n *NGT) InsertCommit(vec []float32, poolSize uint32) (uint, error) {
-	id, err := n.NGT.InsertCommit(tofloat64(vec), int(poolSize))
-	return uint(id), err
-}
-
-func (n *NGT) BulkInsert(vecs [][]float32) ([]uint, []error) {
-	ids, err := n.NGT.BulkInsert(tofloats64(vecs))
-	return touint(ids), err
-}
-
-func (n *NGT) BulkInsertCommit(vecs [][]float32, poolSize uint32) ([]uint, []error) {
-	ids, errs := n.NGT.BulkInsertCommit(tofloats64(vecs), int(poolSize))
-	return touint(ids), errs
-}
-
-func (n *NGT) CreateAndSaveIndex(poolSize uint32) error {
-	return n.NGT.CreateAndSaveIndex(int(poolSize))
-}
-
-func (n *NGT) CreateIndex(poolSize uint32) error {
-	return n.NGT.CreateIndex(int(poolSize))
-}
-
-func (n *NGT) Remove(id uint) error {
-	return n.NGT.Remove(int(id))
-}
-
-func (n *NGT) BulkRemove(ids ...uint) error {
+func (n *ngt) BulkRemove(ids ...uint) error {
 	return ErrNotSupportedMethod
 }
 
-func (n *NGT) GetVector(id uint) ([]float32, error) {
-	vecs, err := n.NGT.GetVector(int(id))
-	return tofloat32(vecs), err
-}
-
-func toResults(in []gongt.SearchResult) (out []ngt.SearchResult) {
-	out = make([]ngt.SearchResult, len(in))
-	for i := range in {
-		out[i] = ngt.SearchResult{
-			ID:       uint32(in[i].ID),
-			Distance: float32(in[i].Distance),
-		}
+func (n *ngt) Close() {
+	if len(n.indexPath) != 0 {
+		os.RemoveAll(n.tmpdir)
 	}
-	return
-}
-
-func tofloats64(in [][]float32) (out [][]float64) {
-	out = make([][]float64, len(in))
-	for i := range in {
-		out[i] = tofloat64(in[i])
-	}
-	return
-}
-
-func tofloat64(in []float32) (out []float64) {
-	out = make([]float64, len(in))
-	for i := range in {
-		out[i] = float64(in[i])
-	}
-	return
-}
-
-func tofloats32(in [][]float64) (out [][]float32) {
-	out = make([][]float32, len(in))
-	for i := range in {
-		out[i] = tofloat32(in[i])
-	}
-	return
-}
-
-func tofloat32(in []float64) (out []float32) {
-	out = make([]float32, len(in))
-	for i := range in {
-		out[i] = float32(in[i])
-	}
-	return
-}
-
-func touint(in []int) (out []uint) {
-	out = make([]uint, len(in))
-	for i := range in {
-		out[i] = uint(in[i])
-	}
-	return
+	n.NGT.Close()
 }
---
+++ hack/benchmark/internal/core/ngt/option.go
@@ -0,0 +1,34 @@
+package ngt
+
+type Option func(*core)
+
+var (
+	defaultOptions = []Option{}
+)
+
+func WithIndexPath(path string) Option {
+	return func(c *core) {
+		if len(path) != 0 {
+			c.idxPath = path
+		}
+	}
+}
+
+func WithObjectType(typ ObjectType) Option {
+	return func(c *core) {
+		switch typ {
+		case Uint8, Float:
+			c.objectType = typ
+		default:
+			c.objectType = ObjectNone
+		}
+	}
+}
+
+func WithDimension(dimension int) Option {
+	return func(c *core) {
+		if dimension > 0 {
+			c.dimension = dimension
+		}
+	}
+}
