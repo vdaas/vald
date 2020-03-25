7172a18e0e0d238d04629c1bbc2d17bb29484ee3
hlts2 <hiroto.funakoshi.hiroto@gmail.com>
2020-03-25 10:52:38 +0900

    Added  strategy
    Signed-off-by: hlts2 <hiroto.funakoshi.hiroto@gmail.com>

---
+++ hack/benchmark/core/benchmark/strategy/insert_commit.go
@@ -0,0 +1,70 @@
+package strategy
+
+import (
+	"context"
+	"testing"
+
+	"github.com/vdaas/vald/hack/benchmark/core/benchmark"
+	"github.com/vdaas/vald/hack/benchmark/internal/assets"
+	"github.com/vdaas/vald/hack/benchmark/internal/core"
+)
+
+type insertCommit struct {
+	poolSize uint32
+}
+
+func NewInsertCommit(opts ...InsertCommitOption) benchmark.Strategy {
+	isrt := new(insertCommit)
+	for _, opt := range append(defaultInsertCommitOptions, opts...) {
+		opt(isrt)
+	}
+	return isrt
+}
+
+func (isrt *insertCommit) Run(ctx context.Context, b *testing.B, c interface{}, typ benchmark.Type, dataset assets.Dataset) {
+	cnt := 0
+	b.Run("InsertCommit", func(bb *testing.B) {
+		switch typ {
+		case benchmark.Float32:
+			isrt.float32(ctx, bb, c.(core.Core32), dataset, &cnt)
+		case benchmark.Float64:
+			isrt.float64(ctx, bb, c.(core.Core64), dataset, &cnt)
+		default:
+			bb.Fatal("invalid data type")
+		}
+	})
+}
+
+func (isrt *insertCommit) float32(ctx context.Context, b *testing.B, core core.Core32, dataset assets.Dataset, cnt *int) {
+	train := dataset.Train()
+
+	b.StopTimer()
+	b.ReportAllocs()
+	b.ResetTimer()
+	b.StartTimer()
+	for i := 0; i < b.N; i++ {
+		_, err := core.InsertCommit(train[*cnt%len(train)], isrt.poolSize)
+		if err != nil {
+			b.Error(err)
+		}
+		*cnt++
+	}
+	b.StopTimer()
+}
+
+func (isrt *insertCommit) float64(ctx context.Context, b *testing.B, core core.Core64, dataset assets.Dataset, cnt *int) {
+	train := dataset.TrainAsFloat64()
+
+	b.StopTimer()
+	b.ReportAllocs()
+	b.ResetTimer()
+	b.StartTimer()
+	for i := 0; i < b.N; i++ {
+		_, err := core.InsertCommit(train[*cnt%len(train)], isrt.poolSize)
+		if err != nil {
+			b.Error(err)
+		}
+		*cnt++
+	}
+	b.StopTimer()
+}
