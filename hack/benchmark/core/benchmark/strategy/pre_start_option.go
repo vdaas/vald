c273e9d3940250db1eb5719b292c0313ba64f1a9
hlts2 <hiroto.funakoshi.hiroto@gmail.com>
2020-03-25 14:00:44 +0900

    deleted unnecessary file
    Signed-off-by: hlts2 <hiroto.funakoshi.hiroto@gmail.com>

--- hack/benchmark/core/benchmark/strategy/pre_start_option.go
+++
@@ -1,9 +0,0 @@
-package strategy
-
-type defaultPreStartOption func(*defaultPreStart)
-
-func withDefaultPreStartCreateIndex(flag bool) defaultPreStartOption {
-	return func(d *defaultPreStart) {
-		d.createIndex = flag
-	}
-}
