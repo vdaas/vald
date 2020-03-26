7a4a81030277709dcadfaf8f4f10dcf12c5003be
hlts2 <hiroto.funakoshi.hiroto@gmail.com>
2020-03-26 17:43:41 +0900

    refactor
    Signed-off-by: hlts2 <hiroto.funakoshi.hiroto@gmail.com>

--- hack/benchmark/core/benchmark/strategy/util.go
+++ hack/benchmark/core/benchmark/strategy/util.go
@@ -1,6 +1,12 @@
 package strategy
 
-import "github.com/vdaas/vald/internal/errors"
+import (
+	"context"
+
+	"github.com/vdaas/vald/hack/benchmark/internal/assets"
+	"github.com/vdaas/vald/hack/benchmark/internal/core"
+	"github.com/vdaas/vald/internal/errors"
+)
 
 func wrapErrors(errs []error) (wrapped error) {
 	for _, err := range errs {
@@ -11,6 +17,50 @@ func wrapErrors(errs []error) (wrapped error) {
 				wrapped = errors.Wrap(wrapped, err.Error())
 			}
 		}
+	}
+	return
+}
+
+func insertAndCreateIndex32(ctx context.Context, c core.Core32, dataset assets.Dataset) (ids []uint, err error) {
+	n := 1000
+
+	train := dataset.Train()
+	ids = make([]uint, 0, len(train)*n)
+
+	for i := 0; i < n; i++ {
+		inserted, errs := c.BulkInsert(train)
+		err = wrapErrors(errs)
+		if err != nil {
+			return nil, err
+		}
+		ids = append(ids, inserted...)
+	}
+
+	err = c.CreateIndex(10)
+	if err != nil {
+		return nil, err
+	}
+	return
+}
+
+func insertAndCreateIndex64(ctx context.Context, c core.Core64, dataset assets.Dataset) (ids []uint, err error) {
+	n := 1000
+
+	train := dataset.TrainAsFloat64()
+	ids = make([]uint, 0, len(train)*n)
+
+	for i := 0; i < n; i++ {
+		inserted, errs := c.BulkInsert(train)
+		err = wrapErrors(errs)
+		if err != nil {
+			return nil, err
+		}
+		ids = append(ids, inserted...)
+	}
+
+	err = c.CreateIndex(10)
+	if err != nil {
+		return nil, err
 	}
 	return
 }
