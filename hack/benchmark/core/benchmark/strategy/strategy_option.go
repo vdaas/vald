74f3ffbb6b8042973671598644f5545de1e7136e
hlts2 <hiroto.funakoshi.hiroto@gmail.com>
2020-03-26 13:45:52 +0900

    Fixed review
    Signed-off-by: hlts2 <hiroto.funakoshi.hiroto@gmail.com>

--- hack/benchmark/core/benchmark/strategy/strategy_option.go
+++ hack/benchmark/core/benchmark/strategy/strategy_option.go
@@ -8,78 +8,85 @@ import (
 	"github.com/vdaas/vald/hack/benchmark/internal/core"
 )
 
-type StrategyOption func(*strategy)
+type StrategyOption func(*strategy) error
 
 var (
 	defaultStrategyOptions = []StrategyOption{}
 )
 
 func WithPreProp32(
-	fn func(context.Context, *testing.B, core.Core32, assets.Dataset) (interface{}, error),
+	fn func(context.Context, *testing.B, core.Core32, assets.Dataset) ([]uint, error),
 ) StrategyOption {
-	return func(s *strategy) {
+	return func(s *strategy) error {
 		if fn != nil {
 			s.preProp32 = fn
 		}
+		return nil
 	}
 }
 
 func WithProp32(
 	fn func(context.Context, *testing.B, core.Core32, assets.Dataset, []uint, *uint64) (interface{}, error),
 ) StrategyOption {
-	return func(s *strategy) {
-		if fn != nil {
+	return func(s *strategy) error {
+		if fn != nil && s.mode == core.Float32 {
 			s.prop32 = fn
 		}
+		return nil
 	}
 }
 
 func WithPreProp64(
-	fn func(context.Context, *testing.B, core.Core64, assets.Dataset) (interface{}, error),
+	fn func(context.Context, *testing.B, core.Core64, assets.Dataset) ([]uint, error),
 ) StrategyOption {
-	return func(s *strategy) {
+	return func(s *strategy) error {
 		if fn != nil {
 			s.preProp64 = fn
 		}
+		return nil
 	}
 }
 
 func WithProp64(
 	fn func(context.Context, *testing.B, core.Core64, assets.Dataset, []uint, *uint64) (interface{}, error),
 ) StrategyOption {
-	return func(s *strategy) {
-		if fn != nil {
+	return func(s *strategy) error {
+		if fn != nil && s.mode == core.Float64 {
 			s.prop64 = fn
 		}
+		return nil
 	}
 }
 
 func WithPropName(str string) StrategyOption {
-	return func(s *strategy) {
+	return func(s *strategy) error {
 		if len(str) != 0 {
 			s.propName = str
 		}
+		return nil
 	}
 }
 
-func WithCore32Initializer(
-	fn func(context.Context, assets.Dataset) (core.Core32, func(), error),
+func WithCore32(
+	fn func(context.Context, *testing.B, assets.Dataset) (core.Core32, core.Closer, error),
 ) StrategyOption {
-	return func(s *strategy) {
+	return func(s *strategy) (err error) {
 		if fn != nil {
-			s.initCore32 = fn
 			s.mode = core.Float32
+			s.initCore32 = fn
 		}
+		return nil
 	}
 }
 
-func WithCore64Initializer(
-	fn func(context.Context, assets.Dataset) (core.Core64, func(), error),
+func WithCore64(
+	fn func(context.Context, *testing.B, assets.Dataset) (core.Core64, core.Closer, error),
 ) StrategyOption {
-	return func(s *strategy) {
+	return func(s *strategy) error {
 		if fn != nil {
-			s.initCore64 = fn
 			s.mode = core.Float64
+			s.initCore64 = fn
 		}
+		return nil
 	}
 }
