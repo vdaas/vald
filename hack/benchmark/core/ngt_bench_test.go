e4e46d34731605532e0dc1080315eeb11e02f61c
hlts2 <hiroto.funakoshi.hiroto@gmail.com>
2020-03-26 15:11:31 +0900

    Added `search strategy`
    Signed-off-by: hlts2 <hiroto.funakoshi.hiroto@gmail.com>

--- hack/benchmark/core/ngt_bench_test.go
+++ hack/benchmark/core/ngt_bench_test.go
@@ -13,6 +13,12 @@ import (
 	"github.com/vdaas/vald/hack/benchmark/internal/core/ngt"
 )
 
+const (
+	size    int     = 1
+	radius  float32 = -1
+	epsilon float32 = 0.01
+)
+
 var targets []string
 
 func init() {
@@ -30,6 +36,30 @@ func BenchmarkNGT_Insert(b *testing.B) {
 			benchmark.WithName(target),
 			benchmark.WithStrategy(
 				strategy.NewInsert(
+					strategy.WithCore32(
+						func(ctx context.Context, b *testing.B, dataset assets.Dataset) (core.Core32, core.Closer, error) {
+							ngt, err := ngt.New(
+								ngt.WithDimension(dataset.Dimension()),
+								ngt.WithObjectType(dataset.ObjectType()),
+							)
+							if err != nil {
+								return nil, nil, err
+							}
+							return ngt, ngt, nil
+						}),
+				),
+			),
+		).Run(context.Background(), b)
+	}
+}
+
+func BenchmarkNGT_Search(b *testing.B) {
+	for _, target := range targets {
+		benchmark.New(b,
+			benchmark.WithName(target),
+			benchmark.WithStrategy(
+				strategy.NewSearch(
+					size, radius, epsilon,
 					strategy.WithCore32(
 						func(ctx context.Context, b *testing.B, dataset assets.Dataset) (core.Core32, core.Closer, error) {
 							ngt, err := ngt.New(
