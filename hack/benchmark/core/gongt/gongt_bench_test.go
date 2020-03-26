ace97b575dc0f1cbb232cf8e817af7196c7fe363
hlts2 <hiroto.funakoshi.hiroto@gmail.com>
2020-03-26 16:43:37 +0900

    Fixed package structure
    Signed-off-by: hlts2 <hiroto.funakoshi.hiroto@gmail.com>

---
+++ hack/benchmark/core/gongt/gongt_bench_test.go
@@ -0,0 +1,101 @@
+package gongt
+
+import (
+	"context"
+	"flag"
+	"strings"
+	"testing"
+
+	"github.com/vdaas/vald/hack/benchmark/core/benchmark"
+	"github.com/vdaas/vald/hack/benchmark/core/benchmark/strategy"
+	"github.com/vdaas/vald/hack/benchmark/internal/assets"
+	"github.com/vdaas/vald/hack/benchmark/internal/core"
+	"github.com/vdaas/vald/hack/benchmark/internal/core/gongt"
+)
+
+const (
+	size    int     = 1
+	radius  float32 = -1
+	epsilon float32 = 0.01
+)
+
+var targets []string
+
+func init() {
+	testing.Init()
+
+	var dataset string
+	flag.StringVar(&dataset, "dataset", "", "available dataset(choice with comma)")
+	flag.Parse()
+	targets = strings.Split(strings.TrimSpace(dataset), ",")
+}
+
+func BenchmarkGONGT_Insert(b *testing.B) {
+	for _, target := range targets {
+		benchmark.New(b,
+			benchmark.WithName(target),
+			benchmark.WithStrategy(
+				strategy.NewInsert(
+					strategy.WithCore64(
+						func(ctx context.Context, b *testing.B, dataset assets.Dataset) (core.Core64, core.Closer, error) {
+							ngt, err := gongt.New(
+								gongt.WithDimension(dataset.Dimension()),
+								gongt.WithObjectType(dataset.ObjectType()),
+							)
+							if err != nil {
+								return nil, nil, err
+							}
+							return ngt, ngt, nil
+						}),
+				),
+			),
+		).Run(context.Background(), b)
+	}
+}
+
+func BenchmarkGONGT_Search(b *testing.B) {
+	for _, target := range targets {
+		benchmark.New(b,
+			benchmark.WithName(target),
+			benchmark.WithStrategy(
+				strategy.NewSearch(
+					size, radius, epsilon,
+					strategy.WithCore64(
+						func(ctx context.Context, b *testing.B, dataset assets.Dataset) (core.Core64, core.Closer, error) {
+							ngt, err := gongt.New(
+								gongt.WithDimension(dataset.Dimension()),
+								gongt.WithObjectType(dataset.ObjectType()),
+							)
+							if err != nil {
+								return nil, nil, err
+							}
+							return ngt, ngt, nil
+						}),
+				),
+			),
+		).Run(context.Background(), b)
+	}
+}
+
+func BenchmarkGONGT_Delete(b *testing.B) {
+	for _, target := range targets {
+		benchmark.New(b,
+			benchmark.WithName(target),
+			benchmark.WithStrategy(
+				strategy.NewDelete(
+					strategy.WithCore64(
+						func(ctx context.Context, b *testing.B, dataset assets.Dataset) (core.Core64, core.Closer, error) {
+							ngt, err := gongt.New(
+								gongt.WithDimension(dataset.Dimension()),
+								gongt.WithObjectType(dataset.ObjectType()),
+							)
+							if err != nil {
+								return nil, nil, err
+							}
+							return ngt, ngt, nil
+						}),
+				),
+			),
+		).Run(context.Background(), b)
+	}
+}
