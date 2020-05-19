aa9f28e7140f6740cbd83aa9c9a936d40a272a5b
Rintaro Okamura <rintaro.okamura@gmail.com>
2020-05-19 10:57:59 +0900

    :sparkles: Add internal/db/storage/blob
    Signed-off-by: Rintaro Okamura <rintaro.okamura@gmail.com>

---
+++ internal/db/storage/blob/s3/option.go
@@ -0,0 +1,53 @@
+//
+// Copyright (C) 2019-2020 Vdaas.org Vald team ( kpango, rinx, kmrmt )
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+package s3
+
+type Option func(s *s3session)
+
+var (
+	defaultOpts = []Option{}
+)
+
+func WithEndpoint(ep string) Option {
+	return func(s *s3session) {
+		s.endpoint = ep
+	}
+}
+
+func WithRegion(rg string) Option {
+	return func(s *s3session) {
+		s.region = rg
+	}
+}
+
+func WithAccessKey(ak string) Option {
+	return func(s *s3session) {
+		s.accessKey = ak
+	}
+}
+
+func WithSecretAccessKey(sak string) Option {
+	return func(s *s3session) {
+		s.secretAccessKey = sak
+	}
+}
+
+func WithToken(tk string) Option {
+	return func(s *s3session) {
+		s.token = tk
+	}
+}
---
+++ internal/db/storage/blob/s3/s3.go
@@ -0,0 +1,70 @@
+//
+// Copyright (C) 2019-2020 Vdaas.org Vald team ( kpango, rinx, kmrmt )
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+package s3
+
+import (
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"gocloud.dev/blob/s3blob"
+)
+
+type s3URLOpener = s3blob.URLOpener
+
+type s3session struct {
+	endpoint        string
+	region          string
+	accessKey       string
+	secretAccessKey string
+	token           string
+}
+
+type S3Session interface {
+	URLOpener() *s3URLOpener
+}
+
+func NewS3Session(opts ...Option) S3Session {
+	s := new(s3session)
+	for _, opt := range append(defaultOpts, opts...) {
+		opt(s)
+	}
+
+	return s
+}
+
+func (s *s3session) URLOpener() *s3URLOpener {
+	session := session.New(
+		aws.NewConfig().WithEndpoint(
+			s.endpoint,
+		).WithRegion(
+			s.region,
+		).WithCredentials(
+			credentials.NewStaticCredentials(
+				s.accessKey,
+				s.secretAccessKey,
+				s.token,
+			),
+		),
+	)
+
+	return &s3URLOpener{
+		ConfigProvider: session,
+		Options: s3blob.Options{
+			UseLegacyList: false,
+		},
+	}
+}
