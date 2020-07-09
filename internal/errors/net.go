ee488ac53851fdb949412dcf10ab86d390b9a445
kevindiu <kevin_diu@yahoo.com.hk>
2020-07-09 15:37:15 +0900

    fix comment

--- internal/errors/net.go
+++ internal/errors/net.go
@@ -13,6 +13,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 //
+
+// Package errors provides error types and function
 package errors
 
 import "time"
