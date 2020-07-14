8e1082ca096c66b040a23837d429d87e0f0fd054
vankichi <kyukawa315@gmail.com>
2020-07-14 13:49:15 +0900

    :recycle: add comment
    Signed-off-by: vankichi <kyukawa315@gmail.com>

--- internal/cache/gache/option.go
+++ internal/cache/gache/option.go
@@ -24,7 +24,7 @@ import (
 	"github.com/kpango/gache"
 )
 
-// Option represents a type of func(*cache).
+// Option represents the functional option for cache.
 type Option func(*cache)
 
 // defaultOptions returns []Option with gache.New().
