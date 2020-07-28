23d10bb95c631ea2d6b95ff33aab28ac582ddc55
Kiichiro YUKAWA <kyukawa315@gmail.com>
2020-07-28 15:32:24 +0900

    :recycle: Update internal/net/http/transport/option.go
    Co-authored-by: Kevin Diu <kevin_diu@yahoo.com.hk>

--- internal/net/http/transport/option.go
+++ internal/net/http/transport/option.go
@@ -39,7 +39,7 @@ func WithRoundTripper(tr http.RoundTripper) Option {
 	}
 }
 
-// WithBackoff returns the Option that set the backoff when bo is not nil.
+// WithBackoff returns the Option that set the backoff.
 func WithBackoff(bo backoff.Backoff) Option {
 	return func(e *ert) {
 		e.bo = bo
