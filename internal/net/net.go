49c8e6c787b8a71bc520be1ce06f6243e476e19f
kevindiu <kevin_diu@yahoo.com.hk>
2020-08-12 14:33:49 +0900

    add comments

--- internal/net/net.go
+++ internal/net/net.go
@@ -91,10 +91,14 @@ func IsIPv4(addr string) bool {
 
 // SplitHostPort splits the address, and return the host/IP address and the port number,
 // and any error occurred.
+// If it is the loopback address, it will return the loopback address and corresponding port number.
+// IPv6 loopback address is not supported yet. For more information, please read https://github.com/vdaas/vald/projects/3#card-43504189
 func SplitHostPort(hostport string) (host string, port uint16, err error) {
 	switch {
+	/* TODO: IPv6 loopback address support
 	case strings.HasPrefix(hostport, "::"):
 		hostport = localIPv6 + hostport
+	*/
 	case strings.HasPrefix(hostport, ":"):
 		hostport = localIPv4 + hostport
 	}
