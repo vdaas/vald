0c72ab7752a218d13eb968a530281ea74bcb2c25
kpango <i.can.feel.gravity@gmail.com>
2020-09-25 20:36:06 +0900

    [patch] fix duplicated search result
    Signed-off-by: kpango <i.can.feel.gravity@gmail.com>

--- pkg/gateway/vald/handler/grpc/handler.go
+++ pkg/gateway/vald/handler/grpc/handler.go
@@ -161,15 +161,13 @@ func (s *server) search(ctx context.Context, cfg *payload.Search_Config,
 					return nil
 				}
 				id := dist.GetId()
-				mu.RLock()
+				mu.Lock()
 				if !visited[id] {
-					mu.RUnlock()
-					mu.Lock()
 					visited[id] = true
 					mu.Unlock()
 					dch <- dist
 				} else {
-					mu.RUnlock()
+					mu.Unlock()
 				}
 				// if !cl.Exists(id) {
 				// 	dch <- dist
@@ -219,9 +217,12 @@ func (s *server) search(ctx context.Context, cfg *payload.Search_Config,
 			}
 			return res, nil
 		case dist := <-dch:
-			if len(res.GetResults()) >= num &&
-				dist.GetDistance() < math.Float32frombits(atomic.LoadUint32(&maxDist)) {
-				atomic.StoreUint32(&maxDist, math.Float32bits(dist.GetDistance()))
+			if len(res.GetResults()) >= num {
+				if dist.GetDistance() < math.Float32frombits(atomic.LoadUint32(&maxDist)) {
+					atomic.StoreUint32(&maxDist, math.Float32bits(dist.GetDistance()))
+				} else {
+					continue
+				}
 			}
 			switch len(res.GetResults()) {
 			case 0:
