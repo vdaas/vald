d56c80aead961616af052ac4a12fc73eb0b42b1c
Rintaro Okamura <rintaro.okamura@gmail.com>
2020-10-14 02:12:26 +0000

    :whale: fix Makefile.d/docker.mk

--- Makefile.d/docker.mk
+++ Makefile.d/docker.mk
@@ -21,6 +21,9 @@ docker/build: \
 	docker/build/agent-sidecar \
 	docker/build/discoverer-k8s \
 	docker/build/gateway-vald \
+	docker/build/gateway-lb \
+	docker/build/gateway-meta \
+	docker/build/gateway-backup \
 	docker/build/meta-redis \
 	docker/build/meta-cassandra \
 	docker/build/backup-manager-mysql \
@@ -116,12 +119,12 @@ docker/name/gateway-lb:
 
 .PHONY: docker/build/gateway-lb
 ## build gateway-lb image
-docker/build/gateway-lb: docker/build/base
+docker/build/gateway-lb:
 	$(DOCKER) build \
 	    $(DOCKER_OPTS) \
 	    -f dockers/gateway/lb/Dockerfile \
 	    -t $(REPO)/$(LB_GATEWAY_IMAGE):$(TAG) . \
-	    --build-arg BASE_TAG=$(TAG) \
+	    --build-arg GO_VERSION=$(GO_VERSION) \
 	    --build-arg DISTROLESS_IMAGE=$(DISTROLESS_IMAGE) \
 	    --build-arg DISTROLESS_IMAGE_TAG=$(DISTROLESS_IMAGE_TAG) \
 	    --build-arg UPX_OPTIONS=$(UPX_OPTIONS)
@@ -132,12 +135,12 @@ docker/name/gateway-meta:
 
 .PHONY: docker/build/gateway-meta
 ## build gateway-meta image
-docker/build/gateway-meta: docker/build/base
+docker/build/gateway-meta:
 	$(DOCKER) build \
 	    $(DOCKER_OPTS) \
 	    -f dockers/gateway/meta/Dockerfile \
 	    -t $(REPO)/$(META_GATEWAY_IMAGE):$(TAG) . \
-	    --build-arg BASE_TAG=$(TAG) \
+	    --build-arg GO_VERSION=$(GO_VERSION) \
 	    --build-arg DISTROLESS_IMAGE=$(DISTROLESS_IMAGE) \
 	    --build-arg DISTROLESS_IMAGE_TAG=$(DISTROLESS_IMAGE_TAG) \
 	    --build-arg UPX_OPTIONS=$(UPX_OPTIONS)
@@ -148,12 +151,12 @@ docker/name/gateway-backup:
 
 .PHONY: docker/build/gateway-backup
 ## build gateway-backup image
-docker/build/gateway-backup: docker/build/base
+docker/build/gateway-backup:
 	$(DOCKER) build \
 	    $(DOCKER_OPTS) \
 	    -f dockers/gateway/backup/Dockerfile \
 	    -t $(REPO)/$(BACKUP_GATEWAY_IMAGE)$(TAG) . \
-	    --build-arg BASE_TAG=$(TAG) \
+	    --build-arg GO_VERSION=$(GO_VERSION) \
 	    --build-arg DISTROLESS_IMAGE=$(DISTROLESS_IMAGE) \
 	    --build-arg DISTROLESS_IMAGE_TAG=$(DISTROLESS_IMAGE_TAG) \
 	    --build-arg UPX_OPTIONS=$(UPX_OPTIONS)
