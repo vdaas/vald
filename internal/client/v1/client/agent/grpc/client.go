ae2df57412b903af76f3bac866d98597e2bf47f7
kpango <i.can.feel.gravity@gmail.com>
2020-10-28 20:03:07 +0900

    fix
    Signed-off-by: kpango <i.can.feel.gravity@gmail.com>

--- internal/client/v1/client/agent/grpc/client.go
+++ internal/client/v1/client/agent/grpc/client.go
@@ -21,9 +21,8 @@ import (
 	"context"
 
 	agent "github.com/vdaas/vald/apis/grpc/v1/agent/core"
-	"github.com/vdaas/vald/apis/grpc/v1/payload"
-	"github.com/vdaas/vald/apis/grpc/v1/vald"
 	"github.com/vdaas/vald/internal/client/v1/client"
+	"github.com/vdaas/vald/internal/client/v1/client/gateway/vald"
 	"github.com/vdaas/vald/internal/net/grpc"
 )
 
@@ -35,6 +34,7 @@ type Client interface {
 }
 
 type agentClient struct {
+	vald.Client
 	addr string
 	c    grpc.Client
 }
@@ -45,280 +45,14 @@ func New(opts ...Option) Client {
 	for _, opt := range opts {
 		opt(c)
 	}
-	return c
-}
-
-func (c *agentClient) Exists(ctx context.Context, in *payload.Object_ID, opts ...grpc.CallOption) (oid *payload.Object_ID, err error) {
-	_, err = c.c.Do(ctx, c.addr, func(ctx context.Context,
-		conn *grpc.ClientConn,
-		copts ...grpc.CallOption) (interface{}, error) {
-		oid, err = vald.NewValdClient(conn).Exists(ctx, in, append(copts, opts...)...)
-		return nil, err
-	})
-	if err != nil {
-		return nil, err
+	return &agentClient{
+		Client: vald.New(
+			vald.WithAddr(c.addr),
+			vald.WithClient(c.c),
+		),
+		addr: c.addr,
+		c:    c.c,
 	}
-	return oid, nil
-}
-
-func (c *agentClient) Search(ctx context.Context, in *payload.Search_Request, opts ...grpc.CallOption) (res *payload.Search_Response, err error) {
-	_, err = c.c.Do(ctx, c.addr, func(ctx context.Context,
-		conn *grpc.ClientConn,
-		copts ...grpc.CallOption) (interface{}, error) {
-		res, err = vald.NewValdClient(conn).Search(ctx, in, append(copts, opts...)...)
-		return nil, err
-	})
-	if err != nil {
-		return nil, err
-	}
-	return res, nil
-}
-
-func (c *agentClient) SearchByID(ctx context.Context, in *payload.Search_IDRequest, opts ...grpc.CallOption) (res *payload.Search_Response, err error) {
-	_, err = c.c.Do(ctx, c.addr, func(ctx context.Context,
-		conn *grpc.ClientConn,
-		copts ...grpc.CallOption) (interface{}, error) {
-		res, err = vald.NewValdClient(conn).SearchByID(ctx, in, append(copts, opts...)...)
-		return nil, err
-	})
-	if err != nil {
-		return nil, err
-	}
-	return res, nil
-}
-
-func (c *agentClient) StreamSearch(ctx context.Context, opts ...grpc.CallOption) (res vald.Search_StreamSearchClient, err error) {
-	_, err = c.c.Do(ctx, c.addr, func(ctx context.Context,
-		conn *grpc.ClientConn,
-		copts ...grpc.CallOption) (interface{}, error) {
-		res, err = vald.NewValdClient(conn).StreamSearch(ctx, append(copts, opts...)...)
-		return nil, err
-	})
-	if err != nil {
-		return nil, err
-	}
-	return res, nil
-}
-
-func (c *agentClient) StreamSearchByID(ctx context.Context, opts ...grpc.CallOption) (res vald.Search_StreamSearchByIDClient, err error) {
-	_, err = c.c.Do(ctx, c.addr, func(ctx context.Context,
-		conn *grpc.ClientConn,
-		copts ...grpc.CallOption) (interface{}, error) {
-		res, err = vald.NewValdClient(conn).StreamSearchByID(ctx, append(copts, opts...)...)
-		return nil, err
-	})
-	if err != nil {
-		return nil, err
-	}
-	return res, nil
-}
-
-func (c *agentClient) MultiSearch(ctx context.Context, in *payload.Search_MultiRequest, opts ...grpc.CallOption) (res *payload.Search_Responses, err error) {
-	_, err = c.c.Do(ctx, c.addr, func(ctx context.Context,
-		conn *grpc.ClientConn,
-		copts ...grpc.CallOption) (interface{}, error) {
-		res, err = vald.NewValdClient(conn).MultiSearch(ctx, in, append(copts, opts...)...)
-		return nil, err
-	})
-	if err != nil {
-		return nil, err
-	}
-	return res, nil
-}
-
-func (c *agentClient) MultiSearchByID(ctx context.Context, in *payload.Search_MultiIDRequest, opts ...grpc.CallOption) (res *payload.Search_Responses, err error) {
-	_, err = c.c.Do(ctx, c.addr, func(ctx context.Context,
-		conn *grpc.ClientConn,
-		copts ...grpc.CallOption) (interface{}, error) {
-		res, err = vald.NewValdClient(conn).MultiSearchByID(ctx, in, append(copts, opts...)...)
-		return nil, err
-	})
-	if err != nil {
-		return nil, err
-	}
-	return res, nil
-}
-
-func (c *agentClient) Insert(ctx context.Context, in *payload.Insert_Request, opts ...grpc.CallOption) (res *payload.Object_Location, err error) {
-	_, err = c.c.Do(ctx, c.addr, func(ctx context.Context,
-		conn *grpc.ClientConn,
-		copts ...grpc.CallOption) (interface{}, error) {
-		res, err = vald.NewValdClient(conn).Insert(ctx, in, append(copts, opts...)...)
-		return nil, err
-	})
-	if err != nil {
-		return nil, err
-	}
-	return res, nil
-}
-
-func (c *agentClient) StreamInsert(ctx context.Context, opts ...grpc.CallOption) (res vald.Insert_StreamInsertClient, err error) {
-	_, err = c.c.Do(ctx, c.addr, func(ctx context.Context,
-		conn *grpc.ClientConn,
-		copts ...grpc.CallOption) (interface{}, error) {
-		res, err = vald.NewValdClient(conn).StreamInsert(ctx, append(copts, opts...)...)
-		return nil, err
-	})
-	if err != nil {
-		return nil, err
-	}
-	return res, nil
-}
-
-func (c *agentClient) MultiInsert(ctx context.Context, in *payload.Insert_MultiRequest, opts ...grpc.CallOption) (res *payload.Object_Locations, err error) {
-	_, err = c.c.Do(ctx, c.addr, func(ctx context.Context,
-		conn *grpc.ClientConn,
-		copts ...grpc.CallOption) (interface{}, error) {
-		res, err = vald.NewValdClient(conn).MultiInsert(ctx, in, append(copts, opts...)...)
-		return nil, err
-	})
-	if err != nil {
-		return nil, err
-	}
-	return res, nil
-}
-
-func (c *agentClient) Update(ctx context.Context, in *payload.Update_Request, opts ...grpc.CallOption) (res *payload.Object_Location, err error) {
-	_, err = c.c.Do(ctx, c.addr, func(ctx context.Context,
-		conn *grpc.ClientConn,
-		copts ...grpc.CallOption) (interface{}, error) {
-		res, err = vald.NewValdClient(conn).Update(ctx, in, append(copts, opts...)...)
-		return nil, err
-	})
-	if err != nil {
-		return nil, err
-	}
-	return res, nil
-}
-
-func (c *agentClient) StreamUpdate(ctx context.Context, opts ...grpc.CallOption) (res vald.Update_StreamUpdateClient, err error) {
-	_, err = c.c.Do(ctx, c.addr, func(ctx context.Context,
-		conn *grpc.ClientConn,
-		copts ...grpc.CallOption) (interface{}, error) {
-		res, err = vald.NewValdClient(conn).StreamUpdate(ctx, append(copts, opts...)...)
-		return nil, err
-	})
-	if err != nil {
-		return nil, err
-	}
-	return res, nil
-}
-
-func (c *agentClient) MultiUpdate(ctx context.Context, in *payload.Update_MultiRequest, opts ...grpc.CallOption) (res *payload.Object_Locations, err error) {
-	_, err = c.c.Do(ctx, c.addr, func(ctx context.Context,
-		conn *grpc.ClientConn,
-		copts ...grpc.CallOption) (interface{}, error) {
-		res, err = vald.NewValdClient(conn).MultiUpdate(ctx, in, append(copts, opts...)...)
-		return nil, err
-	})
-	if err != nil {
-		return nil, err
-	}
-	return res, nil
-}
-
-func (c *agentClient) Upsert(ctx context.Context, in *payload.Upsert_Request, opts ...grpc.CallOption) (res *payload.Object_Location, err error) {
-	_, err = c.c.Do(ctx, c.addr, func(ctx context.Context,
-		conn *grpc.ClientConn,
-		copts ...grpc.CallOption) (interface{}, error) {
-		res, err = vald.NewValdClient(conn).Upsert(ctx, in, append(copts, opts...)...)
-		return nil, err
-	})
-	if err != nil {
-		return nil, err
-	}
-	return res, nil
-}
-
-func (c *agentClient) StreamUpsert(ctx context.Context, opts ...grpc.CallOption) (res vald.Upsert_StreamUpsertClient, err error) {
-	_, err = c.c.Do(ctx, c.addr, func(ctx context.Context,
-		conn *grpc.ClientConn,
-		copts ...grpc.CallOption) (interface{}, error) {
-		res, err = vald.NewValdClient(conn).StreamUpsert(ctx, append(copts, opts...)...)
-		return nil, err
-	})
-	if err != nil {
-		return nil, err
-	}
-	return res, nil
-}
-
-func (c *agentClient) MultiUpsert(ctx context.Context, in *payload.Upsert_MultiRequest, opts ...grpc.CallOption) (res *payload.Object_Locations, err error) {
-	_, err = c.c.Do(ctx, c.addr, func(ctx context.Context,
-		conn *grpc.ClientConn,
-		copts ...grpc.CallOption) (interface{}, error) {
-		res, err = vald.NewValdClient(conn).MultiUpsert(ctx, in, append(copts, opts...)...)
-		return nil, err
-	})
-	if err != nil {
-		return nil, err
-	}
-	return res, nil
-}
-
-func (c *agentClient) Remove(ctx context.Context, in *payload.Remove_Request, opts ...grpc.CallOption) (res *payload.Object_Location, err error) {
-	_, err = c.c.Do(ctx, c.addr, func(ctx context.Context,
-		conn *grpc.ClientConn,
-		copts ...grpc.CallOption) (interface{}, error) {
-		res, err = vald.NewValdClient(conn).Remove(ctx, in, append(copts, opts...)...)
-		return nil, err
-	})
-	if err != nil {
-		return nil, err
-	}
-	return res, nil
-}
-
-func (c *agentClient) StreamRemove(ctx context.Context, opts ...grpc.CallOption) (res vald.Remove_StreamRemoveClient, err error) {
-	_, err = c.c.Do(ctx, c.addr, func(ctx context.Context,
-		conn *grpc.ClientConn,
-		copts ...grpc.CallOption) (interface{}, error) {
-		res, err = vald.NewValdClient(conn).StreamRemove(ctx, append(copts, opts...)...)
-		return nil, err
-	})
-	if err != nil {
-		return nil, err
-	}
-	return res, nil
-}
-
-func (c *agentClient) MultiRemove(ctx context.Context, in *payload.Remove_MultiRequest, opts ...grpc.CallOption) (res *payload.Object_Locations, err error) {
-	_, err = c.c.Do(ctx, c.addr, func(ctx context.Context,
-		conn *grpc.ClientConn,
-		copts ...grpc.CallOption) (interface{}, error) {
-		res, err = vald.NewValdClient(conn).MultiRemove(ctx, in, append(copts, opts...)...)
-		return nil, err
-	})
-	if err != nil {
-		return nil, err
-	}
-	return res, nil
-}
-
-func (c *agentClient) GetObject(ctx context.Context, in *payload.Object_ID, opts ...grpc.CallOption) (res *payload.Object_Vector, err error) {
-	_, err = c.c.Do(ctx, c.addr, func(ctx context.Context,
-		conn *grpc.ClientConn,
-		copts ...grpc.CallOption) (interface{}, error) {
-		res, err = vald.NewValdClient(conn).GetObject(ctx, in, append(copts, opts...)...)
-		return nil, err
-	})
-	if err != nil {
-		return nil, err
-	}
-	return res, nil
-}
-
-func (c *agentClient) StreamGetObject(ctx context.Context, opts ...grpc.CallOption) (res vald.Object_StreamGetObjectClient, err error) {
-	_, err = c.c.Do(ctx, c.addr, func(ctx context.Context,
-		conn *grpc.ClientConn,
-		copts ...grpc.CallOption) (interface{}, error) {
-		res, err = vald.NewValdClient(conn).StreamGetObject(ctx, append(copts, opts...)...)
-		return nil, err
-	})
-	if err != nil {
-		return nil, err
-	}
-	return res, nil
 }
 
 func (c *agentClient) CreateIndex(
