f728018046cb6f7ba26aa22ad845b6c6db72e0f3
kevindiu <kevin_diu@yahoo.com.hk>
2020-11-09 17:11:26 +0900

    add replica set reconciler

---
+++ internal/k8s/replicaset/option.go
@@ -0,0 +1,40 @@
+package main
+
+import "sigs.k8s.io/controller-runtime/pkg/manager"
+
+// Option represents functional option for reconciler.
+type Option func(*reconciler) error
+
+var defaultOpts = []Option{}
+
+// WithControllerName returns Option that sets r.name.
+func WithControllerName(name string) Option {
+	return func(r *reconciler) error {
+		r.name = name
+		return nil
+	}
+}
+
+// WithManager returns Option that sets r.mgr.
+func WithManager(mgr manager.Manager) Option {
+	return func(r *reconciler) error {
+		r.mgr = mgr
+		return nil
+	}
+}
+
+// WithOnErrorFunc returns Option that sets r.onError.
+func WithOnErrorFunc(f func(err error)) Option {
+	return func(r *reconciler) error {
+		r.onError = f
+		return nil
+	}
+}
+
+// WithOnReconcileFunc returns Option that sets r.onReconcile.
+func WithOnReconcileFunc(f func(replicasetList map[string][]ReplicaSet)) Option {
+	return func(r *reconciler) error {
+		r.onReconcile = f
+		return nil
+	}
+}
---
+++ internal/k8s/replicaset/replicaset.go
@@ -0,0 +1,120 @@
+package main
+
+import (
+	"context"
+	"reflect"
+	"strings"
+	"time"
+
+	appsv1 "k8s.io/api/apps/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/runtime"
+	"sigs.k8s.io/controller-runtime/pkg/manager"
+)
+
+type ReplicaSetWatcher k8s.ResourceController
+
+type reconciler struct {
+	ctx         context.Context
+	mgr         manager.Manager
+	name        string
+	namespace   string
+	onError     func(err error)
+	onReconcile func(rs []ReplicaSet)
+}
+
+type ReplicaSet struct {
+	Name            string
+	Namespace       string
+	DesiredReplicas *int32
+	Replicas        int32
+}
+
+func New(opts ...Option) (ReplicaSetWatcher, error) {
+	r := new(reconciler)
+
+	for _, opt := range opts {
+		if err := opt(r); err != nil {
+			return erros.ErrOptionFailed(err, reflect.ValueOf(opt))
+		}
+	}
+
+	return r
+}
+
+func (r *reconciler) Reconcile(req reconcile.Request) (res reconcile.Result, err error) {
+	rsl := new(appsv1.ReplicaSetList)
+
+	err = r.mgr.GetClient().List(r.ctx, rsl)
+	if err != nil {
+		if r.onError != nil {
+			r.onError(err)
+		}
+		res = reconcile.Result{
+			Requeue:      true,
+			RequeueAfter: time.Millisecond * 100,
+		}
+		if errors.IsNotFound(err) {
+			res = reconcile.Result{
+				Requeue:      true,
+				RequeueAfter: time.Second,
+			}
+		}
+		return
+	}
+
+	rs := make(map[string][]ReplicaSet, len(rsl.Items))
+
+	for _, replicaset := range rsl.Items {
+		name, ok := replicaset.GetObjectMeta().GetLabels()["app"]
+		if !ok {
+			pns := strings.Split(pod.GetName(), "-")
+			name = strings.Join(pns[:len(pns)-1], "-")
+		}
+
+		if _, ok := rs[name]; !ok {
+			rs[name] = make([]ReplicaSet, 0, len(rsl.Items))
+		}
+
+		rs[name] = append(rs[name], ReplicaSet{
+			Name:            name,
+			Namespace:       replicaset.GetNamespace(),
+			DesiredReplicas: replicaset.ReplicaSetSpec.Replicas,
+			Replicas:        replicaset.Status.Replicas,
+		})
+	}
+
+	if r.onReconcile != nil {
+		r.onReconcile(rs)
+	}
+
+	return
+}
+
+func (r *reconciler) GetName() string {
+	return r.name
+}
+
+func (r *reconciler) NewReconciler(ctx context.Context, mgr manager.Manager) reconcile.Reconciler {
+	if r.ctx == nil && ctx != nil {
+		r.ctx = ctx
+	}
+	if r.mgr == nil && mgr != nil {
+		r.mgr = mgr
+	}
+	appv1.AddToScheme(r.mgr.GetScheme())
+	return r
+}
+
+func (r *reconciler) For() runtime.Object {
+	return new(appv1.ReplicaSet)
+}
+
+func (r *reconciler) Owns() runtime.Object {
+	return nil
+}
+
+func (r *reconciler) Watches() (*source.Kind, handler.EventHandler) {
+	// return &source.Kind{Type: new(corev1.Pod)}, &handler.EnqueueRequestForObject{}
+	return nil, nil
+}
