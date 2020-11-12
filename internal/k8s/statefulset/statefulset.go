a0d6167f3a5e96251547734a13f1c19d4ca153e6
vankichi <kyukawa315@gmail.com>
2020-11-12 11:14:27 +0900

    :sparkles: implement statefulset reconciler for rebalance
    Signed-off-by: vankichi <kyukawa315@gmail.com>

---
+++ internal/k8s/statefulset/statefulset.go
@@ -0,0 +1,126 @@
+package statefulset
+
+import (
+	"context"
+	"reflect"
+	"strings"
+	"time"
+
+	"github.com/vdaas/vald/internal/errors"
+	"github.com/vdaas/vald/internal/k8s"
+	appsv1 "k8s.io/api/apps/v1"
+	k8serrors "k8s.io/apimachinery/pkg/api/errors"
+
+	"k8s.io/apimachinery/pkg/runtime"
+
+	"sigs.k8s.io/controller-runtime/pkg/handler"
+	"sigs.k8s.io/controller-runtime/pkg/manager"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
+	"sigs.k8s.io/controller-runtime/pkg/source"
+)
+
+// StatefulSetWatcher is a type alias for k8s resource controller
+type StatefulSetWatcher k8s.ResourceController
+
+type reconciler struct {
+	ctx         context.Context
+	mgr         manager.Manager
+	name        string
+	namespace   string
+	onError     func(err error)
+	onReconcile func(rs map[string][]StatefulSet)
+}
+
+// Statefulset is a type alias for the k8s statefulset definition.
+type StatefulSet = appsv1.StatefulSet
+
+// New returns the StatefulSetWather that implements reconciliation loop, or any error occured.
+func New(opts ...Option) (StatefulSetWatcher, error) {
+	r := new(reconciler)
+
+	for _, opt := range append(defaultOpts, opts...) {
+		if err := opt(r); err != nil {
+			return nil, errors.ErrOptionFailed(err, reflect.ValueOf(opt))
+		}
+	}
+
+	return r, nil
+}
+
+// Reconcile implements k8s reconciliation loop to retrive the StatefulSet information from k8s.
+func (r *reconciler) Reconcile(req reconcile.Request) (res reconcile.Result, err error) {
+	ssl := new(appsv1.StatefulSetList)
+
+	err = r.mgr.GetClient().List(r.ctx, ssl)
+	if err != nil {
+		if r.onError != nil {
+			r.onError(err)
+		}
+		res = reconcile.Result{
+			Requeue:      true,
+			RequeueAfter: time.Millisecond * 100,
+		}
+		if k8serrors.IsNotFound(err) {
+			res.RequeueAfter = time.Second
+		}
+		return
+	}
+
+	ssm := make(map[string][]StatefulSet, len(ssl.Items))
+
+	for _, statefulset := range ssl.Items {
+		name, ok := statefulset.GetObjectMeta().GetLabels()["app"]
+		if !ok {
+			pns := strings.Split(statefulset.GetName(), "-")
+			name = strings.Join(pns[:len(pns)-1], "-")
+		}
+		if _, ok := ssm[name]; !ok {
+			ssm[name] = make([]StatefulSet, 0, len(ssl.Items))
+		}
+
+		ssm[name] = append(ssm[name], statefulset)
+	}
+
+	if r.onReconcile != nil {
+		r.onReconcile(ssm)
+	}
+
+	return
+}
+
+// GetName returns the name of resource controller.
+func (r *reconciler) GetName() string {
+	return r.name
+}
+
+// NewReconciler returns the reconciler for the StatefulSet.
+func (r *reconciler) NewReconciler(ctx context.Context, mgr manager.Manager) reconcile.Reconciler {
+	if r.ctx == nil && ctx != nil {
+		r.ctx = ctx
+	}
+
+	if r.mgr == nil && mgr != nil {
+		r.mgr = mgr
+	}
+
+	appsv1.AddToScheme(r.mgr.GetScheme())
+	return r
+}
+
+// For returns the runtime.Object which is StatefulSet.
+func (r *reconciler) For() runtime.Object {
+	return new(appsv1.StatefulSet)
+}
+
+// Owns returns the owner of the StatefulSet wathcer.
+// It will always return nil.
+func (r *reconciler) Owns() runtime.Object {
+	return nil
+}
+
+// Watches returns the kind of the StatefulSet and the event handler.
+// It will always retrun nil.
+func (r *reconciler) Watches() (*source.Kind, handler.EventHandler) {
+	// return &source.kind{Type: new(corev1.Pod)}, &handler.EnqueueRequestForObject{}
+	return nil, nil
+}
