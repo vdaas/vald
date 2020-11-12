8e3f9d5d02750a675f66d3cc1e8e47adb47cddd1
vankichi <kyukawa315@gmail.com>
2020-11-12 15:21:18 +0900

    fix
    Signed-off-by: vankichi <kyukawa315@gmail.com>

--- internal/k8s/statefulset/statefulset.go
+++ internal/k8s/statefulset/statefulset.go
@@ -23,6 +23,7 @@ import (
 
 	"github.com/vdaas/vald/internal/errors"
 	"github.com/vdaas/vald/internal/k8s"
+	"github.com/vdaas/vald/internal/log"
 	appsv1 "k8s.io/api/apps/v1"
 	k8serrors "k8s.io/apimachinery/pkg/api/errors"
 
