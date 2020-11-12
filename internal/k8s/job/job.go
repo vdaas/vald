17f73ba3ed4764bf4868467ebc016802d9529df0
Hiroto Funakoshi <hiroto.funakoshi.hiroto@gmail.com>
2020-11-12 16:26:02 +0900

    :sparkles: add reconciliation loop for job resource (#822)
    * feat: add reconcilation for job resource
    
    Signed-off-by: hlts2 <hiroto.funakoshi.hiroto@gmail.com>
    
    * fix: pointer struct initialize format
    
    Signed-off-by: hlts2 <hiroto.funakoshi.hiroto@gmail.com>
    
    * fix: import path group
    
    Signed-off-by: hlts2 <hiroto.funakoshi.hiroto@gmail.com>
    
    * feat: add type alis for job resource controller
    
    Signed-off-by: hlts2 <hiroto.funakoshi.hiroto@gmail.com>
    
    * fix: typo
    
    Signed-off-by: hlts2 <hiroto.funakoshi.hiroto@gmail.com>
    
    * fix: refactor
    
    Signed-off-by: hlts2 <hiroto.funakoshi.hiroto@gmail.com>
    
    * fix: apply suggestion
    
    Signed-off-by: hlts2 <hiroto.funakoshi.hiroto@gmail.com>
    
    * fix: add comment and refactor
    
    Signed-off-by: hlts2 <hiroto.funakoshi.hiroto@gmail.com>
    
    * fix: apply suggestion
    
    Signed-off-by: hlts2 <hiroto.funakoshi.hiroto@gmail.com>
    
    * shrink job slice
    
    * :robot: Update license headers / Format go codes and yaml files
    
    Signed-off-by: vdaas-ci <ci@vdaas.org>
    
    Co-authored-by: kevindiu <kevin_diu@yahoo.com.hk>
    Co-authored-by: vdaas-ci <ci@vdaas.org>

---
+++ internal/k8s/job/job.go
@@ -0,0 +1,149 @@
+//
+// Copyright (C) 2019-2020 Vdaas.org Vald team ( kpango, rinx, kmrmt )
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+package job
+
+import (
+	"context"
+	"reflect"
+	"strings"
+	"time"
+
+	batchv1 "k8s.io/api/batch/v1"
+	k8serrors "k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/runtime"
+	"sigs.k8s.io/controller-runtime/pkg/handler"
+	"sigs.k8s.io/controller-runtime/pkg/manager"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
+	"sigs.k8s.io/controller-runtime/pkg/source"
+
+	"github.com/vdaas/vald/internal/errors"
+	"github.com/vdaas/vald/internal/k8s"
+	"github.com/vdaas/vald/internal/log"
+)
+
+// JobWatcher is a type alias for k8s resource controller.
+type JobWatcher k8s.ResourceController
+
+type reconciler struct {
+	ctx         context.Context
+	mgr         manager.Manager
+	name        string
+	namespance  string
+	onError     func(err error)
+	onReconcile func(jobList map[string][]Job)
+}
+
+// Job is a type alias for the k8s job definition.
+type Job = batchv1.Job
+
+// New returns the JobWatcher that implements reconciliation loop, or any error occurred.
+func New(opts ...Option) (JobWatcher, error) {
+	r := new(reconciler)
+
+	for _, opt := range append(defaultOpts, opts...) {
+		if err := opt(r); err != nil {
+			return nil, errors.ErrOptionFailed(err, reflect.ValueOf(opt))
+		}
+	}
+
+	return nil, nil
+}
+
+// Reconcile implements k8s reconciliation loop to retrieve the Job information from k8s.
+func (r *reconciler) Reconcile(req reconcile.Request) (res reconcile.Result, err error) {
+	js := new(batchv1.JobList)
+
+	err = r.mgr.GetClient().List(r.ctx, js)
+	if err != nil {
+		if r.onError != nil {
+			r.onError(err)
+		}
+		res = reconcile.Result{
+			Requeue:      true,
+			RequeueAfter: time.Millisecond * 100,
+		}
+		if k8serrors.IsNotFound(err) {
+			log.Error("not found", err)
+			return reconcile.Result{
+				Requeue:      true,
+				RequeueAfter: time.Second,
+			}, nil
+		}
+		return
+	}
+
+	jobs := make(map[string][]Job)
+
+	for _, job := range js.Items {
+		name, ok := job.GetObjectMeta().GetLabels()["app"]
+		if !ok {
+			jns := strings.Split(job.GetName(), "-")
+			name = strings.Join(jns[:len(jns)-1], "-")
+		}
+
+		if _, ok := jobs[name]; !ok {
+			jobs[name] = make([]Job, 0, len(js.Items))
+		}
+
+		jobs[name] = append(jobs[name], job)
+	}
+
+	for name := range jobs {
+		l := len(jobs[name])
+		jobs[name] = jobs[name][:l:l]
+	}
+
+	if r.onReconcile != nil {
+		r.onReconcile(jobs)
+	}
+	return
+}
+
+// GetName returns the name of resource controller.
+func (r *reconciler) GetName() string {
+	return r.name
+}
+
+// NewReconciler returns the reconciler for the Job.
+func (r *reconciler) NewReconciler(ctx context.Context, mgr manager.Manager) reconcile.Reconciler {
+	if r.ctx == nil && ctx != nil {
+		r.ctx = ctx
+	}
+	if r.mgr == nil && mgr != nil {
+		r.mgr = mgr
+	}
+
+	batchv1.AddToScheme(r.mgr.GetScheme())
+	return r
+}
+
+// For returns the runtime.Object which is job.
+func (r *reconciler) For() runtime.Object {
+	return new(batchv1.Job)
+}
+
+// Owns returns the owner of the job watcher.
+// It will always return nil.
+func (r *reconciler) Owns() runtime.Object {
+	return nil
+}
+
+// Watches returns the kind of the job and the event handler.
+// It will always return nil.
+func (r *reconciler) Watches() (*source.Kind, handler.EventHandler) {
+	// return &source.Kind{Type: new(corev1.Pod)}, &handler.EnqueueRequestForObject{}
+	return nil, nil
+}
