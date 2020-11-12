45d3b9b18c7ff3efc29b5e89ed7eb690c1d9858f
vankichi <kyukawa315@gmail.com>
2020-11-12 11:14:27 +0900

    :sparkles: implement statefulset reconciler for rebalance
    Signed-off-by: vankichi <kyukawa315@gmail.com>

---
+++ internal/k8s/statefulset/option.go
@@ -0,0 +1,48 @@
+package statefulset
+
+import "sigs.k8s.io/controller-runtime/pkg/manager"
+
+// Option represents functional option for reconsile StatefulSet.
+type Option func(*reconciler) error
+
+var defaultOpts = []Option{}
+
+// WithControllerName returns Option that sets r.name.
+func WithControllerName(name string) Option {
+	return func(r *reconciler) error {
+		r.name = name
+		return nil
+	}
+}
+
+// WithManager returns Option that sets r.mgr.
+func WithManager(mgr manager.Manager) Option {
+	return func(r *reconciler) error {
+		r.mgr = mgr
+		return nil
+	}
+}
+
+// WithNamaspace returns Option that sets r.namaspace.
+func WithNamespace(ns string) Option {
+	return func(r *reconciler) error {
+		r.namespace = ns
+		return nil
+	}
+}
+
+// WithOnErrorFunc returns Option that sets r.onError.
+func WithOnErrorFunc(f func(err error)) Option {
+	return func(r *reconciler) error {
+		r.onError = f
+		return nil
+	}
+}
+
+// WithOnReconcileFunc returns Option that sets r.onReconcile.
+func WithOnReconcileFunc(f func(statefulSetList map[string][]StatefulSet)) Option {
+	return func(r *reconciler) error {
+		r.onReconcile = f
+		return nil
+	}
+}
