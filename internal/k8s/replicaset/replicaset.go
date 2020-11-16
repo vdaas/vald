aaf9f202487e1301ca99d592c8b9482870026986
kevindiu <kevin_diu@yahoo.com.hk>
2020-11-16 14:35:36 +0900

    add K8sRecourseNotFound error

--- internal/k8s/replicaset/replicaset.go
+++ internal/k8s/replicaset/replicaset.go
@@ -76,7 +76,7 @@ func (r *reconciler) Reconcile(req reconcile.Request) (res reconcile.Result, err
 			RequeueAfter: time.Millisecond * 100,
 		}
 		if k8serrors.IsNotFound(err) {
-			log.Error("not found", err)
+			log.Warn(errors.ErrK8sResourceNotFound(err))
 			res.RequeueAfter = time.Second
 			return res, nil
 		}
