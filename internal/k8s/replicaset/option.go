9878bf0f02603a948e4c024d6c0ef688a81ee195
Kevin Diu <kevindiujp@gmail.com>
2020-11-18 17:53:10 +0900

    :sparkles: add replicaset reconciler (#823)
    * add replica set reconciler
    
    * use appv1 definition
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * add comment
    
    * Apply suggestions from code review
    
    Co-authored-by: Hiroto Funakoshi <hiroto.funakoshi.hiroto@gmail.com>
    
    * Apply suggestions from code review
    
    Co-authored-by: Hiroto Funakoshi <hiroto.funakoshi.hiroto@gmail.com>
    
    * fix package name, add namespace option, fix comment
    
    * Apply suggestions from code review
    
    Co-authored-by: Hiroto Funakoshi <hiroto.funakoshi.hiroto@gmail.com>
    
    * Apply suggestions from code review
    
    Co-authored-by: Kiichiro YUKAWA <kyukawa315@gmail.com>
    
    * :robot: Update license headers / Format go codes and yaml files
    
    Signed-off-by: vdaas-ci <ci@vdaas.org>
    
    * Apply suggestions from code review
    
    Co-authored-by: Kiichiro YUKAWA <kyukawa315@gmail.com>
    
    * fix
    
    * fix import
    
    * add K8sRecourseNotFound error
    
    * fix reconeile result buffer size
    
    * rename var
    
    * refactor
    
    * use sync.pool to store cache
    
    * remove unused var
    
    * fix: initialize position of sync pool
    
    Signed-off-by: hlts2 <hiroto.funakoshi.hiroto@gmail.com>
    
    Co-authored-by: Hiroto Funakoshi <hiroto.funakoshi.hiroto@gmail.com>
    Co-authored-by: Kiichiro YUKAWA <kyukawa315@gmail.com>
    Co-authored-by: vdaas-ci <ci@vdaas.org>

---
+++ internal/k8s/replicaset/option.go
@@ -0,0 +1,63 @@
+//
+// Copyright (C) 2019-2020 Vdaas.org Vald team ( kpango, rinx, kmrmt )
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+package replicaset
+
+import "sigs.k8s.io/controller-runtime/pkg/manager"
+
+// Option represents functional option for reconciler.
+type Option func(*reconciler) error
+
+var defaultOpts = []Option{}
+
+// WithControllerName returns Option to set the controller name.
+func WithControllerName(name string) Option {
+	return func(r *reconciler) error {
+		r.name = name
+		return nil
+	}
+}
+
+// WithNamespace returns Option to set the namespace.
+func WithNamespace(ns string) Option {
+	return func(r *reconciler) error {
+		r.namespace = ns
+		return nil
+	}
+}
+
+// WithManager returns Option to set the controller manager.
+func WithManager(mgr manager.Manager) Option {
+	return func(r *reconciler) error {
+		r.mgr = mgr
+		return nil
+	}
+}
+
+// WithOnErrorFunc returns Option to set the onError hook.
+func WithOnErrorFunc(f func(err error)) Option {
+	return func(r *reconciler) error {
+		r.onError = f
+		return nil
+	}
+}
+
+// WithOnReconcileFunc returns Option to set the onReconcile hook.
+func WithOnReconcileFunc(f func(replicasetList map[string][]ReplicaSet)) Option {
+	return func(r *reconciler) error {
+		r.onReconcile = f
+		return nil
+	}
+}
