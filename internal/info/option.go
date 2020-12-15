09e7cd0b007519fc6f603dcd61a746b5cbcfe9c5
kevindiu <kevin_diu@yahoo.com.hk>
2020-12-15 12:28:59 +0900

    add comments for option

--- internal/info/option.go
+++ internal/info/option.go
@@ -1,7 +1,12 @@
 package info
 
-import "runtime"
+import (
+	"runtime"
 
+	"github.com/vdaas/vald/internal/errors"
+)
+
+// Option represent the functional option for info.
 type Option func(i *info) error
 
 var (
@@ -11,22 +16,34 @@ var (
 	}
 )
 
+// WithServerName returns the option to set the server name.
 func WithServerName(s string) Option {
 	return func(i *info) error {
+		if len(s) == 0 {
+			return errors.NewErrInvalidOption("ServerName", s)
+		}
 		i.detail.ServerName = s
 		return nil
 	}
 }
 
+// WithRuntimeCaller returns the option to set the runtime Caller function.
 func WithRuntimeCaller(f func(skip int) (pc uintptr, file string, line int, ok bool)) Option {
 	return func(i *info) error {
+		if f == nil {
+			return errors.NewErrInvalidOption("RuntimeCaller", f)
+		}
 		i.rtCaller = f
 		return nil
 	}
 }
 
+// WithRuntimeFuncForPC returns the option to set the runtime FuncForPC function.
 func WithRuntimeFuncForPC(f func(pc uintptr) *runtime.Func) Option {
 	return func(i *info) error {
+		if f == nil {
+			return errors.NewErrInvalidOption("RuntimeFuncForPC", f)
+		}
 		i.rtFuncForPC = f
 		return nil
 	}
--- internal/info/option_test.go
+++ internal/info/option_test.go
@@ -3,64 +3,72 @@ package info
 import (
 	"reflect"
 	"runtime"
+	"sync"
 	"testing"
 
-	"github.com/pkg/errors"
+	"github.com/vdaas/vald/internal/errors"
+	"github.com/vdaas/vald/internal/test/comparator"
 	"go.uber.org/goleak"
 )
 
 func TestWithServerName(t *testing.T) {
+	type T = info
 	type args struct {
 		s string
 	}
 	type want struct {
-		want Option
+		obj *T
+		err error
 	}
 	type test struct {
 		name       string
 		args       args
 		want       want
-		checkFunc  func(want, Option) error
+		checkFunc  func(want, *T, error) error
 		beforeFunc func(args)
 		afterFunc  func(args)
 	}
-	defaultCheckFunc := func(w want, got Option) error {
-		if !reflect.DeepEqual(got, w.want) {
-			return errors.Errorf("got: \"%#v\",\n\t\t\t\twant: \"%#v\"", got, w.want)
+
+	defaultCheckFunc := func(w want, obj *T, err error) error {
+		if !errors.Is(err, w.err) {
+			return errors.Errorf("got_error: \"%#v\",\n\t\t\t\twant: \"%#v\"", err, w.err)
+		}
+		if !reflect.DeepEqual(obj, w.obj) {
+			return errors.Errorf("got: \"%#v\",\n\t\t\t\twant: \"%#v\"", obj, w.obj)
 		}
 		return nil
 	}
 	tests := []test{
-		// TODO test cases
-		/*
-		   {
-		       name: "test_case_1",
-		       args: args {
-		           s: "",
-		       },
-		       want: want{},
-		       checkFunc: defaultCheckFunc,
-		   },
-		*/
-
-		// TODO test cases
-		/*
-		   func() test {
-		       return test {
-		           name: "test_case_2",
-		           args: args {
-		           s: "",
-		           },
-		           want: want{},
-		           checkFunc: defaultCheckFunc,
-		       }
-		   }(),
-		*/
+		{
+			name: "set server name success",
+			args: args{
+				s: "srvName",
+			},
+			want: want{
+				obj: &T{
+					detail: Detail{
+						ServerName: "srvName",
+					},
+				},
+			},
+		},
+		{
+			name: "return error when server name is empty",
+			args: args{
+				s: "",
+			},
+			want: want{
+				obj: &T{
+					detail: Detail{},
+				},
+				err: errors.NewErrInvalidOption("ServerName", ""),
+			},
+		},
 	}
 
 	for _, test := range tests {
 		t.Run(test.name, func(tt *testing.T) {
-			defer goleak.VerifyNone(tt)
+			defer goleak.VerifyNone(tt, goleakIgnoreOptions...)
 			if test.beforeFunc != nil {
 				test.beforeFunc(test.args)
 			}
@@ -72,66 +80,89 @@ func TestWithServerName(t *testing.T) {
 			}
 
 			got := WithServerName(test.args.s)
-			if err := test.checkFunc(test.want, got); err != nil {
+			obj := new(T)
+			if err := test.checkFunc(test.want, obj, got(obj)); err != nil {
 				tt.Errorf("error = %v", err)
 			}
-
 		})
 	}
 }
 
 func TestWithRuntimeCaller(t *testing.T) {
+	type T = info
 	type args struct {
 		f func(skip int) (pc uintptr, file string, line int, ok bool)
 	}
 	type want struct {
-		want Option
+		obj *T
+		err error
 	}
 	type test struct {
 		name       string
 		args       args
 		want       want
-		checkFunc  func(want, Option) error
+		checkFunc  func(want, *T, error) error
 		beforeFunc func(args)
 		afterFunc  func(args)
 	}
-	defaultCheckFunc := func(w want, got Option) error {
-		if !reflect.DeepEqual(got, w.want) {
-			return errors.Errorf("got: \"%#v\",\n\t\t\t\twant: \"%#v\"", got, w.want)
+
+	defaultCheckFunc := func(w want, obj *T, err error) error {
+		if !errors.Is(err, w.err) {
+			return errors.Errorf("got_error: \"%#v\",\n\t\t\t\twant: \"%#v\"", err, w.err)
+		}
+
+		opts := []comparator.Option{
+			comparator.AllowUnexported(info{}),
+			comparator.Comparer(func(x, y sync.Once) bool {
+				return reflect.DeepEqual(x, y)
+			}),
+			comparator.Comparer(func(x, y func(skip int) (pc uintptr, file string, line int, ok bool)) bool {
+				return reflect.ValueOf(x).Pointer() == reflect.ValueOf(y).Pointer()
+			}),
+			comparator.Comparer(func(x, y func(pc uintptr) *runtime.Func) bool {
+				return reflect.ValueOf(x).Pointer() == reflect.ValueOf(y).Pointer()
+			}),
+		}
+		if diff := comparator.Diff(w.obj, obj, opts...); len(diff) != 0 {
+			return errors.Errorf("err: %s", diff)
 		}
 		return nil
 	}
 	tests := []test{
-		// TODO test cases
-		/*
-		   {
-		       name: "test_case_1",
-		       args: args {
-		           f: nil,
-		       },
-		       want: want{},
-		       checkFunc: defaultCheckFunc,
-		   },
-		*/
-
-		// TODO test cases
-		/*
-		   func() test {
-		       return test {
-		           name: "test_case_2",
-		           args: args {
-		           f: nil,
-		           },
-		           want: want{},
-		           checkFunc: defaultCheckFunc,
-		       }
-		   }(),
-		*/
+		func() test {
+			f := func(skip int) (pc uintptr, file string, line int, ok bool) {
+				return uintptr(0), "", 0, false
+			}
+			return test{
+				name: "set func success",
+				args: args{
+					f: f,
+				},
+				want: want{
+					obj: &T{
+						rtCaller: f,
+						detail:   Detail{},
+					},
+				},
+			}
+		}(),
+		{
+			name: "return error when func is nil",
+			args: args{
+				f: nil,
+			},
+			want: want{
+				obj: &T{
+					detail: Detail{},
+				},
+				err: errors.NewErrInvalidOption("RuntimeCaller", nil),
+			},
+		},
 	}
 
 	for _, test := range tests {
 		t.Run(test.name, func(tt *testing.T) {
-			defer goleak.VerifyNone(tt)
+			defer goleak.VerifyNone(tt, goleakIgnoreOptions...)
 			if test.beforeFunc != nil {
 				test.beforeFunc(test.args)
 			}
@@ -143,66 +174,89 @@ func TestWithRuntimeCaller(t *testing.T) {
 			}
 
 			got := WithRuntimeCaller(test.args.f)
-			if err := test.checkFunc(test.want, got); err != nil {
+			obj := new(T)
+			if err := test.checkFunc(test.want, obj, got(obj)); err != nil {
 				tt.Errorf("error = %v", err)
 			}
-
 		})
 	}
 }
 
 func TestWithRuntimeFuncForPC(t *testing.T) {
+	type T = info
 	type args struct {
 		f func(pc uintptr) *runtime.Func
 	}
 	type want struct {
-		want Option
+		obj *T
+		err error
 	}
 	type test struct {
 		name       string
 		args       args
 		want       want
-		checkFunc  func(want, Option) error
+		checkFunc  func(want, *T, error) error
 		beforeFunc func(args)
 		afterFunc  func(args)
 	}
-	defaultCheckFunc := func(w want, got Option) error {
-		if !reflect.DeepEqual(got, w.want) {
-			return errors.Errorf("got: \"%#v\",\n\t\t\t\twant: \"%#v\"", got, w.want)
+
+	defaultCheckFunc := func(w want, obj *T, err error) error {
+		if !errors.Is(err, w.err) {
+			return errors.Errorf("got_error: \"%#v\",\n\t\t\t\twant: \"%#v\"", err, w.err)
+		}
+
+		opts := []comparator.Option{
+			comparator.AllowUnexported(info{}),
+			comparator.Comparer(func(x, y sync.Once) bool {
+				return reflect.DeepEqual(x, y)
+			}),
+			comparator.Comparer(func(x, y func(skip int) (pc uintptr, file string, line int, ok bool)) bool {
+				return reflect.ValueOf(x).Pointer() == reflect.ValueOf(y).Pointer()
+			}),
+			comparator.Comparer(func(x, y func(pc uintptr) *runtime.Func) bool {
+				return reflect.ValueOf(x).Pointer() == reflect.ValueOf(y).Pointer()
+			}),
+		}
+		if diff := comparator.Diff(w.obj, obj, opts...); len(diff) != 0 {
+			return errors.Errorf("err: %s", diff)
 		}
 		return nil
 	}
 	tests := []test{
-		// TODO test cases
-		/*
-		   {
-		       name: "test_case_1",
-		       args: args {
-		           f: nil,
-		       },
-		       want: want{},
-		       checkFunc: defaultCheckFunc,
-		   },
-		*/
-
-		// TODO test cases
-		/*
-		   func() test {
-		       return test {
-		           name: "test_case_2",
-		           args: args {
-		           f: nil,
-		           },
-		           want: want{},
-		           checkFunc: defaultCheckFunc,
-		       }
-		   }(),
-		*/
+		func() test {
+			f := func(pc uintptr) *runtime.Func {
+				return nil
+			}
+			return test{
+				name: "set func success",
+				args: args{
+					f: f,
+				},
+				want: want{
+					obj: &T{
+						rtFuncForPC: f,
+						detail:      Detail{},
+					},
+				},
+			}
+		}(),
+		{
+			name: "return error when func is nil",
+			args: args{
+				f: nil,
+			},
+			want: want{
+				obj: &T{
+					detail: Detail{},
+				},
+				err: errors.NewErrInvalidOption("RuntimeFuncForPC", nil),
+			},
+		},
 	}
 
 	for _, test := range tests {
 		t.Run(test.name, func(tt *testing.T) {
-			defer goleak.VerifyNone(tt)
+			defer goleak.VerifyNone(tt, goleakIgnoreOptions...)
 			if test.beforeFunc != nil {
 				test.beforeFunc(test.args)
 			}
@@ -214,10 +268,10 @@ func TestWithRuntimeFuncForPC(t *testing.T) {
 			}
 
 			got := WithRuntimeFuncForPC(test.args.f)
-			if err := test.checkFunc(test.want, got); err != nil {
+			obj := new(T)
+			if err := test.checkFunc(test.want, obj, got(obj)); err != nil {
 				tt.Errorf("error = %v", err)
 			}
-
 		})
 	}
 }
