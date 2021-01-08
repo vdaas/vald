0e93f6b64aa1821f8879e72cb3248e2a761546bf
Hiroto Funakoshi <hiroto.funakoshi.hiroto@gmail.com>
2021-01-08 11:00:48 +0900

    Add test case for internal/errors/grpc.go (#903)
    * fix: test case name for grpc error
    
    Signed-off-by: hlts2 <hiroto.funakoshi.hiroto@gmail.com>
    
    * fix: add comment & test case name more clearly
    
    Signed-off-by: hlts2 <hiroto.funakoshi.hiroto@gmail.com>
    
    * fix: add comment & test case for ErrInvalidGRPCPort method
    
    Signed-off-by: hlts2 <hiroto.funakoshi.hiroto@gmail.com>
    
    * fix: apply suggestion
    
    Signed-off-by: hlts2 <hiroto.funakoshi.hiroto@gmail.com>
    
    * Update internal/errors/grpc.go
    
    * Update internal/errors/grpc.go
    
    * Apply suggestions from code review
    
    Co-authored-by: Kiichiro YUKAWA <kyukawa315@gmail.com>
    
    * fix: apply suggestion about boundary value
    
    Signed-off-by: hlts2 <hiroto.funakoshi.hiroto@gmail.com>
    
    * :robot: Update license headers / Format go codes and yaml files
    
    Signed-off-by: vdaas-ci <ci@vdaas.org>
    
    Co-authored-by: Kiichiro YUKAWA <kyukawa315@gmail.com>
    Co-authored-by: vdaas-ci <ci@vdaas.org>

--- internal/errors/grpc.go
+++ internal/errors/grpc.go
@@ -19,33 +19,39 @@ package errors
 
 var (
 
-	// gRPC
-
-	ErrgRPCClientConnectionClose = func(name string, err error) error {
+	// ErrGRPCClientConnectionClose represents a function to generate an error that the gRPC connection couldn't close.
+	ErrGRPCClientConnectionClose = func(name string, err error) error {
 		return Wrapf(err, "%s's gRPC connection close error", name)
 	}
 
+	// ErrInvalidGRPCPort represents a function to generate an error that the gRPC port is invalid.
 	ErrInvalidGRPCPort = func(addr, host string, port uint16) error {
 		return Errorf("invalid gRPC client connection port to addr: %s,\thost: %s\t port: %d", addr, host, port)
 	}
 
+	// ErrInvalidGRPCClientConn represents a function to generate an error that the vald internal gRPC connection is invalid.
 	ErrInvalidGRPCClientConn = func(addr string) error {
 		return Errorf("invalid gRPC client connection to %s", addr)
 	}
 
+	// ErrGRPCLookupIPAddrNotFound represents a function to generate an error that the vald internal gRPC client couldn't find IP address.
 	ErrGRPCLookupIPAddrNotFound = func(host string) error {
 		return Errorf("vald internal gRPC client could not find ip addrs for %s", host)
 	}
 
+	// ErrGRPCClientNotFound represents an error that the vald internal gRPC client couldn't find.
 	ErrGRPCClientNotFound = New("vald internal gRPC client not found")
 
+	// ErrGRPCClientConnNotFound represents a function to generate an error that the gRPC client connection couldn't find.
 	ErrGRPCClientConnNotFound = func(addr string) error {
 		return Errorf("gRPC client connection not found in %s", addr)
 	}
 
+	// ErrRPCCallFailed represents a function to generate an error that the RPC call failed.
 	ErrRPCCallFailed = func(addr string, err error) error {
 		return Wrapf(err, "addr: %s", addr)
 	}
 
+	// ErrGRPCTargetAddrNotFound represents an error that the gRPC target address couldn't find.
 	ErrGRPCTargetAddrNotFound = New("grpc connection target not found")
 )
