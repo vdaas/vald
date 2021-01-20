865ceaac75ccd21002ed3a03b65de3a5e91c5ae9
Kevin Diu <kevindiujp@gmail.com>
2021-01-20 12:39:52 +0900

    Update internal/errors/info.go
    Co-authored-by: Hiroto Funakoshi <hiroto.funakoshi.hiroto@gmail.com>

--- internal/errors/info.go
+++ internal/errors/info.go
@@ -18,6 +18,6 @@ package errors
 var (
 	// ErrFailedToInitInfo represents an error to initialize info.
 	ErrFailedToInitInfo = func(err error) error {
-		return Wrapf(err, "failed to init info")
+		return Wrap(err, "failed to init info")
 	}
 )
