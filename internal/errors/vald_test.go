fcd511153bad785a0e41b1a635c77a75be17b940
vdaas-ci <ci@vdaas.org>
2021-01-29 04:44:10 +0000

    :robot: Update license headers / Format go codes and yaml files
    Signed-off-by: vdaas-ci <ci@vdaas.org>

--- internal/errors/vald_test.go
+++ internal/errors/vald_test.go
@@ -1,3 +1,18 @@
+//
+// Copyright (C) 2019-2021 vdaas.org vald team <vald@vdaas.org>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
 package errors
 
 import (
@@ -12,13 +27,13 @@ func TestErrMetaDataAlreadyExists(t *testing.T) {
 	type want struct {
 		want error
 	}
-	type test struct{
-		name string
-		args args
-		want want
-		checkFunc func(want, error) error
+	type test struct {
+		name       string
+		args       args
+		want       want
+		checkFunc  func(want, error) error
 		beforeFunc func(args)
-		afterFunc func(args)
+		afterFunc  func(args)
 	}
 	defaultCheckFunc := func(w want, got error) error {
 		if !Is(got, w.want) {
@@ -45,7 +60,7 @@ func TestErrMetaDataAlreadyExists(t *testing.T) {
 			},
 		},
 	}
-	
+
 	for _, test := range tests {
 		t.Run(test.name, func(tt *testing.T) {
 			tt.Parallel()
@@ -73,12 +88,12 @@ func TestErrMetadataCannotFetch(t *testing.T) {
 	type want struct {
 		want error
 	}
-	type test struct{
-		name string
-		want want
-		checkFunc func(want, error) error
+	type test struct {
+		name       string
+		want       want
+		checkFunc  func(want, error) error
 		beforeFunc func()
-		afterFunc func()
+		afterFunc  func()
 	}
 	defaultCheckFunc := func(w want, got error) error {
 		if !Is(got, w.want) {
@@ -95,7 +110,7 @@ func TestErrMetadataCannotFetch(t *testing.T) {
 			},
 		},
 	}
-	
+
 	for _, test := range tests {
 		t.Run(test.name, func(tt *testing.T) {
 			tt.Parallel()
