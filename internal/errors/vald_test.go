7e1997a04e0599664794f0cc0354ca0a6012d498
Kiichiro YUKAWA <kyukawa315@gmail.com>
2021-02-01 18:43:00 +0900

    add test for internal/errors/vald (#958)
    * :white_check_mark: add test for internal/errors/vald
    
    Signed-off-by: vankichi <kyukawa315@gmail.com>
    
    Co-authored-by: vdaas-ci <ci@vdaas.org>
    Co-authored-by: Kevin Diu <kevindiujp@gmail.com>

---
+++ internal/errors/vald_test.go
@@ -0,0 +1,132 @@
+//
+// Copyright (C) 2019-2021 vdaas.org vald team <vald@vdaas.org>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+package errors
+
+import (
+	"testing"
+)
+
+func TestErrMetaDataAlreadyExists(t *testing.T) {
+	t.Parallel()
+	type args struct {
+		meta string
+	}
+	type want struct {
+		want error
+	}
+	type test struct {
+		name       string
+		args       args
+		want       want
+		checkFunc  func(want, error) error
+		beforeFunc func(args)
+		afterFunc  func(args)
+	}
+	defaultCheckFunc := func(w want, got error) error {
+		if !Is(got, w.want) {
+			return Errorf("got: \"%#v\",\n\t\t\t\twant: \"%#v\"", got, w.want)
+		}
+		return nil
+	}
+
+	tests := []test{
+		{
+			name: "return an ErrMetaDataAlreadyExists error when meta is not empty.",
+			args: args{
+				meta: "vald-meta-01",
+			},
+			want: want{
+				want: New("vald metadata:\tvald-meta-01\talready exists "),
+			},
+		},
+		{
+			name: "return an ErrMetaDataAlreadyExists error when meta is empty.",
+			args: args{},
+			want: want{
+				want: New("vald metadata:\t\talready exists "),
+			},
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(tt *testing.T) {
+			tt.Parallel()
+			if test.beforeFunc != nil {
+				test.beforeFunc(test.args)
+			}
+			if test.afterFunc != nil {
+				defer test.afterFunc(test.args)
+			}
+			if test.checkFunc == nil {
+				test.checkFunc = defaultCheckFunc
+			}
+
+			got := ErrMetaDataAlreadyExists(test.args.meta)
+			if err := test.checkFunc(test.want, got); err != nil {
+				tt.Errorf("error = %v", err)
+			}
+		})
+	}
+}
+
+func TestErrMetadataCannotFetch(t *testing.T) {
+	t.Parallel()
+	type want struct {
+		want error
+	}
+	type test struct {
+		name       string
+		want       want
+		checkFunc  func(want, error) error
+		beforeFunc func()
+		afterFunc  func()
+	}
+	defaultCheckFunc := func(w want, got error) error {
+		if !Is(got, w.want) {
+			return Errorf("got: \"%#v\",\n\t\t\t\twant: \"%#v\"", got, w.want)
+		}
+		return nil
+	}
+
+	tests := []test{
+		{
+			name: "return an ErrMetaDataCannotFetch error",
+			want: want{
+				want: New("vald metadata cannot fetch"),
+			},
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(tt *testing.T) {
+			tt.Parallel()
+			if test.beforeFunc != nil {
+				test.beforeFunc()
+			}
+			if test.afterFunc != nil {
+				defer test.afterFunc()
+			}
+			if test.checkFunc == nil {
+				test.checkFunc = defaultCheckFunc
+			}
+
+			got := ErrMetaDataCannotFetch()
+			if err := test.checkFunc(test.want, got); err != nil {
+				tt.Errorf("error = %v", err)
+			}
+		})
+	}
+}
