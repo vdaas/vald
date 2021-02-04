b58d27afb352f7da88153e2671636f0d2d6a92ba
vdaas-ci <ci@vdaas.org>
2021-02-04 11:19:02 +0000

    :robot: Update license headers / Format go codes and yaml files
    Signed-off-by: vdaas-ci <ci@vdaas.org>

--- internal/errors/runtime_test.go
+++ internal/errors/runtime_test.go
@@ -1,7 +1,7 @@
 //
 // Copyright (C) 2019-2021 vdaas.org vald team <vald@vdaas.org>
 //
-// Licensed under the Apache LicensdefaultErr, Version 2.0 (the "License");
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
