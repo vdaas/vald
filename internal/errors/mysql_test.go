4247b37228eccd866f982e1b7361dc6cec867b03
vankichi <kyukawa315@gmail.com>
2021-02-09 18:51:20 +0900

    :white_check_mark: :recycle: add new defined errror for MySQL
    Signed-off-by: vankichi <kyukawa315@gmail.com>

--- internal/errors/mysql_test.go
+++ internal/errors/mysql_test.go
@@ -411,6 +411,55 @@ func TestErrRequiredMemberNotFilled(t *testing.T) {
 	}
 }
 
+func TestErrMySQLSessionNil(t *testing.T) {
+	t.Parallel()
+	type want struct {
+		want error
+	}
+	type test struct {
+		name       string
+		want       want
+		checkFunc  func(want, error) error
+		beforeFunc func()
+		afterFunc  func()
+	}
+	defaultCheckFunc := func(w want, got error) error {
+		if !Is(got, w.want) {
+			return Errorf("got: \"%#v\",\n\t\t\t\twant: \"%#v\"", got, w.want)
+		}
+		return nil
+	}
+	tests := []test{
+		{
+			name: "return an ErrMySQLSessionNil error",
+			want: want{
+				want: New("error MySQL session is nil"),
+			},
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(tt *testing.T) {
+			tt.Parallel()
+			defer goleak.VerifyNone(tt, goleak.IgnoreCurrent())
+			if test.beforeFunc != nil {
+				test.beforeFunc()
+			}
+			if test.afterFunc != nil {
+				defer test.afterFunc()
+			}
+			if test.checkFunc == nil {
+				test.checkFunc = defaultCheckFunc
+			}
+
+			got := ErrMySQLSessionNil()
+			if err := test.checkFunc(test.want, got); err != nil {
+				tt.Errorf("error = %v", err)
+			}
+		})
+	}
+}
+
 func TestErrMySQLNotFoundIdentity_Error(t *testing.T) {
 	t.Parallel()
 	type fields struct {
