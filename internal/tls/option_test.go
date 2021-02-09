f2385eea0185123da2bd024f13f15a6e8dbed154
Yusuke Kato <kpango@vdaas.org>
2021-02-09 18:40:38 +0900

    Update internal/tls/option_test.go
    Co-authored-by: Kiichiro YUKAWA <kyukawa315@gmail.com>

--- internal/tls/option_test.go
+++ internal/tls/option_test.go
@@ -378,7 +378,11 @@ func TestWithInsecureSkipVerify(t *testing.T) {
 		},
 	}
 
-	for _, test := range tests {
+	for _, tc := range tests {
+		test := tc
+		t.Run(tt, func() {
+			tt.Parallel()
+		})
 		t.Run(test.name, func(tt *testing.T) {
 			defer goleak.VerifyNone(tt, goleakIgnoreOptions...)
 			if test.beforeFunc != nil {
