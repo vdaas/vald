1afeb052d60f50e0fd92dbbfd67d40e6a2d3d3bd
Yusuke Kato <kpango@vdaas.org>
2021-03-02 19:41:46 +0900

    refactor discoverer client (#1056)
    Signed-off-by: kpango <kpango@vdaas.org>

--- internal/net/grpc/client.go
+++ internal/net/grpc/client.go
@@ -573,12 +573,7 @@ func (g *gRPCClient) Connect(ctx context.Context, addr string, dopts ...DialOpti
 				g.atomicAddrs.Add(addr)
 				return conn, nil
 			}
-			log.Warnf("failed to reconnect unhealthy pool addr= %s\terror= %s\t trying to disconnect", addr, func() string {
-				if err != nil {
-					return err.Error()
-				}
-				return "connection response is nil or pool is nil"
-			}())
+			log.Warnf("failed to reconnect unhealthy pool addr= %s\tconn= %v\terror= %v\t trying to disconnect", addr, conn, err)
 			err = g.Disconnect(ctx, addr)
 			if err != nil {
 				log.Warnf("failed to disconnect unhealthy pool addr= %s\terror= %s", addr, err.Error())
