360222db05ebec740ff01846015a611f6bc94ccd
Kiichiro YUKAWA <kyukawa315@gmail.com>
2021-03-23 11:14:42 +0900

    Update internal/rand/rand_bench_test.go
    Co-authored-by: Kevin Diu <kevindiujp@gmail.com>

--- internal/rand/rand_bench_test.go
+++ internal/rand/rand_bench_test.go
@@ -46,6 +46,7 @@ func BenchmarkLimitedUint32_10(b *testing.B) {
 }
 
 func BenchmarkLimitedUint32_100(b *testing.B) {
+	b.ReportAllocs()
 	b.RunParallel(func(pb *testing.PB) {
 		for pb.Next() {
 			LimitedUint32(100)
