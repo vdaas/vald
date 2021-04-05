776f612e3297cf05e407a81c4fda26384009be33
hlts2 <hiroto.funakoshi.hiroto@gmail.com>
2021-04-05 12:21:55 +0900

    feat: add godoc comment
    Signed-off-by: hlts2 <hiroto.funakoshi.hiroto@gmail.com>

--- internal/config/mysql.go
+++ internal/config/mysql.go
@@ -73,6 +73,8 @@ func (m *MySQL) Bind() *MySQL {
 	return m
 }
 
+// Opts creates and returns the slice with the functional options for the internal mysql package.
+// In addition, Opts returns the error when the any errors are occurred.
 func (m *MySQL) Opts() ([]mysql.Option, error) {
 	nt := net.NetworkTypeFromString(m.Network)
 	if nt == 0 || nt == net.Unknown || strings.EqualFold(nt.String(), net.Unknown.String()) {
