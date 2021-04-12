fabddf746ce1b06d19e626475f1bbe92d63aff6c
Hiroto Funakoshi <hiroto.funakoshi.hiroto@gmail.com>
2021-04-12 14:17:25 +0900

    Update internal/config/sidecar_test.go

--- internal/config/sidecar_test.go
+++ internal/config/sidecar_test.go
@@ -122,7 +122,7 @@ func TestAgentSidecar_Bind(t *testing.T) {
 			filename := "vald-ngt-1"
 			filenameSuffix := "tar.gz"
 			return test{
-				name: "return AgentSidecar when all of object are set",
+				name: "return AgentSidecar when all of object are not set",
 				fields: fields{
 					Mode:               mode,
 					WatchDir:           watchDir,
