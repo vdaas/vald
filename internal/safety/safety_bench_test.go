a26d7e8b461bfcede170a927feda5a83ca6fa9e5
kevindiu <kevin_diu@yahoo.com.hk>
2021-04-14 12:10:26 +0900

    rename BenchmarkRecoverWithoutPanicFunc_with_execution and remove old BenchmarkRecoverWithoutPanicFunc
    Signed-off-by: kevindiu <kevin_diu@yahoo.com.hk>

--- internal/safety/safety_bench_test.go
+++ internal/safety/safety_bench_test.go
@@ -55,38 +55,6 @@ func BenchmarkRecoverWithoutPanicFunc(b *testing.B) {
 				},
 			},
 		},
-	}
-	for _, test := range tests {
-		b.Run(test.name, func(b *testing.B) {
-			b.RunParallel(func(pb *testing.PB) {
-				for pb.Next() {
-					got := RecoverWithoutPanicFunc(test.args.fn)
-					if got == nil {
-						b.Error("got is empty")
-					}
-				}
-			})
-		})
-	}
-}
-
-func BenchmarkRecoverWithoutPanicFunc_with_execution(b *testing.B) {
-	type args struct {
-		fn func() error
-	}
-	type test struct {
-		name string
-		args args
-	}
-	tests := []test{
-		{
-			name: "fn return error func",
-			args: args{
-				fn: func() error {
-					return errors.New("fn err")
-				},
-			},
-		},
 		{
 			name: "fn panic runtime error func",
 			args: args{
