a34fd2f2b2da039b967e27ab5780d05af73dcaef
kevindiu <kevin_diu@yahoo.com.hk>
2021-05-06 15:01:56 +0900

    implement pkg/agent/core/ngt/router test & refactor router implementation
    Signed-off-by: kevindiu <kevin_diu@yahoo.com.hk>

--- pkg/agent/core/ngt/router/router_test.go
+++ pkg/agent/core/ngt/router/router_test.go
@@ -19,10 +19,13 @@ package router
 
 import (
 	"net/http"
-	"reflect"
 	"testing"
 
+	"github.com/gorilla/mux"
+	"github.com/vdaas/vald/internal/errgroup"
 	"github.com/vdaas/vald/internal/errors"
+	"github.com/vdaas/vald/internal/net/http/routing"
+	"github.com/vdaas/vald/pkg/agent/core/ngt/handler/rest"
 	"go.uber.org/goleak"
 )
 
@@ -32,7 +35,7 @@ func TestNew(t *testing.T) {
 		opts []Option
 	}
 	type want struct {
-		want http.Handler
+		routes []routing.Route
 	}
 	type test struct {
 		name       string
@@ -43,37 +46,150 @@ func TestNew(t *testing.T) {
 		afterFunc  func(args)
 	}
 	defaultCheckFunc := func(w want, got http.Handler) error {
-		if !reflect.DeepEqual(got, w.want) {
-			return errors.Errorf("got: \"%#v\",\n\t\t\t\twant: \"%#v\"", got, w.want)
+		gh, ok := got.(*mux.Router)
+		if !ok {
+			return errors.New("type cast got failed")
+		}
+		for _, r := range w.routes {
+			gotR := gh.Get(r.Name)
+			if gotR == nil {
+				return errors.Errorf("route not found: %s", r.Name)
+			}
+
+			if gotR.GetHandler() == nil {
+				return errors.Errorf("handler not found: %s", r.Name)
+			}
+
+			gotP, err := gotR.GetPathRegexp()
+			if err != nil {
+				return err
+			}
+			if gotP == "" {
+				return errors.Errorf("pattern is empty: %s", r.Name)
+			}
 		}
 		return nil
 	}
 	tests := []test{
-		// TODO test cases
-		/*
-		   {
-		       name: "test_case_1",
-		       args: args {
-		           opts: nil,
-		       },
-		       want: want{},
-		       checkFunc: defaultCheckFunc,
-		   },
-		*/
+		func() test {
+			h := rest.New()
+			eg := errgroup.Get()
 
-		// TODO test cases
-		/*
-		   func() test {
-		       return test {
-		           name: "test_case_2",
-		           args: args {
-		           opts: nil,
-		           },
-		           want: want{},
-		           checkFunc: defaultCheckFunc,
-		       }
-		   }(),
-		*/
+			return test{
+				name: "return handlers",
+				args: args{
+					opts: []Option{
+						WithHandler(h),
+						WithErrGroup(eg),
+					},
+				},
+				want: want{
+					routes: []routing.Route{
+						{
+							"Index",
+							[]string{
+								http.MethodGet,
+							},
+							"/",
+							h.Index,
+						},
+						{
+							"Search",
+							[]string{
+								http.MethodPost,
+							},
+							"/search",
+							h.Search,
+						},
+						{
+							"Search By ID",
+							[]string{
+								http.MethodPost,
+							},
+							"/id/search",
+							h.SearchByID,
+						},
+						{
+							"Insert",
+							[]string{
+								http.MethodPost,
+							},
+							"/insert",
+							h.Insert,
+						},
+						{
+							"Multiple Insert",
+							[]string{
+								http.MethodPost,
+							},
+							"/insert/multi",
+							h.MultiInsert,
+						},
+						{
+							"Update",
+							[]string{
+								http.MethodPost,
+								http.MethodPatch,
+								http.MethodPut,
+							},
+							"/update",
+							h.Update,
+						},
+						{
+							"Multiple Update",
+							[]string{
+								http.MethodPost,
+								http.MethodPatch,
+								http.MethodPut,
+							},
+							"/update/multi",
+							h.MultiUpdate,
+						},
+						{
+							"Remove",
+							[]string{
+								http.MethodDelete,
+							},
+							"/delete",
+							h.Remove,
+						},
+						{
+							"Multiple Remove",
+							[]string{
+								http.MethodDelete,
+								http.MethodPost,
+							},
+							"/delete/multi",
+							h.MultiRemove,
+						},
+						{
+							"Create Index",
+							[]string{
+								http.MethodPost,
+							},
+							"/index/create",
+							h.CreateIndex,
+						},
+						{
+							"Save Index",
+							[]string{
+								http.MethodGet,
+							},
+							"/index/save",
+							h.SaveIndex,
+						},
+						{
+							"GetObject",
+							[]string{
+								http.MethodGet,
+							},
+							"/object/{id}",
+							h.GetObject,
+						},
+					},
+				},
+			}
+		}(),
 	}
 
 	for _, tc := range tests {
