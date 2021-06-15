4c2f2a0dc77a5e8c63eef7e25ba3d5c585019d6f
hlts2 <hiroto.funakoshi.hiroto@gmail.com>
2021-06-15 10:54:01 +0900

    fix condition of IVExists method
    Signed-off-by: hlts2 <hiroto.funakoshi.hiroto@gmail.com>

--- pkg/agent/core/ngt/service/vqueue/queue.go
+++ pkg/agent/core/ngt/service/vqueue/queue.go
@@ -260,7 +260,8 @@ func (v *vqueue) IVExists(uuid string) bool {
 		return true
 	}
 	// data exists both queue, compare data timestamp if insert queue timestamp is newer than delete one, this function returns exists(true)
-	return di < vec.date
+	// However, if insert and delete are sent by the update instruction, the timestamp will be the same
+	return di <= vec.date
 }
 
 func (v *vqueue) DVExists(uuid string) bool {
