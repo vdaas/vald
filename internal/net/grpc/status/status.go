7f4ab2192139c3f677db9d3b5c55dd455cfcf0ce
Yusuke Kato <kpango@vdaas.org>
2021-06-24 14:45:32 +0900

    bugfix correct error backoff handling for lb gateway (#1309)
    * bugfix correct error backoff handling for lb gateway
    
    Signed-off-by: kpango <kpango@vdaas.org>
    
    * fix review point and add status validation
    
    Signed-off-by: kpango <kpango@vdaas.org>

--- internal/net/grpc/status/status.go
+++ internal/net/grpc/status/status.go
@@ -117,6 +117,34 @@ func newStatus(code codes.Code, msg string, err error, details ...interface{}) (
 		log.Warn("failed to set error details:", err)
 	}
 
+	err = st.Err()
+	if err != nil {
+		switch st.Code() {
+		case codes.Internal,
+			codes.DataLoss:
+			log.Error(err.Error())
+		case codes.Unavailable,
+			codes.ResourceExhausted:
+			log.Warn(err.Error())
+		case codes.FailedPrecondition,
+			codes.InvalidArgument,
+			codes.OutOfRange,
+			codes.Unauthenticated,
+			codes.PermissionDenied,
+			codes.Unknown:
+			log.Debug(err.Error())
+		case codes.Aborted,
+			codes.Canceled,
+			codes.DeadlineExceeded,
+			codes.AlreadyExists,
+			codes.NotFound,
+			codes.OK,
+			codes.Unimplemented:
+		default:
+			log.Warn(err.Error())
+		}
+	}
+
 	return st
 }
 
@@ -208,35 +236,43 @@ func ParseError(err error, defaultCode codes.Code, defaultMsg string, details ..
 			defaultMsg = "failed to parse grpc status from error"
 		}
 		st = newStatus(defaultCode, defaultMsg, err, details...)
-		err = errors.Wrap(st.Err(), err.Error())
-		msg = err.Error()
-	} else {
-		pms := make([]proto.Message, 0, len(details))
-		for _, detail := range details {
-			pm, ok := detail.(proto.Message)
-			if ok {
-				pms = append(pms, pm)
-			}
-		}
-		sst, err := st.WithDetails(pms...)
-		if err == nil {
-			st = sst
-		}
-		err = st.Err()
-		if err == nil {
+		rerr = errors.Wrap(st.Err(), err.Error())
+		if st == nil || st.Message() == "" {
+			msg = rerr.Error()
+		} else {
 			msg = st.Message()
-		} else {
-			msg = st.Err().Error()
+		}
+		return st, msg, errors.Wrap(st.Err(), err.Error())
+	}
+
+	pms := make([]proto.Message, 0, len(details))
+	for _, detail := range details {
+		pm, ok := detail.(proto.Message)
+		if ok {
+			pms = append(pms, pm)
 		}
 	}
-	if err != nil {
-		if st.Code() == codes.Internal {
-			log.Error(err)
-		} else {
-			log.Warn(err)
+	sst, err := st.WithDetails(pms...)
+	if err == nil {
+		st = sst
+		err = st.Err()
+	} else {
+		log.Error(err)
+	}
+	err = st.Err()
+	if err == nil {
+		msg = st.Message()
+	} else {
+		msg = st.Err().Error()
+		rerr = err
+		switch st.Code() {
+		case codes.Internal:
+			log.Error(rerr.Error())
+		case codes.Unavailable,
+			codes.ResourceExhausted:
+			log.Warn(rerr.Error())
 		}
 	}
-	rerr = err
 	return st, msg, rerr
 }
 
