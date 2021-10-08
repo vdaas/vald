41bc7d724ce2328831a26cf12aa7db26d65be18f
kpango <kpango@vdaas.org>
2021-10-08 17:29:40 +0900

    fix
    Signed-off-by: kpango <kpango@vdaas.org>

--- Makefile.d/k8s.mk
+++ Makefile.d/k8s.mk
@@ -282,7 +282,7 @@ k8s/external/minio/deploy:
 	kubectl wait --for=condition=ready pod -l app=minio --timeout=600s
 	kubectl apply -f k8s/external/minio/mb-job.yaml
 	sleep $(K8S_SLEEP_DURATION_FOR_WAIT_COMMAND)
-	kubectl wait --for=condition=completed job/minio-make-bucket --timeout=600s
+	kubectl wait --for=condition=complete job/minio-make-bucket --timeout=600s
 
 .PHONY: k8s/external/minio/delete
 ## delete minio
