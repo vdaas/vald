04675c99d72763770d28a5e88e1f9b25a61338dd
kpango <kpango@vdaas.org>
2021-10-13 20:59:01 +0900

    fix
    Signed-off-by: kpango <kpango@vdaas.org>

--- internal/k8s/metrics/node/node.go
+++ internal/k8s/metrics/node/node.go
@@ -65,6 +65,32 @@ func (r *reconciler) addListOpts(opt client.ListOption) {
 	if opt == nil {
 		return
 	}
+	switch v := opt.(type) {
+	case client.MatchingFields:
+		if len(v) == 0 {
+			return
+		}
+	case *client.MatchingFields:
+		if len(*v) == 0 {
+			return
+		}
+	case client.MatchingLabels:
+		if len(v) == 0 {
+			return
+		}
+	case *client.MatchingLabels:
+		if len(*v) == 0 {
+			return
+		}
+	case client.InNamespace:
+		if len(v) == 0 {
+			return
+		}
+	case *client.InNamespace:
+		if len(*v) == 0 {
+			return
+		}
+	}
 	log.Debugf("%#v", opt)
 	if r.lopts == nil {
 		r.lopts = make([]client.ListOption, 0, 1)
