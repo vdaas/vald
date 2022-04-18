dadf400f9d0fa94df150dcb1f8e5a2bd53ffcda2
hlts2 <hiroto.funakoshi.hiroto@gmail.com>
2022-04-18 17:46:51 +0900

    tweak
    Signed-off-by: hlts2 <hiroto.funakoshi.hiroto@gmail.com>

--- internal/info/info.go
+++ internal/info/info.go
@@ -208,9 +208,9 @@ func (d Detail) String() string {
 						fileMaxLen = fl
 					}
 				}
-				urlFormat := fmt.Sprintf("%%-%ds\t%%-%ds\t%%s", urlMaxLen, fileMaxLen)
+				urlFormat := fmt.Sprintf("%%-%ds\t%%-%ds\t", urlMaxLen, fileMaxLen)
 				for i, st := range sts {
-					info[fmt.Sprintf("%s-%03d", tag, i)] = fmt.Sprintf(urlFormat, st.URL, (st.File + "#L" + strconv.Itoa(st.Line)), st.FuncName)
+					info[fmt.Sprintf("%s-%03d", tag, i)] = fmt.Sprintf(urlFormat, st.URL, (st.File+"#L"+strconv.Itoa(st.Line))) + st.FuncName
 				}
 			} else {
 				strs, ok := v.([]string)
