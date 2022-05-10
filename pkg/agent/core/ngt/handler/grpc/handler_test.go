8662ec807a48cfd25cb2db9ef7d461318f352911
kevindiu <kevindiujp@gmail.com>
2022-05-10 13:29:57 +0900

    change max dim
    Signed-off-by: kevindiu <kevindiujp@gmail.com>

--- pkg/agent/core/ngt/handler/grpc/handler_test.go
+++ pkg/agent/core/ngt/handler/grpc/handler_test.go
@@ -3664,7 +3664,7 @@ func Test_server_MultiInsert(t *testing.T) {
 		id        = "uuid-1"           // insert request id
 		intVecDim = 3                  // int vector dimension
 		f32VecDim = 3                  // float32 vector dimension
-		maxVecDim = math.MaxInt32 >> 7
+		maxVecDim = math.MaxInt32 >> 16
 	)
 	var (
 		ip = net.LoadLocalIP() // agent ip address
