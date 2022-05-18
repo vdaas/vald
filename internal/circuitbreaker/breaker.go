3633668b47cefe3002ee0052e321b9ce46a01eb3
hlts2 <hiroto.funakoshi.hiroto@gmail.com>
2022-05-19 03:40:55 +0900

    add comment
    Signed-off-by: hlts2 <hiroto.funakoshi.hiroto@gmail.com>

--- internal/circuitbreaker/breaker.go
+++ internal/circuitbreaker/breaker.go
@@ -6,23 +6,44 @@ import (
 	"time"
 )
 
+// counts holds the number of successes/failures.
 type counts struct {
 	consecutiveSuccesses uint32
 	consecutiveFailures  uint32
 }
 
 func (c *counts) onSuccess() (n uint32) {
+	// This function is called when user defined function succeds.
+
 	// 1. Increment consecutiveSuccesses by using atomic.
 	// 2. Clear consecutiveFailures by using atomic.
+
+	// e.g. the following is an example flow.
+	/**
+		n = atomic.AddUint32(&c.consecutiveSuccesses, 1)
+		atomic.StoreUint32(&c.consecutiveFailures, 0)
+	**/
 	return n
 }
 func (c *counts) onFailure() (n uint32) {
+	// This function is called when user defined function fails.
+
 	// 1. Increment consecutiveFailures by using atomic.
 	// 2. Clear consecutiveFailures by using atomic.
+
+	// e.g. the following is an example flow.
+	/**
+		n = atomic.AddUint32(&c.consecutiveFailures, 1)
+		atomic.StoreUint32(&c.consecutiveSuccesses, 0)
+	**/
 	return n
 }
 func (c *counts) reset() {
 	// 1. Clear consecutiveSuccesses and consecutiveFailures by using atomic
+
+	// e.g. the following is an example flow.
+	atomic.StoreUint32(&c.consecutiveFailures, 0)
+	atomic.StoreUint32(&c.consecutiveSuccesses, 0)
 }
 
 type breaker struct {
@@ -124,7 +145,7 @@ func (b *breaker) fail() {
 		    }
 		case stateHalfOpen:
 	        if b.st.CompareAndSwap(stateHalfOpen, stateOpen) {
-		        b.reset() // Reset all counter
+		        b.reset(time.Now()) // Reset all counter
 		    } else {
 		        // The state has been changed to "Closed" during this function calling.
 		    }
