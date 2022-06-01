15ea532137ecae182da0c3d27b635c3c1cc31fde
kevindiu <kevindiujp@gmail.com>
2022-06-01 11:40:24 +0900

    format code
    Signed-off-by: kevindiu <kevindiujp@gmail.com>

--- internal/test/data/request/doc.go
+++ internal/test/data/request/doc.go
@@ -13,25 +13,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 //
+
+// Package request provides functions to generate proto request for testing
 package request
-
-import (
-	"strconv"
-
-	"github.com/vdaas/vald/apis/grpc/v1/payload"
-)
-
-func GenObjectLocations(num int, name string, ip string) *payload.Object_Locations {
-	result := &payload.Object_Locations{
-		Locations: make([]*payload.Object_Location, num),
-	}
-
-	for i := 0; i < num; i++ {
-		result.Locations[i] = &payload.Object_Location{
-			Name: name,
-			Uuid: "uuid-" + strconv.Itoa(i+1),
-			Ips:  []string{ip},
-		}
-	}
-	return result
-}
