33ce2f09aa2a795b4b61da163d254e0a685f2293
Kosuke Morimoto <ksk@vdaas.org>
2022-06-08 01:59:31 +0000

    add drafts of upsert test cases
    Signed-off-by: Kosuke Morimoto <ksk@vdaas.org>

--- pkg/agent/core/ngt/handler/grpc/upsert_test.go
+++ pkg/agent/core/ngt/handler/grpc/upsert_test.go
@@ -168,6 +168,95 @@ func Test_server_StreamUpsert(t *testing.T) {
 		}
 		return nil
 	}
+
+	/*
+		Upsert test cases (only test float32 unless otherwise specified):
+		- Equivalence Class Testing ( 1000 vectors inserted before an upsert )
+			- case 1.1: success upsert with new ID
+			- case 1.2: success upsert with existent ID
+			- case 2.1: fail upsert with one different dimension vector (type: uint8)
+			- case 2.2: fail upsert with one different dimension vector (type: float32)
+		- Boundary Value Testing ( 1000 vectors inserted before an upsert if not specified )
+			- case 1.1: fail upsert with "" as ID
+			- case 1.2: fail upsert to empty index with "" as ID
+			- case 2.1: success upsert with ^@ as duplicated ID
+			- case 2.2: success upsert with ^@ as new ID
+			- case 2.3: success upsert to empty index with ^@ as new ID
+			- case 2.4: success upsert with ^I as duplicated ID
+			- case 2.5: success upsert with ^I as new ID
+			- case 2.6: success upsert to empty index with ^I as new ID
+			- case 2.7: success upsert with ^J as duplicated ID
+			- case 2.8: success upsert with ^J as new ID
+			- case 2.9: success upsert to empty index with ^J as new ID
+			- case 2.10: success upsert with ^M as duplicated ID
+			- case 2.11: success upsert with ^M as new ID
+			- case 2.12: success upsert to empty index with ^M as new ID
+			- case 2.13: success upsert with ^[ as duplicated ID
+			- case 2.14: success upsert with ^[ as new ID
+			- case 2.15: success upsert to empty index with ^[ as new ID
+			- case 2.16: success upsert with ^? as duplicated ID
+			- case 2.17: success upsert with ^? as new ID
+			- case 2.18: success upsert to empty index with ^? as new ID
+			- case 3.1: success upsert with utf-8 ID from utf-8 index
+			- case 3.2: success upsert with utf-8 ID from s-jis index
+			- case 3.3: success upsert with utf-8 ID from euc-jp index
+			- case 3.4: success upsert with s-jis ID from utf-8 index
+			- case 3.5: success upsert with s-jis ID from s-jis index
+			- case 3.6: success upsert with s-jis ID from euc-jp index
+			- case 3.4: success upsert with euc-jp ID from utf-8 index
+			- case 3.5: success upsert with euc-jp ID from s-jis index
+			- case 3.6: success upsert with euc-jp ID from euc-jp index
+			- case 4.1: success upsert with 😀 as duplicated ID
+			- case 4.2: success upsert with 😀 as new ID
+			- case 4.3: success upsert to empty index with 😀 as new ID
+			- case 5.1: success upsert with one 0 value vector with duplicated ID (type: uint8)
+			- case 5.2: success upsert with one 0 value vector with new ID (type: uint8)
+			- case 5.3: success upsert to empty index with one 0 value vector with new ID (type: uint8)
+			- case 5.4: success upsert with one +0 value vector with duplicated ID (type: float32)
+			- case 5.5: success upsert with one +0 value vector with new ID (type: float32)
+			- case 5.6: success upsert to empty index with one +0 value vector with new ID (type: float32)
+			- case 5.7: success upsert with one -0 value vector with duplicated ID (type: float32)
+			- case 5.8: success upsert with one -0 value vector with new ID (type: float32)
+			- case 5.9: success upsert to empty index with one -0 value vector with new ID (type: float32)
+			- case 6.1: success upsert with one min value vector with duplicated ID (type: uint8)
+			- case 6.2: success upsert with one min value vector with new ID (type: uint8)
+			- case 6.3: success upsert to empty index with one min value vector with new ID (type: uint8)
+			- case 6.4: success upsert with one min value vector with duplicated ID (type: float32)
+			- case 6.5: success upsert with one min value vector with new ID (type: float32)
+			- case 6.6: success upsert to empty index with one min value vector with new ID (type: float32)
+			- case 7.1: success upsert with one max value vector with duplicated ID (type: uint8)
+			- case 7.2: success upsert with one max value vector with new ID (type: uint8)
+			- case 7.3: success upsert to empty index with one max value vector with new ID (type: uint8)
+			- case 7.4: success upsert with one max value vector with duplicated ID (type: float32)
+			- case 7.5: success upsert with one max value vector (type: float32)
+			- case 7.6: success upsert to empty index with one max value vector (type: float32)
+			- case 8.1: success upsert with one NaN value vector with duplicated ID (type: float32) // NOTE: To fix it, it is necessary to check all of vector value
+			- case 8.2: success upsert with one NaN value vector with new ID (type: float32) // NOTE: To fix it, it is necessary to check all of vector value
+			- case 8.3: success upsert to empty index with one NaN value vector with new ID (type: float32) // NOTE: To fix it, it is necessary to check all of vector value
+			- case 9.1: success upsert with one +inf value vector with duplicated ID (type: float32)
+			- case 9.2: success upsert with one +inf value vector with new ID (type: float32)
+			- case 9.3: success upsert to empty index with one +inf value vector with new ID (type: float32)
+			- case 9.4: success upsert with one -inf value vector with duplicated ID (type: float32)
+			- case 9.5: success upsert with one -inf value vector with new ID (type: float32)
+			- case 9.6: success upsert to empty index with one -inf value vector with new ID (type: float32)
+			- case 10.1: fail upsert with one nil vector
+			- case 10.2: fail upsert to empty index with one nil vector
+			- case 11.1: fail upsert with one empty vector
+			- case 11.2: fail upsert to empty index with one empty vector
+		- Decision Table Testing
+			- case 1.1: fail upsert with one duplicated vector, duplicated ID and SkipStrictExistCheck is true
+			- case 1.2: success upsert with one different vector, duplicated ID and SkipStrictExistsCheck is true
+			- case 1.3: success upsert with one duplicated vector, different ID and SkipStrictExistCheck is true
+			- case 2.1: success upsert with one duplicated vector, new ID and SkipStrictExistCheck is true
+			- case 2.2: success upsert with one different vector, new ID and SkipStrictExistsCheck is true
+			- case 2.3: success upsert with one duplicated vector, new ID and SkipStrictExistCheck is true
+			- case 3.1: fail upsert with one duplicated vector, duplicated ID and SkipStrictExistCheck is false
+			- case 3.2: success upsert with one different vector, duplicated ID and SkipStrictExistsCheck is false
+			- case 3.3: success upsert with one duplicated vector, different ID and SkipStrictExistCheck is false
+			- case 4.1: success upsert with one duplicated vector, new ID and SkipStrictExistCheck is false
+			- case 4.2: success upsert with one different vector, new ID and SkipStrictExistsCheck is false
+			- case 4.3: success upsert with one duplicated vector, new ID and SkipStrictExistCheck is false
+	*/
 	tests := []test{
 		// TODO test cases
 		/*
