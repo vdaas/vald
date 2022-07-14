e26c65d8d531b0787187bcc9ed0a3d83ddc5ca77
kpango <kpango@vdaas.org>
2022-07-14 14:37:17 +0900

    add distance type
    Signed-off-by: kpango <kpango@vdaas.org>

--- internal/config/ngt.go
+++ internal/config/ngt.go
@@ -29,7 +29,7 @@ type NGT struct {
 	BulkInsertChunkSize int `yaml:"bulk_insert_chunk_size" json:"bulk_insert_chunk_size,omitempty"`
 
 	// DistanceType represent the ngt index distance type
-	// it should be `l1`, `l2`, `angle`, `hamming`, `cosine`, `normalizedangle`, `normalizedcosine` or `jaccard`. for further details about NGT libraries supported distance is https://github.com/yahoojapan/NGT/wiki/Command-Quick-Reference and vald agent's supported NGT distance type is https://pkg.go.dev/github.com/vdaas/vald/internal/core/algorithm/ngt#pkg-constants
+	// it should be `l1`, `l2`, `angle`, `hamming`, `cosine`,`poincare`, `lorentz`, `jaccard`, `sparsejaccard`, `normalizedangle` or `normalizedcosine`. for further details about NGT libraries supported distance is https://github.com/yahoojapan/NGT/wiki/Command-Quick-Reference and vald agent's supported NGT distance type is https://pkg.go.dev/github.com/vdaas/vald/internal/core/algorithm/ngt#pkg-constants
 	DistanceType string `yaml:"distance_type" json:"distance_type,omitempty" info:"distance_type"`
 
 	// ObjectType represent the ngt index object type float or int
