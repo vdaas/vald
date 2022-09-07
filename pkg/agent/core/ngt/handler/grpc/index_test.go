19a5532577af6020c70089110eb84f876827667f
kevindiu <kevindiujp@gmail.com>
2022-09-07 17:31:44 +0900

    disable log for save index test to avoid error log
    Signed-off-by: kevindiu <kevindiujp@gmail.com>

--- pkg/agent/core/ngt/handler/grpc/index_test.go
+++ pkg/agent/core/ngt/handler/grpc/index_test.go
@@ -1,16 +1,18 @@
+//
 // Copyright (C) 2019-2022 vdaas.org vald team <vald@vdaas.org>
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//	https://www.apache.org/licenses/LICENSE-2.0
+//    https://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+//
 package grpc
 
 import (
@@ -1116,7 +1118,9 @@ func Test_server_IndexInfo(t *testing.T) {
 
 					go func() {
 						if _, err := s.SaveIndex(ctx, &payload.Empty{}); err != nil {
-							t.Log(err)
+							// since the context closed error will be returned after checkFunc,
+							// we ignore the error here to avoid go test error
+							// t.Log(err)
 						}
 					}()
 				},
