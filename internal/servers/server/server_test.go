3e286c5654d1ba9a10b484375390713715295e31
Vdaas CI <vald@vdaas.org>
2022-11-18 05:57:35 +0000

    :robot: Update license headers / Format Go codes and YAML files
    Signed-off-by: Vdaas CI <vald@vdaas.org>

--- internal/servers/server/server_test.go
+++ internal/servers/server/server_test.go
@@ -429,7 +429,7 @@ func Test_server_ListenAndServe(t *testing.T) {
 			})
 
 			srv := &http.Server{
-				Handler: handler,
+				Handler:           handler,
 				ReadHeaderTimeout: 3 * time.Second,
 			}
 
