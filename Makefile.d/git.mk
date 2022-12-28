13f6811141996209696477e55ccd3c1b076cc6ff
Yusuke Kato <kpango@vdaas.org>
2022-12-28 10:56:48 +0900

    add auto update libs & deps make command (#1917)
    Signed-off-by: kpango <kpango@vdaas.org>

--- Makefile.d/git.mk
+++ Makefile.d/git.mk
@@ -22,5 +22,5 @@ git/config/init:
 .PHONY: git/hooks/init
 ## add configs for registering pre-defined git hooks
 git/hooks/init:
-	ln -sf ../../hack/git/hooks/pre-commit .git/hooks/pre-commit
-	chmod a+x .git/hooks/pre-commit
+	ln -sf $(ROOTDIR)/hack/git/hooks/pre-commit $(ROOTDIR)/.git/hooks/pre-commit
+	chmod a+x $(ROOTDIR)/.git/hooks/pre-commit
