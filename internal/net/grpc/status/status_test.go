b78a1896ff59663e4ad9b3e40f61d8f6b086051c
kevindiu <kevin_diu@yahoo.com.hk>
2023-02-02 10:23:10 +0900

    revert internal grpc status test implementation
    Signed-off-by: kevindiu <kevin_diu@yahoo.com.hk>

--- internal/net/grpc/status/status_test.go
+++ internal/net/grpc/status/status_test.go
@@ -18,11 +18,136 @@
 package status
 
 import (
+	"encoding/json"
+	"reflect"
 	"testing"
 
+	"github.com/vdaas/vald/internal/errors"
+	"github.com/vdaas/vald/internal/info"
+	"github.com/vdaas/vald/internal/net/grpc/codes"
+	"github.com/vdaas/vald/internal/net/grpc/errdetails"
 	"github.com/vdaas/vald/internal/test/goleak"
 )
 
 func TestMain(m *testing.M) {
+	info.Init("")
 	goleak.VerifyTestMain(m)
+}
+
+func TestParseError(t *testing.T) {
+	t.Parallel()
+	type args struct {
+		err         error
+		defaultCode codes.Code
+		defaultMsg  string
+		details     []interface{}
+	}
+	type want struct {
+		wantSt  codes.Code
+		wantMsg string
+		err     error
+	}
+	type test struct {
+		name       string
+		args       args
+		want       want
+		checkFunc  func(want, *Status, string, error) error
+		beforeFunc func(args)
+		afterFunc  func(args)
+	}
+	defaultCheckFunc := func(w want, gotSt *Status, gotMsg string, err error) error {
+		if !errors.Is(err, w.err) {
+			return errors.Errorf("got_error: \"%#v\",\n\t\t\t\twant: \"%#v\"", err, w.err)
+		}
+		if !reflect.DeepEqual(gotSt, w.wantSt) {
+			return errors.Errorf("got: \"%#v\",\n\t\t\t\twant: \"%#v\"", gotSt, w.wantSt)
+		}
+		if !reflect.DeepEqual(gotMsg, w.wantMsg) {
+			return errors.Errorf("got: \"%#v\",\n\t\t\t\twant: \"%#v\"", gotMsg, w.wantMsg)
+		}
+		return nil
+	}
+	tests := []test{
+		func() test {
+			return test{
+				name: "test_case_1",
+				args: args{
+					err: WrapWithNotFound(errors.ErrEmptySearchResult.Error(), errors.ErrEmptySearchResult,
+						&errdetails.RequestInfo{
+							RequestId: "sample request ID",
+						},
+						&errdetails.ResourceInfo{
+							ResourceType: "sample resource type",
+							ResourceName: "sample resource name",
+						}, info.Get()),
+					defaultCode: codes.Internal,
+					defaultMsg:  "failed to parse Search gRPC error response",
+					details:     nil,
+				},
+				want: want{
+					wantSt: codes.NotFound,
+				},
+				checkFunc: func(w want, gotSt *Status, gotMsg string, err error) error {
+					if w.wantSt != gotSt.Code() {
+						b, err := json.MarshalIndent(gotSt.Details(), "", "\t")
+						t.Log(gotSt.String(), string(b), err)
+						return errors.ErrEmptySearchResult
+					}
+					return nil
+				},
+			}
+		}(),
+		/*
+		   {
+		       name: "test_case_1",
+		       args: args {
+		           err: nil,
+		           defaultCode: nil,
+		           defaultMsg: "",
+		           details: nil,
+		       },
+		       want: want{},
+		       checkFunc: defaultCheckFunc,
+		   },
+		*/
+
+		// TODO test cases
+		/*
+		   func() test {
+		       return test {
+		           name: "test_case_2",
+		           args: args {
+		           err: nil,
+		           defaultCode: nil,
+		           defaultMsg: "",
+		           details: nil,
+		           },
+		           want: want{},
+		           checkFunc: defaultCheckFunc,
+		       }
+		   }(),
+		*/
+	}
+
+	for _, tc := range tests {
+		test := tc
+		t.Run(test.name, func(tt *testing.T) {
+			tt.Parallel()
+			if test.beforeFunc != nil {
+				test.beforeFunc(test.args)
+			}
+			if test.afterFunc != nil {
+				defer test.afterFunc(test.args)
+			}
+			checkFunc := test.checkFunc
+			if test.checkFunc == nil {
+				checkFunc = defaultCheckFunc
+			}
+
+			gotSt, gotMsg, err := ParseError(test.args.err, test.args.defaultCode, test.args.defaultMsg, test.args.details...)
+			if err := checkFunc(test.want, gotSt, gotMsg, err); err != nil {
+				tt.Errorf("error = %v", err)
+			}
+		})
+	}
 }
