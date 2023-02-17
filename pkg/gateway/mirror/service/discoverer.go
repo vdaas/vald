6c49e03653cd17d7cd5efd494f9eb538550ae44a
hlts2 <hiroto.funakoshi.hiroto@gmail.com>
2023-02-17 11:59:18 +0900

    fix log variable
    Signed-off-by: hlts2 <hiroto.funakoshi.hiroto@gmail.com>

--- pkg/gateway/mirror/service/discoverer.go
+++ pkg/gateway/mirror/service/discoverer.go
@@ -217,7 +217,7 @@ func (d *discoverer) startAdvertise(ctx context.Context) (<-chan error, error) {
 					case ech <- err:
 					}
 				}
-				log.Infof("[mirror]: connected mirror gateway targets: %v", resTgts)
+				log.Infof("[mirror]: connected mirror gateway targets: %v", d.client.GRPCClient().ConnectedAddrs())
 			}
 		}
 	})
