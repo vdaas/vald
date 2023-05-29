8175def09ea4964e9705b03ed2b4892b6ac2fa2d
ykadowak <yusuke.kadowaki.1231@gmail.com>
2023-05-29 05:32:00 +0000

    remove check apis/proto/v1

--- Makefile.d/proto.mk
+++ Makefile.d/proto.mk
@@ -58,8 +58,7 @@ proto/deps: \
 	$(GOPATH)/src/github.com/planetscale/vtprotobuf \
 	$(GOPATH)/src/github.com/protocolbuffers/protobuf \
 	$(GOPATH)/src/google.golang.org/genproto \
-	$(GOPATH)/src/google.golang.org/protobuf \
-	$(ROOTDIR)/apis/proto/v1
+	$(GOPATH)/src/google.golang.org/protobuf
 
 $(GOPATH)/src/github.com/protocolbuffers/protobuf:
 	git clone \
