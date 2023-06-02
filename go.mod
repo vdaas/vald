f93e738fc8aa5cc2bf777464b9e7bdc765330db8
vankichi <kyukawa315@gmail.com>
2023-06-02 11:09:39 +0900

    :sparkles: Add search result aggregation option
    Signed-off-by: vankichi <kyukawa315@gmail.com>

--- go.mod
+++ go.mod
@@ -543,6 +543,7 @@ require (
 	go.opentelemetry.io/otel/trace v1.11.2
 	go.uber.org/automaxprocs v0.0.0-00010101000000-000000000000
 	go.uber.org/goleak v1.2.1
+	go.uber.org/ratelimit v0.2.0
 	go.uber.org/zap v1.24.0
 	gocloud.dev v0.0.0-00010101000000-000000000000
 	golang.org/x/exp v0.0.0-20230510235704-dd950f8aeaea
@@ -551,6 +552,7 @@ require (
 	golang.org/x/sync v0.2.0
 	golang.org/x/sys v0.8.0
 	golang.org/x/text v0.9.0
+	golang.org/x/time v0.3.0
 	golang.org/x/tools v0.9.1
 	gonum.org/v1/hdf5 v0.0.0-00010101000000-000000000000
 	gonum.org/v1/plot v0.10.1
@@ -653,7 +655,6 @@ require (
 	golang.org/x/image v0.7.0 // indirect
 	golang.org/x/mod v0.10.0 // indirect
 	golang.org/x/term v0.8.0 // indirect
-	golang.org/x/time v0.3.0 // indirect
 	golang.org/x/xerrors v0.0.0-20220907171357-04be3eba64a2 // indirect
 	gomodules.xyz/jsonpatch/v2 v2.3.0 // indirect
 	google.golang.org/api v0.125.0 // indirect
--- internal/config/benchmark.go
+++ internal/config/benchmark.go
@@ -127,16 +127,18 @@ func (cfg *UpsertConfig) Bind() *UpsertConfig {
 
 // SearchConfig defines the desired state of search config
 type SearchConfig struct {
-	Epsilon            float32 `json:"epsilon,omitempty"`
-	Radius             float32 `json:"radius,omitempty"`
-	Num                int32   `json:"num,omitempty"`
-	MinNum             int32   `json:"min_num,omitempty"`
-	Timeout            string  `json:"timeout,omitempty"`
-	EnableLinearSearch bool    `json:"enable_linear_search,omitempty"`
+	Epsilon              float32 `json:"epsilon,omitempty"`
+	Radius               float32 `json:"radius,omitempty"`
+	Num                  int32   `json:"num,omitempty"`
+	MinNum               int32   `json:"min_num,omitempty"`
+	Timeout              string  `json:"timeout,omitempty"`
+	EnableLinearSearch   bool    `json:"enable_linear_search,omitempty"`
+	AggregationAlgorithm string  `json:"aggregation_algorithm,omitempty"`
 }
 
 func (cfg *SearchConfig) Bind() *SearchConfig {
 	cfg.Timeout = GetActualValue(cfg.Timeout)
+	cfg.AggregationAlgorithm = GetActualValue(cfg.AggregationAlgorithm)
 	return cfg
 }
 
