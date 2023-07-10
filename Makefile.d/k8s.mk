bafd9a4612a61d1c65a231fc58d0c0d2f7a51518
hlts2 <hiroto.funakoshi.hiroto@gmail.com>
2023-07-10 11:52:28 +0900

    add jaeger operator wait duration
    Signed-off-by: hlts2 <hiroto.funakoshi.hiroto@gmail.com>

--- Makefile.d/k8s.mk
+++ Makefile.d/k8s.mk
@@ -13,6 +13,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+
+JAEGER_OPERATOR_WAIT_DURATION := 0
+
 .PHONY: k8s/manifest/clean
 ## clean k8s manifests
 k8s/manifest/clean:
@@ -227,7 +230,7 @@ k8s/metrics/jaeger/deploy:
 	helm repo add jaegertracing https://jaegertracing.github.io/helm-charts
 	helm install jaeger jaegertracing/jaeger-operator --version $(JAEGER_OPERATOR_VERSION)
 	kubectl wait --for=condition=ready pod -l app.kubernetes.io/name=jaeger-operator --timeout=60s
-	kubectl wait --for=condition=available deployment/jaeger-jaeger-operator --timeout=60s 
+	sleep $(JAEGER_OPERATOR_WAIT_DURATION)
 	kubectl apply -f k8s/metrics/jaeger/jaeger.yaml
 
 .PHONY: k8s/metrics/jaeger/delete
