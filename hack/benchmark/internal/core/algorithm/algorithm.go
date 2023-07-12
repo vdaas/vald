38d537f4b678baa92c8a5de286322783d16f581b
Kevin Diu <kevindiujp@gmail.com>
2023-07-12 09:45:52 +0900

    Fix hack/benchmark search interface change (#2129)

--- hack/benchmark/internal/core/algorithm/algorithm.go
+++ hack/benchmark/internal/core/algorithm/algorithm.go
@@ -17,6 +17,8 @@
 // Package algorithm provides core interface
 package algorithm
 
+import "context"
+
 type Mode uint32
 
 const (
@@ -29,7 +31,7 @@ type Closer interface {
 }
 
 type Bit32 interface {
-	Search(vec []float32, size int, epsilon, radius float32) (interface{}, error)
+	Search(ctx context.Context, vec []float32, size int, epsilon, radius float32) (interface{}, error)
 	Insert(vec []float32) (uint, error)
 	InsertCommit(vec []float32, poolSize uint32) (uint, error)
 	BulkInsert(vecs [][]float32) ([]uint, []error)
@@ -44,7 +46,7 @@ type Bit32 interface {
 }
 
 type Bit64 interface {
-	Search(vec []float64, size int, epsilon, radius float32) (interface{}, error)
+	Search(ctx context.Context, vec []float64, size int, epsilon, radius float32) (interface{}, error)
 	Insert(vec []float64) (uint, error)
 	InsertCommit(vec []float64, poolSize uint32) (uint, error)
 	BulkInsert(vecs [][]float64) ([]uint, []error)
