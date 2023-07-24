3d8899f1493595a116266bdfbfaa26b8f1d87013
ykadowak <yusuke.kadowaki.1231@gmail.com>
2023-07-24 04:07:07 +0000

    fix test

--- pkg/agent/core/ngt/service/vqueue/stateful_test.go
+++ pkg/agent/core/ngt/service/vqueue/stateful_test.go
@@ -36,9 +36,10 @@ type qSystem struct {
 }
 
 type resultContainer struct {
-	err    error
-	vector []float32
-	exists bool
+	err       error
+	vector    []float32
+	timestamp int64
+	exists    bool
 }
 
 type qState struct {
@@ -693,9 +694,10 @@ var (
 			sy := systemUnderTest.(*qSystem)
 			q := sy.q
 
-			vec, exists := q.GetVector(idA)
+			vec, ts, exists := q.GetVector(idA)
 			return &resultContainer{
 				vector: vec,
+				timestamp: ts,
 				exists: exists,
 			}
 		},
@@ -755,9 +757,10 @@ var (
 			sy := systemUnderTest.(*qSystem)
 			q := sy.q
 
-			vec, exists := q.GetVector(idB)
+			vec, ts, exists := q.GetVector(idB)
 			return &resultContainer{
 				vector: vec,
+				timestamp: ts,
 				exists: exists,
 			}
 		},
@@ -817,9 +820,10 @@ var (
 			sy := systemUnderTest.(*qSystem)
 			q := sy.q
 
-			vec, exists := q.GetVector(idC)
+			vec, ts, exists := q.GetVector(idC)
 			return &resultContainer{
 				vector: vec,
+				timestamp: ts,
 				exists: exists,
 			}
 		},
