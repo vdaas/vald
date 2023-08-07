8a1221d8da2645fc3e603a173516dc6e86dbdf84
ykadowak <yusuke.kadowaki.1231@gmail.com>
2023-08-07 08:33:21 +0000

    add corrector configuration

---
+++ internal/config/corrector.go
@@ -0,0 +1,54 @@
+//
+// Copyright (C) 2019-2023 vdaas.org vald team <vald@vdaas.org>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+// Package config providers configuration type and load configuration logic
+package config
+
+// Corrector represents the index correction configurations.
+type Corrector struct {
+	// AgentPort represent agent port number
+	AgentPort int `json:"agent_port" yaml:"agent_port"`
+
+	// AgentName represent agents meta_name for service discovery
+	AgentName string `json:"agent_name" yaml:"agent_name"`
+
+	// AgentNamespace represent agent namespace location
+	AgentNamespace string `json:"agent_namespace" yaml:"agent_namespace"`
+
+	// AgentDNS represent agents dns A record for service discovery
+	AgentDNS string `json:"agent_dns" yaml:"agent_dns"`
+
+	CreationPoolSize uint32 `yaml:"creation_pool_size" json:"creation_pool_size"`
+
+	// NodeName represents node name
+	NodeName string `json:"node_name" yaml:"node_name"`
+
+	// Discoverer represent agent discoverer service configuration
+	Discoverer *DiscovererClient `json:"discoverer" yaml:"discoverer"`
+}
+
+// Bind binds the actual data from the Indexer receiver field.
+func (im *Corrector) Bind() *Corrector {
+	im.AgentName = GetActualValue(im.AgentName)
+	im.AgentNamespace = GetActualValue(im.AgentNamespace)
+	im.AgentDNS = GetActualValue(im.AgentDNS)
+	im.NodeName = GetActualValue(im.NodeName)
+
+	if im.Discoverer != nil {
+		im.Discoverer = im.Discoverer.Bind()
+	}
+	return im
+}
