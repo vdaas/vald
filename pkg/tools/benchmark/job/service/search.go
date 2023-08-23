caabde21ba5a61d0778ff2aac2c35d64faa38756
vankichi <kyukawa315@gmail.com>
2023-08-23 13:47:52 +0900

    :recycle: use golang/sync/error for prevent OOM KILL
    Signed-off-by: vankichi <kyukawa315@gmail.com>

--- pkg/tools/benchmark/job/service/search.go
+++ pkg/tools/benchmark/job/service/search.go
@@ -22,9 +22,9 @@ import (
 	"math"
 
 	"github.com/vdaas/vald/apis/grpc/v1/payload"
-	"github.com/vdaas/vald/internal/errgroup"
 	"github.com/vdaas/vald/internal/errors"
 	"github.com/vdaas/vald/internal/log"
+	"golang.org/x/sync/errgroup"
 )
 
 func (j *job) search(ctx context.Context, ech chan error) error {
@@ -47,8 +47,10 @@ func (j *job) search(ctx context.Context, ech chan error) error {
 		}(),
 	}
 	sres := make([]*payload.Search_Response, j.dataset.Indexes)
-	eg, egctx := errgroup.New(ctx)
-	eg.Limitation(j.concurrencyLimit)
+	eg, egctx := errgroup.WithContext(ctx)
+	eg.SetLimit(j.concurrencyLimit)
+	// eg, egctx := errgroup.New(ctx)
+	// eg.Limitation(j.concurrencyLimit)
 	for i := j.dataset.Range.Start; i <= j.dataset.Range.End; i++ {
 		iter := i
 		eg.Go(func() error {
--- pkg/tools/benchmark/job/service/update.go
+++ pkg/tools/benchmark/job/service/update.go
@@ -23,9 +23,9 @@ import (
 	"strconv"
 
 	"github.com/vdaas/vald/apis/grpc/v1/payload"
-	"github.com/vdaas/vald/internal/errgroup"
 	"github.com/vdaas/vald/internal/errors"
 	"github.com/vdaas/vald/internal/log"
+	"golang.org/x/sync/errgroup"
 )
 
 func (j *job) update(ctx context.Context, ech chan error) error {
@@ -39,8 +39,10 @@ func (j *job) update(ctx context.Context, ech chan error) error {
 	if j.timestamp > int64(0) {
 		cfg.Timestamp = j.timestamp
 	}
-	eg, egctx := errgroup.New(ctx)
-	eg.Limitation(j.concurrencyLimit)
+	eg, egctx := errgroup.WithContext(ctx)
+	eg.SetLimit(j.concurrencyLimit)
+	// eg, egctx := errgroup.New(ctx)
+	// eg.Limitation(j.concurrencyLimit)
 	for i := j.dataset.Range.Start; i <= j.dataset.Range.End; i++ {
 		iter := i
 		eg.Go(func() error {
