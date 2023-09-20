e5f73a342f341450267ad5b6727b2d9bd7f0c25b
ykadowak <yusuke.kadowaki.1231@gmail.com>
2023-09-20 02:28:45 +0000

    Add type check for type assertion

--- cmd/index/job/correction/main.go
+++ cmd/index/job/correction/main.go
@@ -1,7 +1,7 @@
 // Copyright (C) 2019-2023 vdaas.org vald team <vald@vdaas.org>
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// You may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //	https://www.apache.org/licenses/LICENSE-2.0
@@ -45,7 +45,11 @@ func main() {
 				return cfg, &cfg.GlobalConfig, nil
 			}),
 			runner.WithDaemonInitializer(func(cfg interface{}) (runner.Runner, error) {
-				return usecase.New(cfg.(*config.Data))
+				c, ok := cfg.(*config.Data)
+				if !ok {
+					return nil, errors.ErrInvalidConfig
+				}
+				return usecase.New(c)
 			}),
 		)
 	})(); err != nil {
