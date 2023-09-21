cb012ad8da7189dac4420a315806ae484807b161
kevindiu <kevin_diu@yahoo.com.hk>
2023-09-21 01:59:57 +0000

    update lb logic
    Signed-off-by: kevindiu <kevin_diu@yahoo.com.hk>

--- pkg/gateway/lb/handler/grpc/handler.go
+++ pkg/gateway/lb/handler/grpc/handler.go
@@ -1837,7 +1837,7 @@ func (s *server) Update(ctx context.Context, req *payload.Update_Request) (res *
 			return nil, err
 		}
 		if conv.F32stos(vec.GetVector()) == conv.F32stos(req.GetVector().GetVector()) {
-			if req.GetConfig().GetUpdateTimestampIfExists() {
+			if req.GetConfig().GetUpdateTimestampIfExists() && vec.Timestamp == req.GetConfig().GetTimestamp() {
 				// if the vector is the same, broadcast update request to update index timestamp
 				return broadcastUpdate()
 			}
