e4dc59016342d54e71bb09392b6c73298e4ff273
ykadowak <yusuke.kadowaki.1231@gmail.com>
2023-11-30 02:36:30 +0000

    Revert "Add verification for index correction e2e and add clusterrole cronjobs for operator to deploy index correction (#2205)"
    This reverts commit b4028e780af500909738d57b2fa18611a3c55892.

--- tests/e2e/crud/crud_test.go
+++ tests/e2e/crud/crud_test.go
@@ -24,7 +24,6 @@ import (
 	"flag"
 	"fmt"
 	"os"
-	"os/exec"
 	"testing"
 	"time"
 
@@ -43,14 +42,13 @@ var (
 	port int
 	ds   *hdf5.Dataset
 
-	insertNum           int
-	correctionInsertNum int
-	searchNum           int
-	searchByIDNum       int
-	getObjectNum        int
-	updateNum           int
-	upsertNum           int
-	removeNum           int
+	insertNum     int
+	searchNum     int
+	searchByIDNum int
+	getObjectNum  int
+	updateNum     int
+	upsertNum     int
+	removeNum     int
 
 	insertFrom     int
 	searchFrom     int
@@ -75,7 +73,6 @@ func init() {
 	flag.IntVar(&port, "port", 8081, "gRPC port")
 
 	flag.IntVar(&insertNum, "insert-num", 10000, "number of id-vector pairs used for insert")
-	flag.IntVar(&correctionInsertNum, "correction-insert-num", 3000, "number of id-vector pairs used for insert")
 	flag.IntVar(&searchNum, "search-num", 10000, "number of id-vector pairs used for search")
 	flag.IntVar(&searchByIDNum, "search-by-id-num", 100, "number of id-vector pairs used for search-by-id")
 	flag.IntVar(&getObjectNum, "get-object-num", 100, "number of id-vector pairs used for get-object")
@@ -761,9 +758,7 @@ func TestE2EIndexJobCorrection(t *testing.T) {
 		t.Fatalf("an error occurred: %s", err)
 	}
 
-	// prepare train data
-	train := ds.Train[insertFrom : insertFrom+correctionInsertNum]
-
+	train := ds.Train[insertFrom : insertFrom+insertNum]
 	err = op.Insert(t, ctx, operation.Dataset{
 		Train: train,
 	})
@@ -773,49 +768,12 @@ func TestE2EIndexJobCorrection(t *testing.T) {
 
 	sleep(t, waitAfterInsertDuration)
 
-	t.Log("Test case 1: just execute index correction and check if replica number is correct after correction")
 	exe := operation.NewCronJobExecutor("vald-index-correction")
 	err = exe.CreateAndWait(t, ctx, "correction-test")
 	if err != nil {
 		t.Fatalf("an error occurred: %s", err)
 	}
 
-	// check if replica number is correct
-	err = op.StreamListObject(t, ctx, operation.Dataset{
-		Train: train,
-	})
-	if err != nil {
-		t.Fatalf("an error occurred: %s", err)
-	}
-
-	t.Log("Test case 2: execute index correction after one agent removed")
-	t.Log("removing vald-agent-ngt-0...")
-	cmd := exec.CommandContext(ctx, "sh", "-c", "kubectl delete pod vald-agent-ngt-0 && kubectl wait --for=condition=Ready pod/vald-agent-ngt-0")
-	out, err := cmd.Output()
-	if err != nil {
-		if exitErr, ok := err.(*exec.ExitError); ok {
-			t.Fatalf("%s, %s, %v", string(out), string(exitErr.Stderr), err)
-		} else {
-			t.Fatalf("unexpected error on creating job: %v", err)
-		}
-	}
-	t.Log(string(out))
-
-	// correct the deleted index
-	err = exe.CreateAndWait(t, ctx, "correction-test")
-	if err != nil {
-		t.Fatalf("an error occurred: %s", err)
-	}
-
-	// check if replica number is correct
-	err = op.StreamListObject(t, ctx, operation.Dataset{
-		Train: train,
-	})
-	if err != nil {
-		t.Fatalf("an error occurred: %s", err)
-	}
-
-	t.Log("Tear down. Removing all vectors...")
 	err = op.Remove(t, ctx, operation.Dataset{
 		Train: train,
 	})
