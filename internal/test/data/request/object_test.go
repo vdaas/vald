b6da74c80c1ecdf1ea2880914d1860bd9b42f7c5
ykadowak <yusuke.kadowaki.1231@gmail.com>
2023-11-30 07:02:13 +0000

    Revert "Use internal comparator instead of go-cmp (#2132)"
    This reverts commit bf17b939e988020d3d7680d9dc91d0b3a4cab38e.

--- internal/test/data/request/object_test.go
+++ internal/test/data/request/object_test.go
@@ -17,19 +17,19 @@ import (
 	"reflect"
 	"testing"
 
+	"github.com/google/go-cmp/cmp"
 	"github.com/vdaas/vald/apis/grpc/v1/payload"
 	"github.com/vdaas/vald/internal/errors"
 	"github.com/vdaas/vald/internal/test/comparator"
 	"github.com/vdaas/vald/internal/test/goleak"
 )
 
-var defaultObjectLocationComparators = []comparator.Option{
+var defaultObjectLocationComparators = []cmp.Option{
 	comparator.IgnoreUnexported(payload.Object_Locations{}),
 	comparator.IgnoreUnexported(payload.Object_Location{}),
 }
 
 func TestGenObjectLocations(t *testing.T) {
-	t.Parallel()
 	type args struct {
 		num    int
 		name   string
@@ -152,7 +152,6 @@ func TestGenObjectLocations(t *testing.T) {
 }
 
 func TestGenObjectStreamLocation(t *testing.T) {
-	t.Parallel()
 	type args struct {
 		num    int
 		name   string
