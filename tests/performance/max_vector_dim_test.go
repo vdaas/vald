d8a5821352fa4da922da4f3162d591ed939de1bb
ykadowak <yusuke.kadowaki.1231@gmail.com>
2023-11-30 02:33:43 +0000

    Revert "Fix coverage CI error (#2150)"
    This reverts commit 734cd4f31310e8cc2fd11955cef4e076b1eb678f.

--- tests/performance/max_vector_dim_test.go
+++ tests/performance/max_vector_dim_test.go
@@ -16,7 +16,6 @@ package performance
 import (
 	"bufio"
 	"context"
-	"flag"
 	"os"
 	"strconv"
 	"sync"
@@ -29,7 +28,6 @@ import (
 	"github.com/vdaas/vald/internal/core/algorithm/ngt"
 	"github.com/vdaas/vald/internal/errgroup"
 	"github.com/vdaas/vald/internal/errors"
-	"github.com/vdaas/vald/internal/log"
 	"github.com/vdaas/vald/internal/safety"
 	"github.com/vdaas/vald/internal/strings"
 	"github.com/vdaas/vald/internal/test/data/vector"
@@ -79,15 +77,6 @@ func parse(raw string) (key string, value int) {
 		return keyValue[0], val
 	}
 	return keyValue[0], val
-}
-
-func TestMain(m *testing.M) {
-	flag.Parse()
-	if testing.Short() {
-		log.Info("skipping this pkg test when -short because it takes a long time")
-		os.Exit(0)
-	}
-	os.Exit(m.Run())
 }
 
 // Test for investigation of max dimension size for agent handler
