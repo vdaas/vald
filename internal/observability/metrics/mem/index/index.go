5462501685bbf4f4d5f4b0c691dde9a813f2979b
Kosuke Morimoto <ksk@vdaas.org>
2023-12-12 15:11:46 +0900

    add memory metrics
    Signed-off-by: Kosuke Morimoto <ksk@vdaas.org>

--- internal/observability/metrics/mem/index/index.go
+++ internal/observability/metrics/mem/index/index.go
@@ -33,6 +33,9 @@ const (
 	sysMetricsName        = "sys_bytes"
 	sysMetricsDescription = "Total bytes of memory obtained from the OS"
 
+	lookupsMetricsName        = "lookups_count"
+	lookupsMetricsDescription = "The number of pointers"
+
 	mallocsMetricsName        = "mallocs_total"
 	mallocsMetricsDescription = "The cumulative count of heap objects allocated"
 
@@ -54,17 +57,53 @@ const (
 	heapReleasedMetricsName        = "heap_released_bytes"
 	heapReleasedMetricsDescription = "Bytes of physical memory returned to the OS"
 
+	heapObjectsMetricsName        = "heap_objects_count"
+	heapObjectsMetricsDescription = "The number of allocated heap objects"
+
 	stackInuseMetricsName        = "stack_inuse_bytes"
 	stackInuseMetricsDescription = "Bytes in stack spans"
 
 	stackSysMetricsName        = "stack_sys_bytes"
 	stackSysMetricsDescription = "Bytes of stack memory obtained from the OS"
 
+	mspanInuseMetricsName        = "mspan_inuse_bytes"
+	mspanInuseMetricsDescription = "Bytes of allocated mspan structures"
+
+	mspanSysMetricsName        = "mspan_sys_bytes"
+	mspanSysMetricsDescription = "Bytes of memory obtained from the OS for mspan structures"
+
+	mcacheInuseMetricsName        = "mcache_inuse_bytes"
+	mcacheInuseMetricsDescription = "Bytes of allocated mcache structures"
+
+	mcacheSysMetricsName        = "mcache_sys_bytes"
+	mcacheSysMetricsDescription = "Bytes of memory obtained from the OS mcache structures"
+
+	buckHashSysMetricsName        = "buckhash_sys_bytes"
+	buckHashSysMetricsDescription = "Bytes of memory in profiling bucket hash tables"
+
+	gcSysMetricsName        = "gc_sys_bytes"
+	gcSysMetricsDescription = "Bytes of memory in GC metadata"
+
+	otherSysMetricsName        = "other_sys_bytes"
+	otherSysMetricsDescription = "Bytes of memory in misc off-heap runtime allocations"
+
+	nextGcSysMetricsName        = "next_gc_bytes"
+	nextGcSysMetricsDescription = "Target heap size of the next GC"
+
 	pauseTotalMsMetricsName        = "pause_ms_total"
 	pauseTotalMsMetricsDescription = "The cumulative milliseconds in GC"
 
 	numGCMetricsName        = "gc_count"
 	numGCMetricsDescription = "The number of completed GC cycles"
+
+	numForcedGCMetricsName        = "forced_gc_count"
+	numForcedGCMetricsDescription = "The number of GC cycles called by the application"
+
+	heapWillReturnMetricsName        = "heap_will_return_bytes"
+	heapWillReturnMetricsDescription = "Bytes of returning to OS. It contains the two following parts (heapWillReturn = heapIdle - heapReleased)"
+
+	liveObjectsMetricsName        = "live_objects_count"
+	liveObjectsMetricsDescription = "The cumulative count of living heap objects allocated. It contains the two following parts (liveObjects = mallocs - frees)"
 )
 
 type memoryMetrics struct{}
@@ -75,7 +114,7 @@ func New() metrics.Metric {
 
 func (*memoryMetrics) View() ([]*metrics.View, error) {
 	alloc, err := view.New(
-		view.MatchInstrumentName(allocMetricsDescription),
+		view.MatchInstrumentName(allocMetricsName),
 		view.WithSetDescription(allocMetricsDescription),
 		view.WithSetAggregation(aggregation.LastValue{}),
 	)
@@ -84,7 +123,7 @@ func (*memoryMetrics) View() ([]*metrics.View, error) {
 	}
 
 	totalAlloc, err := view.New(
-		view.MatchInstrumentName(totalAllocMetricsDescription),
+		view.MatchInstrumentName(totalAllocMetricsName),
 		view.WithSetDescription(totalAllocMetricsDescription),
 		view.WithSetAggregation(aggregation.LastValue{}),
 	)
@@ -95,6 +134,15 @@ func (*memoryMetrics) View() ([]*metrics.View, error) {
 	sys, err := view.New(
 		view.MatchInstrumentName(sysMetricsName),
 		view.WithSetDescription(sysMetricsDescription),
+		view.WithSetAggregation(aggregation.LastValue{}),
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	lookups, err := view.New(
+		view.MatchInstrumentName(lookupsMetricsName),
+		view.WithSetDescription(lookupsMetricsDescription),
 		view.WithSetAggregation(aggregation.LastValue{}),
 	)
 	if err != nil {
@@ -164,6 +212,15 @@ func (*memoryMetrics) View() ([]*metrics.View, error) {
 		return nil, err
 	}
 
+	heapObjects, err := view.New(
+		view.MatchInstrumentName(heapObjectsMetricsName),
+		view.WithSetDescription(heapObjectsMetricsDescription),
+		view.WithSetAggregation(aggregation.LastValue{}),
+	)
+	if err != nil {
+		return nil, err
+	}
+
 	stackInuse, err := view.New(
 		view.MatchInstrumentName(stackInuseMetricsName),
 		view.WithSetDescription(stackInuseMetricsDescription),
@@ -176,6 +233,78 @@ func (*memoryMetrics) View() ([]*metrics.View, error) {
 	stackSys, err := view.New(
 		view.MatchInstrumentName(stackSysMetricsName),
 		view.WithSetDescription(stackSysMetricsDescription),
+		view.WithSetAggregation(aggregation.LastValue{}),
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	mspanInuse, err := view.New(
+		view.MatchInstrumentName(mspanInuseMetricsName),
+		view.WithSetDescription(mspanInuseMetricsDescription),
+		view.WithSetAggregation(aggregation.LastValue{}),
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	mspanSys, err := view.New(
+		view.MatchInstrumentName(mspanSysMetricsName),
+		view.WithSetDescription(mspanSysMetricsDescription),
+		view.WithSetAggregation(aggregation.LastValue{}),
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	mcacheInuse, err := view.New(
+		view.MatchInstrumentName(mcacheInuseMetricsName),
+		view.WithSetDescription(mcacheInuseMetricsDescription),
+		view.WithSetAggregation(aggregation.LastValue{}),
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	mcacheSys, err := view.New(
+		view.MatchInstrumentName(mcacheSysMetricsName),
+		view.WithSetDescription(mcacheSysMetricsDescription),
+		view.WithSetAggregation(aggregation.LastValue{}),
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	buckHashSys, err := view.New(
+		view.MatchInstrumentName(buckHashSysMetricsName),
+		view.WithSetDescription(buckHashSysMetricsDescription),
+		view.WithSetAggregation(aggregation.LastValue{}),
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	gcSys, err := view.New(
+		view.MatchInstrumentName(gcSysMetricsName),
+		view.WithSetDescription(gcSysMetricsDescription),
+		view.WithSetAggregation(aggregation.LastValue{}),
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	otherSys, err := view.New(
+		view.MatchInstrumentName(otherSysMetricsName),
+		view.WithSetDescription(otherSysMetricsDescription),
+		view.WithSetAggregation(aggregation.LastValue{}),
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	nextGcSys, err := view.New(
+		view.MatchInstrumentName(nextGcSysMetricsName),
+		view.WithSetDescription(nextGcSysMetricsDescription),
 		view.WithSetAggregation(aggregation.LastValue{}),
 	)
 	if err != nil {
@@ -200,10 +329,38 @@ func (*memoryMetrics) View() ([]*metrics.View, error) {
 		return nil, err
 	}
 
+	numForcedGC, err := view.New(
+		view.MatchInstrumentName(numForcedGCMetricsName),
+		view.WithSetDescription(numForcedGCMetricsDescription),
+		view.WithSetAggregation(aggregation.LastValue{}),
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	heapWillReturn, err := view.New(
+		view.MatchInstrumentName(heapWillReturnMetricsName),
+		view.WithSetDescription(heapWillReturnMetricsDescription),
+		view.WithSetAggregation(aggregation.LastValue{}),
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	liveObjects, err := view.New(
+		view.MatchInstrumentName(liveObjectsMetricsName),
+		view.WithSetDescription(liveObjectsMetricsDescription),
+		view.WithSetAggregation(aggregation.LastValue{}),
+	)
+	if err != nil {
+		return nil, err
+	}
+
 	return []*metrics.View{
 		&alloc,
 		&totalAlloc,
 		&sys,
+		&lookups,
 		&mallocs,
 		&frees,
 		&heapAlloc,
@@ -211,10 +368,20 @@ func (*memoryMetrics) View() ([]*metrics.View, error) {
 		&heapIdle,
 		&heapInuse,
 		&heapReleased,
+		&heapObjects,
 		&stackInuse,
 		&stackSys,
+		&mspanInuse,
+		&mspanSys,
+		&buckHashSys,
+		&gcSys,
+		&otherSys,
+		&nextGcSys,
 		&pauseTotalMs,
 		&numGC,
+		&numForcedGC,
+		&heapWillReturn,
+		&liveObjects,
 	}, nil
 }
 
@@ -229,7 +396,7 @@ func (*memoryMetrics) Register(m metrics.Meter) error {
 	}
 
 	totalAlloc, err := m.AsyncInt64().Gauge(
-		totalAllocMetricsDescription,
+		totalAllocMetricsName,
 		metrics.WithDescription(totalAllocMetricsDescription),
 		metrics.WithUnit(metrics.Bytes),
 	)
@@ -245,6 +412,12 @@ func (*memoryMetrics) Register(m metrics.Meter) error {
 	if err != nil {
 		return err
 	}
+
+	looksup, err := m.AsyncInt64().Gauge(
+		lookupsMetricsName,
+		metrics.WithDescription(lookupsMetricsDescription),
+		metrics.WithUnit(metrics.Dimensionless),
+	)
 
 	mallocs, err := m.AsyncInt64().Gauge(
 		mallocsMetricsName,
@@ -309,6 +482,15 @@ func (*memoryMetrics) Register(m metrics.Meter) error {
 		return err
 	}
 
+	heapObjects, err := m.AsyncInt64().Gauge(
+		heapObjectsMetricsName,
+		metrics.WithDescription(heapObjectsMetricsDescription),
+		metrics.WithUnit(metrics.Dimensionless),
+	)
+	if err != nil {
+		return err
+	}
+
 	stackInuse, err := m.AsyncInt64().Gauge(
 		stackInuseMetricsName,
 		metrics.WithDescription(stackInuseMetricsDescription),
@@ -327,6 +509,78 @@ func (*memoryMetrics) Register(m metrics.Meter) error {
 		return err
 	}
 
+	mspanInuse, err := m.AsyncInt64().Gauge(
+		mspanInuseMetricsName,
+		metrics.WithDescription(mspanInuseMetricsDescription),
+		metrics.WithUnit(metrics.Bytes),
+	)
+	if err != nil {
+		return err
+	}
+
+	mspanSys, err := m.AsyncInt64().Gauge(
+		mspanSysMetricsName,
+		metrics.WithDescription(mspanSysMetricsDescription),
+		metrics.WithUnit(metrics.Bytes),
+	)
+	if err != nil {
+		return err
+	}
+
+	mcacheInuse, err := m.AsyncInt64().Gauge(
+		mcacheInuseMetricsName,
+		metrics.WithDescription(mcacheInuseMetricsDescription),
+		metrics.WithUnit(metrics.Bytes),
+	)
+	if err != nil {
+		return err
+	}
+
+	mcacheSys, err := m.AsyncInt64().Gauge(
+		mcacheSysMetricsName,
+		metrics.WithDescription(mcacheSysMetricsDescription),
+		metrics.WithUnit(metrics.Bytes),
+	)
+	if err != nil {
+		return err
+	}
+
+	buckHashSys, err := m.AsyncInt64().Gauge(
+		buckHashSysMetricsName,
+		metrics.WithDescription(buckHashSysMetricsDescription),
+		metrics.WithUnit(metrics.Bytes),
+	)
+	if err != nil {
+		return err
+	}
+
+	gcSys, err := m.AsyncInt64().Gauge(
+		gcSysMetricsName,
+		metrics.WithDescription(gcSysMetricsDescription),
+		metrics.WithUnit(metrics.Bytes),
+	)
+	if err != nil {
+		return err
+	}
+
+	otherSys, err := m.AsyncInt64().Gauge(
+		otherSysMetricsName,
+		metrics.WithDescription(otherSysMetricsDescription),
+		metrics.WithUnit(metrics.Bytes),
+	)
+	if err != nil {
+		return err
+	}
+
+	nextGcSys, err := m.AsyncInt64().Gauge(
+		nextGcSysMetricsName,
+		metrics.WithDescription(nextGcSysMetricsDescription),
+		metrics.WithUnit(metrics.Bytes),
+	)
+	if err != nil {
+		return err
+	}
+
 	pauseTotalMs, err := m.AsyncInt64().Gauge(
 		pauseTotalMsMetricsName,
 		metrics.WithDescription(pauseTotalMsMetricsDescription),
@@ -339,7 +593,34 @@ func (*memoryMetrics) Register(m metrics.Meter) error {
 	numGC, err := m.AsyncInt64().Gauge(
 		numGCMetricsName,
 		metrics.WithDescription(numGCMetricsDescription),
+		metrics.WithUnit(metrics.Dimensionless),
+	)
+	if err != nil {
+		return err
+	}
+
+	numForcedGC, err := m.AsyncInt64().Gauge(
+		numForcedGCMetricsName,
+		metrics.WithDescription(numForcedGCMetricsDescription),
+		metrics.WithUnit(metrics.Dimensionless),
+	)
+	if err != nil {
+		return err
+	}
+
+	heapWillReturn, err := m.AsyncInt64().Gauge(
+		heapWillReturnMetricsName,
+		metrics.WithDescription(heapWillReturnMetricsDescription),
 		metrics.WithUnit(metrics.Bytes),
+	)
+	if err != nil {
+		return err
+	}
+
+	liveObjects, err := m.AsyncInt64().Gauge(
+		liveObjectsMetricsName,
+		metrics.WithDescription(liveObjectsMetricsDescription),
+		metrics.WithUnit(metrics.Dimensionless),
 	)
 	if err != nil {
 		return err
@@ -350,6 +631,7 @@ func (*memoryMetrics) Register(m metrics.Meter) error {
 			alloc,
 			totalAlloc,
 			sys,
+			looksup,
 			mallocs,
 			frees,
 			heapAlloc,
@@ -357,10 +639,22 @@ func (*memoryMetrics) Register(m metrics.Meter) error {
 			heapIdle,
 			heapInuse,
 			heapReleased,
+			heapObjects,
 			stackInuse,
 			stackSys,
+			mspanInuse,
+			mspanSys,
+			mcacheInuse,
+			mcacheSys,
+			buckHashSys,
+			gcSys,
+			otherSys,
+			nextGcSys,
 			pauseTotalMs,
 			numGC,
+			numForcedGC,
+			heapWillReturn,
+			liveObjects,
 		},
 		func(ctx context.Context) {
 			var mstats runtime.MemStats
@@ -369,6 +663,7 @@ func (*memoryMetrics) Register(m metrics.Meter) error {
 			alloc.Observe(ctx, int64(mstats.Alloc))
 			totalAlloc.Observe(ctx, int64(mstats.TotalAlloc))
 			sys.Observe(ctx, int64(mstats.Sys))
+			looksup.Observe(ctx, int64(mstats.Lookups))
 			mallocs.Observe(ctx, int64(mstats.Mallocs))
 			frees.Observe(ctx, int64(mstats.Frees))
 			heapAlloc.Observe(ctx, int64(mstats.HeapAlloc))
@@ -376,15 +671,29 @@ func (*memoryMetrics) Register(m metrics.Meter) error {
 			heapIdle.Observe(ctx, int64(mstats.HeapIdle))
 			heapInuse.Observe(ctx, int64(mstats.HeapInuse))
 			heapReleased.Observe(ctx, int64(mstats.HeapReleased))
+			heapObjects.Observe(ctx, int64(mstats.HeapObjects))
 			stackInuse.Observe(ctx, int64(mstats.StackInuse))
 			stackSys.Observe(ctx, int64(mstats.StackSys))
+			mspanInuse.Observe(ctx, int64(mstats.MSpanInuse))
+			mspanSys.Observe(ctx, int64(mstats.MSpanSys))
+			mcacheInuse.Observe(ctx, int64(mstats.MCacheInuse))
+			mcacheSys.Observe(ctx, int64(mstats.MCacheSys))
+			buckHashSys.Observe(ctx, int64(mstats.BuckHashSys))
+			gcSys.Observe(ctx, int64(mstats.GCSys))
+			otherSys.Observe(ctx, int64(mstats.OtherSys))
+			nextGcSys.Observe(ctx, int64(mstats.NextGC))
+
 
 			ptMs := int64(0)
 			if mstats.PauseTotalNs > 0 {
 				ptMs = int64(mstats.PauseTotalNs / uint64(time.Millisecond))
 			}
 			pauseTotalMs.Observe(ctx, ptMs)
-			numGC.Observe(ctx, int64(mstats.NextGC))
+			numGC.Observe(ctx, int64(mstats.NumGC))
+			numForcedGC.Observe(ctx, int64(mstats.NumForcedGC))
+			
+			heapWillReturn.Observe(ctx, int64(mstats.HeapIdle - mstats.HeapReleased))
+			liveObjects.Observe(ctx, int64(mstats.Mallocs - mstats.Frees))
 		},
 	)
 }
