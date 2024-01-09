e7a6ab9c0084e0ede1e68ea9cf8bb392ffd3f773
hlts2 <hiroto.funakoshi.hiroto@gmail.com>
2024-01-09 11:00:19 +0900

    fix: schema and proto update and format
    Signed-off-by: hlts2 <hiroto.funakoshi.hiroto@gmail.com>

--- internal/hash/hash.go
+++ internal/hash/hash.go
@@ -1,10 +1,11 @@
-// Copyright (C) 2019-2023 vdaas.org vald team <vald@vdaas.org>
+//
+// Copyright (C) 2019-2024 vdaas.org vald team <vald@vdaas.org>
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // You may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//	https://www.apache.org/licenses/LICENSE-2.0
+//    https://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
