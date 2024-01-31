7d1b3f84169d41bfa26c4664ba8771051c97d08b
ykadowak <yusuke.kadowaki.1231@gmail.com>
2024-01-31 02:36:02 +0000

    Apply format

--- .devcontainer/postAttachCommand.sh
+++ .devcontainer/postAttachCommand.sh
@@ -1,4 +1,22 @@
 #!/bin/bash -eu
+
+#
+# Copyright (C) 2019-2024 vdaas.org vald team <vald@vdaas.org>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# You may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+:
+
 #
 # This script is executed as postAttachCommand in devcontainer.json
 # This script does...
@@ -6,7 +24,7 @@
 # - add command history setting to .zshrc to persist history
 #
 
-echo "creating symbolic link of config ZSHRC..."
+echo "creating symbolic link of config..."
 
 LINK_TARGET="$(pwd)/cmd/agent/core/ngt/sample.yaml"
 LINK_SRC="/etc/server/config.yaml"
