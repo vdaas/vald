c15fe7374233a7e600e3d0d363a4b73261b9200d
ykadowak <yusuke.kadowaki.1231@gmail.com>
2024-02-14 07:54:28 +0000

    Add pod and job controllers

--- pkg/index/operator/service/operator.go
+++ pkg/index/operator/service/operator.go
@@ -17,9 +17,10 @@ import (
 	"context"
 	"reflect"
 
-	agent "github.com/vdaas/vald/apis/grpc/v1/agent/core"
 	"github.com/vdaas/vald/internal/errors"
 	"github.com/vdaas/vald/internal/k8s"
+	"github.com/vdaas/vald/internal/k8s/job"
+	"github.com/vdaas/vald/internal/k8s/pod"
 	"github.com/vdaas/vald/internal/log"
 	"github.com/vdaas/vald/internal/observability/trace"
 	"github.com/vdaas/vald/internal/safety"
@@ -27,8 +28,7 @@ import (
 )
 
 const (
-	apiName        = "vald/index/operator"
-	grpcMethodName = "core.v1.Agent/" + agent.CreateIndexRPCName
+	apiName = "vald/index/operator"
 )
 
 // Operator represents an interface for indexing.
@@ -37,8 +37,9 @@ type Operator interface {
 }
 
 type operator struct {
-	ctrl k8s.Controller
-	eg   errgroup.Group
+	ctrl      k8s.Controller
+	eg        errgroup.Group
+	namespace string
 }
 
 // New returns Indexer object if no error occurs.
@@ -56,7 +57,40 @@ func New(opts ...Option) (o Operator, err error) {
 		}
 	}
 
-	operator.ctrl, err = k8s.New()
+	var k8sOpts []k8s.Option
+	podOpts := k8s.WithResourceController(pod.New(
+		pod.WithControllerName("pod reconciler for index operator"),
+		pod.WithOnErrorFunc(func(err error) {
+			log.Error("failed to reconcile:", err)
+		}),
+		pod.WithNamespace(operator.namespace),
+		// TODO:
+		pod.WithOnReconcileFunc(func(podList map[string][]pod.Pod) {
+			log.Debugf("reconciled pod list: %v", podList)
+		}),
+		// pod.WithFields(selector.GetPodFields()),
+		// pod.WithLabels(selector.GetPodLabels()),
+	))
+	k8sOpts = append(k8sOpts, podOpts)
+
+	jobController, err := job.New(
+		job.WithControllerName("job reconciler for index operator"),
+		job.WithNamespaces(operator.namespace),
+		job.WithOnErrorFunc(func(err error) {
+			log.Errorf("failed to reconcile job resource:", err)
+		}),
+		// TODO:
+		job.WithOnReconcileFunc(func(ctx context.Context, jobList map[string][]job.Job) {
+			log.Debugf("reconciled job list: %v", jobList)
+		}),
+	)
+	if err != nil {
+		return nil, err
+	}
+	jobOpts := k8s.WithResourceController(jobController)
+	k8sOpts = append(k8sOpts, jobOpts)
+
+	operator.ctrl, err = k8s.New(k8sOpts...)
 	if err != nil {
 		return nil, err
 	}
@@ -65,7 +99,7 @@ func New(opts ...Option) (o Operator, err error) {
 
 // Start starts indexing process.
 func (o *operator) Start(ctx context.Context) (<-chan error, error) {
-	ctx, span := trace.StartSpan(ctx, apiName+"/service/index.Start")
+	ctx, span := trace.StartSpan(ctx, apiName+"/service/operator.Start")
 	defer func() {
 		if span != nil {
 			span.End()
