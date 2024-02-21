ec807b9e4e5faf14f2d9b3b5cbe8a70d1a82e250
ykadowak <yusuke.kadowaki.1231@gmail.com>
2024-02-21 04:40:16 +0000

    remove unused parameters

--- internal/config/index_operator.go
+++ internal/config/index_operator.go
@@ -15,36 +15,18 @@ package config
 
 // IndexCreation represents the configurations for index creation.
 type IndexOperator struct {
-	// AgentPort represent agent port number
-	AgentPort int `json:"agent_port" yaml:"agent_port"`
-
 	// AgentName represent agents meta_name for service discovery
 	AgentName string `json:"agent_name" yaml:"agent_name"`
 
 	// AgentNamespace represent agent namespace location
 	AgentNamespace string `json:"agent_namespace" yaml:"agent_namespace"`
 
-	// AgentDNS represent agents dns A record for service discovery
-	AgentDNS string `json:"agent_dns" yaml:"agent_dns"`
-
-	// NodeName represents node name
-	NodeName string `json:"node_name" yaml:"node_name"`
-
 	// Concurrency represents indexing concurrency.
 	Concurrency int `json:"concurrency" yaml:"concurrency"`
-
-	// CreationPoolSize represents batch pool size for indexing.
-	CreationPoolSize uint32 `json:"creation_pool_size" yaml:"creation_pool_size"`
-
-	// TargetAddrs represents indexing target addresses.
-	TargetAddrs []string `json:"target_addrs" yaml:"target_addrs"`
 }
 
 func (ic *IndexOperator) Bind() *IndexOperator {
 	ic.AgentName = GetActualValue(ic.AgentName)
 	ic.AgentNamespace = GetActualValue(ic.AgentNamespace)
-	ic.AgentDNS = GetActualValue(ic.AgentDNS)
-	ic.NodeName = GetActualValue(ic.NodeName)
-	ic.TargetAddrs = GetActualValues(ic.TargetAddrs)
 	return ic
 }
