78218d221ef504ef68262c327aeb43d497f7ce68
Kosuke Morimoto <ksk@vdaas.org>
2024-03-08 17:53:21 +0900

    add new idea of Base trait
    Signed-off-by: Kosuke Morimoto <ksk@vdaas.org>

--- rust/libs/algorithm/src/base.rs
+++ rust/libs/algorithm/src/base.rs
@@ -1,37 +1,56 @@
-pub mod algorithm {
+pub mod algorithm1 {
     use anyhow::Result;
 
-    pub trait Base<T, U, SP, SR, IP, IR, UP, UR, RP, RR, CP, CR, P> {
-        fn search(&self, v: &Vec<T>, p: Option<SP>) -> Result<SR>;
-        fn insert(&self, v: &Vec<T>, id: &U, p: Option<IP>) -> Result<IR>;
-        fn update(&self, v: &Vec<T>, id: &U, p: Option<UP>) -> Result<UR>;
+    pub trait Base<T, U, SP, SR, IP, IR, UP, UR, RP, RR, CP, CR, P>: Send + Sync
+    where
+        T: num::Num,
+        U: num::sign::Unsigned,
+    {
+        fn search(&self, v: &[T], p: Option<SP>) -> Result<SR>;
+        fn insert(&self, v: &[T], id: &U, p: Option<IP>) -> Result<IR>;
+        fn update(&self, v: &[T], id: &U, p: Option<UP>) -> Result<UR>;
         fn remove(&self, id: &U, p: Option<RP>) -> Result<RR>;
         fn commit(&self, p: Option<CP>) -> Result<CR>;
-
-        fn new(p: Option<P>) -> Result<Self>;
     }
 }
 
 pub mod algorithm2 {
     use anyhow::Result;
 
-    pub trait Search<T, P, R> {
+    pub trait Search<T: num::Num, P, R>: Send + Sync {
         fn search(&self, v: &[T], p: Option<P>) -> Result<R>;
     }
 
-    pub trait Insert<T, U, P, R> {
+    pub trait Insert<T: num::Num, U: num::sign::Unsigned, P, R>: Send + Sync {
         fn insert(&self, v: &[T], id: &U, p: Option<P>) -> Result<R>;
     }
 
-    pub trait Update<T, U, P, R> {
+    pub trait Update<T: num::Num, U: num::sign::Unsigned, P, R>: Send + Sync {
         fn update(&self, v: &[T], id: &U, p: Option<P>) -> Result<R>;
     }
 
-    pub trait Remove<U, P, R> {
+    pub trait Remove<U: num::sign::Unsigned, P, R>: Send + Sync {
         fn remove(&self, id: &U, p: Option<P>) -> Result<R>;
     }
 
-    pub trait Commit<P, R> {
+    pub trait Commit<P, R>: Send + Sync {
         fn commit(&self, p: Option<P>) -> Result<R>;
+    }
+}
+
+// inspired by https://github.com/jonluca/hora
+pub mod algorithm3 {
+    use anyhow::Result;
+
+    pub trait Base<T, U, SP, SR, IP, UP, RP, CP>: Send + Sync
+    where
+        T: num::Num,
+        U: num::sign::Unsigned,
+    {
+        fn search(&self, v: &[T], p: Option<SP>) -> Result<Vec<SR>>;
+        fn insert(&mut self, v: &[T], id: &U, p: Option<IP>) -> Result<()>;
+        fn update(&mut self, v: &[T], id: &U, p: Option<UP>) -> Result<()>;
+        fn remove(&mut self, id: &U, p: Option<RP>) -> Result<()>;
+        fn commit(&mut self, p: Option<CP>) -> Result<()>;
     }
 }
