43c35c28c93a1abd08abe70c124153984f3f3bd3
datelier <57349093+datelier@users.noreply.github.com>
2024-07-10 16:34:54 +0900

    add Faiss BinaryIndex

--- internal/core/algorithm/faiss/Capi.h
+++ internal/core/algorithm/faiss/Capi.h
@@ -20,6 +20,7 @@ extern "C" {
   #include <stdio.h>
   #include <stdlib.h>
   #include <stdbool.h>
+  #include <stdint.h>
 
   typedef void* FaissQuantizer;
   typedef void* FaissIndex;
@@ -33,19 +34,58 @@ extern "C" {
       const int nlist,
       const int m,
       const int nbits_per_idx,
+      const int method_type,
       const int metric_type);
-  FaissStruct* faiss_read_index(const char* fname);
+  FaissStruct* faiss_create_index_ivfpq(
+      const int d,
+      const int nlist,
+      const int m,
+      const int nbits_per_idx,
+      const int metric_type);
+  FaissStruct* faiss_create_index_binaryivf(
+      const int d,
+      const int nlist);
+  FaissStruct* faiss_read_index(const char* fname, const int method_type);
+  FaissStruct* faiss_read_index_ivfpq(const char* fname);
+  FaissStruct* faiss_read_index_binaryindex(const char* fname);
   bool faiss_write_index(
+      const FaissStruct* st,
+      const char* fname,
+      const int method_type);
+  bool faiss_write_index_ivfpq(
+      const FaissStruct* st,
+      const char* fname);
+  bool faiss_write_index_binaryivf(
       const FaissStruct* st,
       const char* fname);
   bool faiss_train(
       const FaissStruct* st,
       const int nb,
+      const float* xb,
+      const int method_type);
+  bool faiss_train_ivfpq(
+      const FaissStruct* st,
+      const int nb,
       const float* xb);
+  bool faiss_train_binaryivf(
+      const FaissStruct* st,
+      const int nb,
+      const uint8_t* xb);
   int faiss_add(
       const FaissStruct* st,
       const int nb,
       const float* xb,
+      const long int* xids,
+      const int method_type);
+  int faiss_add_ivfpq(
+      const FaissStruct* st,
+      const int nb,
+      const float* xb,
+      const long int* xids);
+  int faiss_add_binaryivf(
+      const FaissStruct* st,
+      const int nb,
+      const uint8_t* xb,
       const long int* xids);
   bool faiss_search(
       const FaissStruct* st,
@@ -53,8 +93,32 @@ extern "C" {
       const int nq,
       const float* xq,
       long* I,
+      float* D,
+      const int method_type);
+  bool faiss_search_ivfpq(
+      const FaissStruct* st,
+      const int k,
+      const int nq,
+      const float* xq,
+      long* I,
+      float* D);
+  bool faiss_search_binaryivf(
+      const FaissStruct* st,
+      const int k,
+      const int nq,
+      const uint8_t* xq,
+      long* I,
       float* D);
   int faiss_remove(
+      const FaissStruct* st,
+      const int size,
+      const long int* ids,
+      const int method_type);
+  int faiss_remove_ivfpq(
+      const FaissStruct* st,
+      const int size,
+      const long int* ids);
+  int faiss_remove_binaryivf(
       const FaissStruct* st,
       const int size,
       const long int* ids);
