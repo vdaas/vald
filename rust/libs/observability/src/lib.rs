608320dc71a637690f85b02d00350caf5702f695
hlts2 <hiroto.funakoshi.hiroto@gmail.com>
2024-07-24 16:19:23 +0900

    feat: add module files
    Signed-off-by: hlts2 <hiroto.funakoshi.hiroto@gmail.com>

--- rust/libs/observability/src/lib.rs
+++ rust/libs/observability/src/lib.rs
@@ -13,17 +13,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 //
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+pub mod config;
+pub mod macros;
+pub mod observability;
