4f1938dd5ae5425aa7e13ffbc59fdc9903868dec
Kosuke Morimoto <ksk@vdaas.org>
2024-07-31 13:33:51 +0900

    fix
    Signed-off-by: Kosuke Morimoto <ksk@vdaas.org>

--- rust/libs/kvs/benches/hand_written.rs
+++ rust/libs/kvs/benches/hand_written.rs
@@ -10,7 +10,7 @@ use procfs::process::Status;
 use rand::{thread_rng, Rng};
 use util::{random_bytes, setup_kvs};
 
-fn parallel_bencher<T: KVS + 'static>(name: &str, path: &Path, size: usize, kdim: usize, vdim: usize, db: T, ratio: f64, thread: usize, interval: u64, timer: u64) -> Vec<(usize, Duration, Status, u64)> {
+fn bencher<T: KVS + 'static>(name: &str, path: &Path, size: usize, kdim: usize, vdim: usize, db: T, ratio: f64, thread: usize, interval: u64, timer: u64) -> Vec<(usize, Duration, Status, u64)> {
     let me = Arc::new(procfs::process::Process::myself().unwrap());
     let shutdown = Arc::new(AtomicBool::new(false));
     let begin = Arc::new(Instant::now());
@@ -31,13 +31,13 @@ fn parallel_bencher<T: KVS + 'static>(name: &str, path: &Path, size: usize, kdim
                     v if v <= ratio => {
                         let mut key = set_count.fetch_add(1, Ordering::Release).to_ne_bytes().to_vec();
                         key.resize_with(kdim, Default::default);
-                        db.set(&key, &random_bytes(vdim)).unwrap();
+                        db.set(&key, &random_bytes(vdim)).unwrap_or_default();
                     }
                     _ => {
                         get_count.fetch_add(1, Ordering::Release);
                         let mut key = rng.gen_range(0..size).to_ne_bytes().to_vec();
                         key.resize_with(kdim, Default::default);
-                        db.get(&key).unwrap();
+                        db.get(&key).unwrap_or_default();
                     }
                 }
             }
@@ -58,7 +58,7 @@ fn parallel_bencher<T: KVS + 'static>(name: &str, path: &Path, size: usize, kdim
         thread::sleep(Duration::from_secs(interval));
         let p = begin.elapsed();
         let st = me.status().unwrap();
-        let dir_size = fs_extra::dir::get_size(path).unwrap();
+        let dir_size = fs_extra::dir::get_size(path).unwrap_or_default();
         let set_count = set_count.load(Ordering::Relaxed);
         println!("{},set,{},{},{},{},{},{},{}", name, kdim, vdim, thread, set_count, p.as_nanos(), st.vmrss.unwrap(), dir_size/1024);
         let get_count = get_count.load(Ordering::Relaxed);
@@ -75,68 +75,32 @@ fn parallel_bencher<T: KVS + 'static>(name: &str, path: &Path, size: usize, kdim
     progress
 }
 
-fn parallel_benchmark<T: KVS + 'static>(size: usize, kdims: &[usize], vdims: &[usize], threads: &[usize], ratio: f64, interval: u64, timer: u64) {
+fn benchmark<T: KVS + 'static>(size: usize, kdims: &[usize], vdims: &[usize], threads: &[usize], ratio: f64, interval: u64, timer: u64) {
     let name = type_name::<T>().split("::").last().unwrap();
     for &kdim in kdims {
         for &vdim in vdims {
             for &thread in threads {
                 let (path, db) = setup_kvs::<T>(format!("{}-{}-{}", kdim, vdim, thread).as_str());
-                parallel_bencher(name, path.as_path(), size, kdim, vdim, db, ratio, thread, interval, timer);
+                bencher(name, path.as_path(), size, kdim, vdim, db, ratio, thread, interval, timer);
             }
-        }
-    }
-}
-
-fn bencher(name: &str, operation: &str, path: &Path, size: usize, kdim: usize, vdim: usize, f: impl Fn(&[u8])) -> Vec<(usize, Duration, Status, u64)> {
-    let me = procfs::process::Process::myself().unwrap();
-    let mut progress = vec![];
-    let begin = Instant::now();
-    for i in 0..size {
-        let mut arg = i.to_ne_bytes().to_vec();
-        arg.resize_with(kdim, Default::default);
-        f(&arg);
-        if i & (i + 1) == 0 && i + 1 >= 1 << 14 {
-            let p = begin.elapsed();
-            let st = me.status().unwrap();
-            let dir_size = fs_extra::dir::get_size(path).unwrap();
-            println!("{},{},{},{},1,{},{},{},{}", name, operation, kdim, vdim, i+1, p.as_nanos(), st.vmrss.unwrap(), dir_size/1024);
-            progress.push((i+1, p, st, dir_size));
-        }
-    }
-    progress
-}
-
-fn monotonic_benchmark<T: KVS + 'static>(size: usize, kdims: &[usize], vdims: &[usize]) {
-    let name = type_name::<T>().split("::").last().unwrap();
-    for &kdim in kdims {
-        for &vdim in vdims {
-            let (path, db) = setup_kvs::<T>(format!("{}-{}", kdim, vdim).as_str());
-            bencher(name, "set", path.as_path(), size, kdim, vdim, |k| { db.set(k, &random_bytes(vdim)).unwrap(); });
-            bencher(name, "get", path.as_path(), size, kdim, vdim, |k| { db.get(k).unwrap(); });
         }
     }
 }
 
 fn main() {
     let size = 1 << 24;
-    let kdims: &[usize] = &[1 << 3, 1 << 10];
-    let vdims: &[usize] = &[1 << 8, 1 << 12];
-    let threads: &[usize] = &[1, 16, 32];
+    let kdims: &[usize] = &[1 << 2, 1 << 4, 1 << 6, 1 << 8, 1 << 10, 1 << 12, 1 << 14];
+    let vdims: &[usize] = &[1 << 10];
+    let threads: &[usize] = &[16];
     let ratio = 0.5;
-    let interval = 3;
-    let timer = 30;
+    let interval = 5;
+    let timer = 15;
     println!("name,operation,key size(B),value size(B),thread,operation count,time(ns),vmrss(KB),file size(B)");
-    //monotonic_benchmark::<Kv>(size, kdims, vdims);
-    //monotonic_benchmark::<Persy>(size, kdims, vdims);
-    //monotonic_benchmark::<Redb>(size, kdims, vdims);
-    //monotonic_benchmark::<Rkv>(size, kdims, vdims);
-    //monotonic_benchmark::<Rocksdb>(size, kdims, vdims);
-    //monotonic_benchmark::<Sled>(size, kdims, vdims);
-    parallel_benchmark::<Kv>(size, kdims, vdims, threads, ratio, interval, timer);
-    parallel_benchmark::<Kv2>(size, kdims, vdims, threads, ratio, interval, timer);
+    benchmark::<Kv>(size, kdims, vdims, threads, ratio, interval, timer);
+    benchmark::<Kv2>(size, kdims, vdims, threads, ratio, interval, timer);
     //parallel_benchmark::<Persy>(size, kdims, vdims, threads, ratio, interval, timer);
     //parallel_benchmark::<Redb>(size, kdims, vdims, threads, ratio, interval, timer);
     //parallel_benchmark::<Rkv>(size, kdims, vdims, threads);
-    parallel_benchmark::<Rocksdb>(size, kdims, vdims, threads, ratio, interval, timer);
-    parallel_benchmark::<Sled>(size, kdims, vdims, threads, ratio, interval, timer);
+    benchmark::<Rocksdb>(size, kdims, vdims, threads, ratio, interval, timer);
+    benchmark::<Sled>(size, kdims, vdims, threads, ratio, interval, timer);
 }
