e94820142389313e8e9624c851cdfb976830d160
Kosuke Morimoto <ksk@vdaas.org>
2024-09-06 18:39:55 +0900

    fix
    Signed-off-by: Kosuke Morimoto <ksk@vdaas.org>

--- internal/backoff/backoff.go
+++ internal/backoff/backoff.go
@@ -185,23 +185,23 @@ func (b *backoff) Do(
 				} else {
 					dur *= b.backoffFactor
 					jdur = b.addJitter(dur)
-					select {
-					case <-dctx.Done():
-						switch dctx.Err() {
-						case context.DeadlineExceeded:
-							log.Debugf("[backoff]\tfor: "+name+",\tDeadline Exceeded\terror: %v", err.Error())
-							return nil, errors.ErrBackoffTimeout(err)
-						case context.Canceled:
-							log.Debugf("[backoff]\tfor: "+name+",\tCanceled\terror: %v", err.Error())
-							return nil, err
-						default:
-							return nil, errors.Join(dctx.Err(), err)
-						}
-					default:
-					}
 				}
 			}
 		}
+	}
+	select {
+	case <-dctx.Done():
+		switch dctx.Err() {
+		case context.DeadlineExceeded:
+			log.Debugf("[backoff]\tfor: "+name+",\tDeadline Exceeded\terror: %v", err.Error())
+			return nil, errors.ErrBackoffTimeout(err)
+		case context.Canceled:
+			log.Debugf("[backoff]\tfor: "+name+",\tCanceled\terror: %v", err.Error())
+			return nil, err
+		default:
+			return nil, errors.Join(dctx.Err(), err)
+		}
+	default:
 	}
 	return res, err
 }
