be43a933d68b502a9503b7c8b8efc52f2066ef6f
smorihira <mos.aif.0924@gmail.com>
2024-09-11 12:42:10 +0900

    feat: add init and init tracer

--- rust/bin/meta/src/main.rs
+++ rust/bin/meta/src/main.rs
@@ -16,15 +16,69 @@
 
 mod handler;
 
+use opentelemetry::global;
+use opentelemetry::propagation::Extractor;
+use tonic::transport::Server;
+use tonic::Request;
+use observability::{config::Config, observability::{Observability, ObservabilityImpl}};
+
+struct MetadataMap<'a>(&'a tonic::metadata::MetadataMap);
+
+impl<'a> Extractor for MetadataMap<'a> {
+    fn get(&self, key: &str) -> Option<&str> {
+        self.0.get(key).and_then(|metadata| metadata.to_str().ok())
+    }
+
+    fn keys(&self) -> Vec<&str> {
+        self.0
+            .keys()
+            .map(|key| match key {
+                tonic::metadata::KeyRef::Ascii(v) => v.as_str(),
+                tonic::metadata::KeyRef::Binary(v) => v.as_str(),
+            })
+            .collect::<Vec<_>>()    }
+}
+
+fn intercept(mut req: Request<()>) -> Result<Request<()>, tonic::Status> {
+    let parent_cx = global::get_text_map_propagator(|prop| {
+        prop.extract(&MetadataMap(req.metadata()))
+    });
+    req.extensions_mut().insert(parent_cx);
+    Ok(req)
+}
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // TODO: yaml is given from outside
+    // let config_yaml = r#"enabled: false
+    // endpoint: ""
+    // attributes: []
+    // tracer:
+    //     enabled: false
+    // meter:
+    //     enabled: false
+    //     export_duration:
+    //         secs: 60
+    //         nanos: 0
+    //     export_timeout_duration:
+    //         secs: 30
+    //         nanos: 0
+    // "#;
+    //
+    // decode config yaml
+    // let observability_cfg = serde_yaml::from_str(config_yaml).unwrap();
+    let observability_cfg = Config::default();
+    let mut observability = ObservabilityImpl::new(observability_cfg)?;
+
     let addr = "[::1]:8081".parse()?;
-    let meta = handler::Meta::new().expect("Failed to initialize Meta service");
+    let cfg_path = "/var/lib/meta/database"; // TODO: set the appropriate path
+    let meta = handler::Meta::new(cfg_path)?;
 
     tonic::transport::Server::builder()
-        .add_service(proto::meta::v1::meta_server::MetaServer::new(meta))
+        .add_service(proto::meta::v1::meta_server::MetaServer::with_interceptor(meta, intercept))
         .serve(addr)
         .await?;
 
+    observability.shutdown()?;
     Ok(())
 }
