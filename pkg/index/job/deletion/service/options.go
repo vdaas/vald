019bf55573f0c24167eb9f7886d3e4629157b99f
HighPon <s.shiraki.business@gmail.com>
2024-10-13 01:09:33 +0000

    feat: Implement delete expired index job

---
+++ pkg/index/job/deletion/service/options.go
@@ -0,0 +1,40 @@
+package service
+
+import (
+	"github.com/vdaas/vald/internal/client/v1/client/discoverer"
+	"github.com/vdaas/vald/internal/errors"
+)
+
+type Option func(_ *index) error
+
+// WithDiscoverer returns Option that sets discoverer client.
+func WithDiscoverer(client discoverer.Client) Option {
+	return func(idx *index) error {
+		if client == nil {
+			return errors.NewErrCriticalOption("discoverer", client)
+		}
+		idx.client = client
+		return nil
+	}
+}
+
+// WithIndexingConcurrency returns Option that sets indexing concurrency.
+func WithIndexingConcurrency(num int) Option {
+	return func(idx *index) error {
+		if num <= 0 {
+			return errors.NewErrInvalidOption("indexingConcurrency", num)
+		}
+		idx.concurrency = num
+		return nil
+	}
+}
+
+// WithTargetAddrs returns Option that sets indexing target addresses.
+func WithTargetAddrs(addrs ...string) Option {
+	return func(idx *index) error {
+		if len(addrs) != 0 {
+			idx.targetAddrs = append(idx.targetAddrs, addrs...)
+		}
+		return nil
+	}
+}
