a5ec5ee74078fc587db575577d8fd75d107e2019
HighPon <s.shiraki.business@gmail.com>
2024-11-18 04:17:07 +0000

    [WIP] feat: Implenet vald index export

---
+++ internal/config/index_exporter.go
@@ -0,0 +1,43 @@
+// Copyright (C) 2019-2024 vdaas.org vald team <vald@vdaas.org>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// You may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//	https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+package config
+
+// IndexExporter represents the configurations for index exportation.
+type IndexExporter struct {
+	// Concurrency represents indexing concurrency.
+	Concurrency int `json:"concurrency" yaml:"concurrency"`
+
+	// KVSBackgroundSyncInterval represents interval for checked id list kvs sync duration
+	KVSBackgroundSyncInterval string `json:"kvs_background_sync_interval" yaml:"kvs_background_sync_interval"`
+
+	// KVSBackgroundCompactionInterval represents interval for checked id list kvs compaction duration
+	KVSBackgroundCompactionInterval string `json:"kvs_background_compaction_interval" yaml:"kvs_background_compaction_interval"`
+
+	// IndexPath represents the export index file path
+	IndexPath string `json:"index_path,omitempty" yaml:"index_path"`
+
+	// Gateway represent gateway service configuration
+	Gateway *GRPCClient `json:"gateway" yaml:"gateway"`
+}
+
+func (e *IndexExporter) Bind() *IndexExporter {
+	e.KVSBackgroundCompactionInterval = GetActualValue(e.KVSBackgroundCompactionInterval)
+	e.KVSBackgroundSyncInterval = GetActualValue(e.KVSBackgroundSyncInterval)
+	e.IndexPath = GetActualValue(e.IndexPath)
+
+	if e.Gateway != nil {
+		e.Gateway = e.Gateway.Bind()
+	}
+	return e
+}
---
+++ pkg/index/job/exportation/service/options.go
@@ -0,0 +1,104 @@
+// Copyright (C) 2019-2024 vdaas.org vald team <vald@vdaas.org>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// You may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//	https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+package service
+
+import (
+	"github.com/vdaas/vald/internal/client/v1/client/vald"
+	"github.com/vdaas/vald/internal/errors"
+	"github.com/vdaas/vald/internal/sync/errgroup"
+	"github.com/vdaas/vald/internal/timeutil"
+)
+
+type Option func(_ *export) error
+
+var defaultOpts = []Option{
+	WithStreamListConcurrency(200),
+	WithKVSSyncInterval("5s"),
+	WithKVSCompactionInterval("5s"),
+	WithIndexPath("/var/export/index"),
+	WithErrGroup(errgroup.Get()),
+}
+
+// WithStreamListConcurrency returns Option that sets streamListConcurrency.
+func WithStreamListConcurrency(num int) Option {
+	return func(e *export) error {
+		if num <= 0 {
+			return errors.NewErrInvalidOption("streamListConcurrency", num)
+		}
+		e.streamListConcurrency = num
+		return nil
+	}
+}
+
+// WithKVSSyncInterval returns Option that sets interval for background file sync.
+func WithKVSSyncInterval(dur string) Option {
+	return func(e *export) error {
+		if dur == "" {
+			return nil
+		}
+		d, err := timeutil.Parse(dur)
+		if err != nil {
+			return err
+		}
+		e.backgroundSyncInterval = d
+		return nil
+	}
+}
+
+// WithKVSCompactionInterval returns Option that sets interval for background file compaction.
+func WithKVSCompactionInterval(dur string) Option {
+	return func(e *export) error {
+		if dur == "" {
+			return nil
+		}
+		d, err := timeutil.Parse(dur)
+		if err != nil {
+			return err
+		}
+		e.backgroundCompactionInterval = d
+		return nil
+	}
+}
+
+// WithIndexPath returns Option that sets indexPath.
+func WithIndexPath(path string) Option {
+	return func(e *export) error {
+		if path == "" {
+			return errors.NewErrInvalidOption("indexPath", path)
+		}
+		e.indexPath = path
+		return nil
+	}
+}
+
+// WithGateway returns Option that sets gateway client.
+func WithGateway(client vald.Client) Option {
+	return func(e *export) error {
+		if client == nil {
+			return errors.NewErrCriticalOption("gateway", client)
+		}
+		e.gateway = client
+		return nil
+	}
+}
+
+// WithErrGroup returns Option that set errgroup.
+func WithErrGroup(eg errgroup.Group) Option {
+	return func(e *export) error {
+		if eg != nil {
+			e.eg = eg
+		}
+		return nil
+	}
+}
