0a09fa4e864a0b7ae074423e569f4154dd03bc0c
Kosuke Morimoto <kou.morimoto@gmail.com>
2024-12-10 22:55:57 +0900

    :bug: fix minikube start failure

--- hack/docker/gen/main.go
+++ hack/docker/gen/main.go
@@ -508,6 +508,7 @@ var (
 		"make telepresence/install",
 		"make tparse/install",
 		"make yq/install",
+		"make docker-cli/install",
 	}
 
 	devContainerPreprocess = []string{
@@ -807,7 +808,7 @@ func main() {
 			ContainerType: CIContainer,
 			PackageDir:    "ci/base",
 			RuntimeUser:   defaultBuildUser,
-			ExtraPackages: append([]string{"npm"}, append(clangBuildDeps,
+			ExtraPackages: append([]string{"npm", "sudo"}, append(clangBuildDeps,
 				append(ngtBuildDeps,
 					append(faissBuildDeps,
 						append(rustBuildDeps,
