cc995eda91a21631c69cefb922a3e3af80a374e3
HighPon <s.shiraki.business@gmail.com>
2024-12-15 17:23:32 +0000

    fix: Delete index importation

--- Makefile.d/build.mk
+++ Makefile.d/build.mk
@@ -26,7 +26,6 @@ binary/build: \
 	cmd/index/job/creation/index-creation \
 	cmd/index/job/deletion/index-deletion \
 	cmd/index/job/exportation/index-exportation \
-	cmd/index/job/importation/index-importation \
 	cmd/index/job/readreplica/rotate/readreplica-rotate \
 	cmd/index/job/save/index-save \
 	cmd/index/operator/index-operator \
@@ -90,10 +89,6 @@ cmd/index/job/exportation/index-exportation:
 	$(eval CGO_ENABLED = 0)
 	$(call go-build,index/job/exportation,,-static,,,$@)
 
-cmd/index/job/importation/index-importation:
-	$(eval CGO_ENABLED = 0)
-	$(call go-build,index/job/importation,,-static,,,$@)
-
 cmd/index/job/save/index-save:
 	$(eval CGO_ENABLED = 0)
 	$(call go-build,index/job/save,,-static,,,$@)
@@ -139,7 +134,6 @@ binary/build/zip: \
 	artifacts/vald-index-creation-$(GOOS)-$(GOARCH).zip \
 	artifacts/vald-index-deletion-$(GOOS)-$(GOARCH).zip \
 	artifacts/vald-index-exportation-$(GOOS)-$(GOARCH).zip \
-	artifacts/vald-index-importation-$(GOOS)-$(GOARCH).zip \
 	artifacts/vald-index-operator-$(GOOS)-$(GOARCH).zip \
 	artifacts/vald-index-save-$(GOOS)-$(GOARCH).zip \
 	artifacts/vald-lb-gateway-$(GOOS)-$(GOARCH).zip \
@@ -200,10 +194,6 @@ artifacts/vald-index-deletion-$(GOOS)-$(GOARCH).zip: cmd/index/job/deletion/inde
 	zip --junk-paths $@ $<
 
 artifacts/vald-index-exporation-$(GOOS)-$(GOARCH).zip: cmd/index/job/exportation/index-exportation
-	$(call mkdir, $(dir $@))
-	zip --junk-paths $@ $<
-
-artifacts/vald-index-importation-$(GOOS)-$(GOARCH).zip: cmd/index/job/importation/index-importation
 	$(call mkdir, $(dir $@))
 	zip --junk-paths $@ $<
 
