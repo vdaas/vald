13f076934bd2913ae6e641ea84a935e053836a8e
Kosuke Morimoto <ksk@vdaas.org>
2025-02-19 18:02:28 +0900

    implement meta and apply observability to meta (#2823)
    * feat: implemented get and new
    
    * feat: implement delete and set
    
    * feat: add init and init tracer
    
    * feat: add observability config
    
    * feat: downgrade jeager and add client test
    
    * fix: modify package name of test_client
    
    * refactor: format some files
    
    * feat: add tracer
    
    * feat: add tracer
    
    * otel migration
    
    * move test client
    
    * update rust version
    
    ---------
    
    Co-authored-by: smorihira <mos.aif.0924@gmail.com>
    Co-authored-by: Kiichiro YUKAWA <kyukawa315@gmail.com>

--- rust/bin/meta/src/main.rs
+++ rust/bin/meta/src/main.rs
@@ -16,15 +16,83 @@
 
 mod handler;
 
+use observability::{
+    config::{Config, Tracer}, observability::{Observability, ObservabilityImpl, SERVICE_NAME}
+};
+use opentelemetry::global;
+use opentelemetry::propagation::Extractor;
+use tonic::transport::Server;
+use tonic::Request;
+
+struct MetadataMap<'a>(&'a tonic::metadata::MetadataMap);
+
+impl<'a> Extractor for MetadataMap<'a> {
+    fn get(&self, key: &str) -> Option<&str> {
+        self.0.get(key).and_then(|metadata| metadata.to_str().ok())
+    }
+
+    fn keys(&self) -> Vec<&str> {
+        self.0
+            .keys()
+            .map(|key| match key {
+                tonic::metadata::KeyRef::Ascii(v) => v.as_str(),
+                tonic::metadata::KeyRef::Binary(v) => v.as_str(),
+            })
+            .collect::<Vec<_>>()
+    }
+}
+
+fn intercept(mut req: Request<()>) -> Result<Request<()>, tonic::Status> {
+    let parent_cx =
+        global::get_text_map_propagator(|prop| prop.extract(&MetadataMap(req.metadata())));
+    req.extensions_mut().insert(parent_cx);
+    Ok(req)
+}
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let addr = "[::1]:8081".parse()?;
-    let meta = handler::Meta::default();
+    // TODO: yaml is given from outside
+    // let config_yaml = r#"enabled: false
+    // endpoint: ""
+    // attributes: []
+    // tracer:
+    //     enabled: false
+    // meter:
+    //     enabled: false
+    //     export_duration:
+    //         secs: 60
+    //         nanos: 0
+    //     export_timeout_duration:
+    //         secs: 30
+    //         nanos: 0
+    // "#;
+    //
+    // decode config yaml
+    // let observability_cfg = serde_yaml::from_str(config_yaml).unwrap();
+    let observability_cfg = Config::new()
+        .enabled(true)
+        .attribute(SERVICE_NAME, "vald-lb-gateway")
+        .attribute("target_pod", "target_pod")
+        .attribute("target_node", "target_node")
+        .attribute("exported_kubernetes_namaspace", "default")
+        .attribute("kubernetes_name", "vald-lb-gateway")
+        .endpoint("http://127.0.0.1:4318")
+        .tracer(Tracer::new().enabled(true));
+    let mut observability = ObservabilityImpl::new(observability_cfg)?;
 
-    tonic::transport::Server::builder()
-        .add_service(proto::meta::v1::meta_server::MetaServer::new(meta))
+    let addr = "[::1]:8095".parse()?;
+    let cfg_path = "/tmp/meta/database"; // TODO: set the appropriate path
+    let meta = handler::Meta::new(cfg_path)?;
+
+    // the interceptor given here is implicitly executed for each request
+    Server::builder()
+        .add_service(proto::meta::v1::meta_server::MetaServer::with_interceptor(
+            meta, intercept,
+        ))
         .serve(addr)
         .await?;
 
+    observability.shutdown()?;
     Ok(())
 }
+
---
+++ rust/bin/meta/src/test_client.rs
@@ -0,0 +1,138 @@
+//
+// Copyright (C) 2019-2025 vdaas.org vald team <vald@vdaas.org>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// You may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+use opentelemetry::global;
+use opentelemetry::propagation::Injector;
+use prost_types::Any;
+use proto::meta::v1::meta_client::MetaClient;
+use proto::payload::v1::meta;
+use tonic::metadata::MetadataMap;
+use tonic::Request;
+struct MetadataInjector<'a>(&'a mut MetadataMap);
+
+impl<'a> Injector for MetadataInjector<'a> {
+    fn set(&mut self, key: &str, value: String) {
+        let key_owned = key.to_owned();
+        let parsed_key = key_owned
+            .parse::<tonic::metadata::MetadataKey<_>>()
+            .unwrap();
+        self.0.insert(parsed_key, value.parse().unwrap());
+    }
+}
+
+fn inject_trace_context<T>(request: &mut Request<T>) {
+    let metadata = request.metadata_mut();
+    let mut injector = MetadataInjector(metadata);
+
+    let current_context = opentelemetry::Context::current();
+
+    global::get_text_map_propagator(|propagator| {
+        propagator.inject_context(&current_context, &mut injector);
+    });
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let mut client = MetaClient::connect("http://[::1]:8095").await?;
+
+    // 1. set key: aaa, value: 1
+    let any_value = Any {
+        type_url: "".to_string(),
+        value: b"1".to_vec(),
+    };
+    let mut request = tonic::Request::new(meta::KeyValue {
+        key: Some(meta::Key {
+            key: "aaa".to_string(),
+        }),
+        value: Some(meta::Value {
+            value: Some(any_value),
+        }),
+    });
+    inject_trace_context(&mut request);
+    client.set(request).await?;
+    println!("Set key: aaa, value: 1");
+
+    // 2. set key: bbb, value: 2
+    let any_value = Any {
+        type_url: "".to_string(),
+        value: b"2".to_vec(),
+    };
+    let mut request = tonic::Request::new(meta::KeyValue {
+        key: Some(meta::Key {
+            key: "bbb".to_string(),
+        }),
+        value: Some(meta::Value {
+            value: Some(any_value),
+        }),
+    });
+    inject_trace_context(&mut request);
+    client.set(request).await?;
+    println!("Set key: bbb, value: 2");
+
+    // 3. get key: bbb
+    let mut request = tonic::Request::new(meta::Key {
+        key: "bbb".to_string(),
+    });
+    inject_trace_context(&mut request);
+    let response = client.get(request).await?;
+    println!("Get key: bbb, RESPONSE={:?}", response.into_inner());
+
+    // 4. set key: bbb, value: 3
+    let any_value = Any {
+        type_url: "".to_string(),
+        value: b"3".to_vec(),
+    };
+    let mut request = tonic::Request::new(meta::KeyValue {
+        key: Some(meta::Key {
+            key: "bbb".to_string(),
+        }),
+        value: Some(meta::Value {
+            value: Some(any_value),
+        }),
+    });
+    inject_trace_context(&mut request);
+    client.set(request).await?;
+    println!("Set key: bbb, value: 3");
+
+    // 5. get key: bbb
+    let mut request = tonic::Request::new(meta::Key {
+        key: "bbb".to_string(),
+    });
+    inject_trace_context(&mut request);
+    let response = client.get(request).await?;
+    println!("Get key: bbb, RESPONSE={:?}", response.into_inner());
+
+    // 6. delete key: aaa
+    let mut request = tonic::Request::new(meta::Key {
+        key: "aaa".to_string(),
+    });
+    inject_trace_context(&mut request);
+    client.delete(request).await?;
+    println!("Deleted key: aaa");
+
+    // 7. get key: aaa (after deletion)
+    let mut request = tonic::Request::new(meta::Key {
+        key: "aaa".to_string(),
+    });
+    inject_trace_context(&mut request);
+    let response = client.get(request).await;
+    match response {
+        Ok(res) => println!("Get key: aaa, RESPONSE={:?}", res.into_inner()),
+        Err(e) => println!("Get key: aaa failed with error: {:?}", e),
+    }
+
+    Ok(())
+}
