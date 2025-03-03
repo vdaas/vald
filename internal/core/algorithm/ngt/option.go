d3289962c11d49b466957e5b8ec0406ac7b40e27
irokaru <karuta@nononotyaya.net>
2025-03-03 13:06:12 +0900

    Update internal/core/algorithm/ngt/option.go
    Co-authored-by: Yusuke Kato <kpango@vdaas.org>
    Signed-off-by: irokaru <karuta@nononotyaya.net>

--- internal/core/algorithm/ngt/option.go
+++ internal/core/algorithm/ngt/option.go
@@ -123,7 +123,7 @@ func WithDistanceTypeByString(dt string) Option {
 		d = Cosine
 	case "poincare", "poinc", "poi", "po", "pc":
 		d = Poincare
-	case "lorentz", "loren", "lor", "lo", "lz":
+	case "lorentz", "lorent", "lorenz", "loren", "lor", "lo", "lz":
 		d = Lorentz
 	case "jaccard", "jac":
 		d = Jaccard
