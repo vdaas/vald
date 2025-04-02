52a4b092b7a74d1a5f6e5e127e0e958416037212
vdaas-ci <57942646+vdaas-ci@users.noreply.github.com>
2025-04-02 14:07:41 +0900

    Backport PR #2897 to release/v1.7 for implement mulit-API for qbg handler (#2906)
    * implement mulit-API for qbg handler (#2897)
    
    * implement mulit-API for qbg handler
    
    * apply coderabbit suggestion
    
    * Update rust/bin/agent/src/main.rs
    
    Signed-off-by: Kiichiro YUKAWA <kyukawa315@gmail.com>
    
    ---------
    
    Signed-off-by: Kiichiro YUKAWA <kyukawa315@gmail.com>
    Co-authored-by: datelier <57349093+datelier@users.noreply.github.com>
    Co-authored-by: Kiichiro YUKAWA <kyukawa315@gmail.com>

--- rust/bin/agent/src/handler/insert.rs
+++ rust/bin/agent/src/handler/insert.rs
@@ -13,8 +13,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 //
-use algorithm::Error;
-use log::{info, warn};
+use algorithm::{Error, MultiError};
+use log::{error, info, warn};
 use prost::Message;
 use proto::{
     payload::v1::{insert, object},
@@ -109,7 +109,7 @@ impl insert_server::Insert for super::Agent {
                             warn!("{:?}", status);
                             status
                         }
-                        Error::UUIDNotFound { id: _ } => {
+                        Error::UUIDNotFound { uuid: _ } => {
                             let mut err_details = ErrorDetails::new();
                             err_details.set_error_info(err.to_string(), domain, metadata);
                             err_details.set_request_info(
@@ -174,8 +174,142 @@ impl insert_server::Insert for super::Agent {
     #[doc = " A method to add new multiple vectors in a single request.\n"]
     async fn multi_insert(
         &self,
-        _request: tonic::Request<insert::MultiRequest>,
+        request: tonic::Request<insert::MultiRequest>,
     ) -> std::result::Result<tonic::Response<object::Locations>, tonic::Status> {
-        todo!()
+        info!("Recieved a request from {:?}", request.remote_addr());
+        let mreq = request.get_ref();
+        let hostname = cargo::util::hostname()?;
+        let domain = hostname.to_str().unwrap();
+        let mut uuids: Vec<String> = Vec::new();
+        let mut vmap = HashMap::new();
+        {
+            let mut s = self.s.write().await;
+            for req in mreq.requests.clone() {
+                let vec = match req.vector.clone() {
+                    Some(v) => v,
+                    None => return Err(Status::invalid_argument("Missing vector in request")),
+                };
+                if vec.vector.len() != s.get_dimension_size() {
+                    let err = Error::IncompatibleDimensionSize {
+                        got: vec.vector.len(),
+                        want: s.get_dimension_size(),
+                    };
+                    let mut err_details = ErrorDetails::new();
+                    let metadata = HashMap::new();
+                    let resource_type = self.resource_type.clone() + "/qbg.MultiInsert";
+                    let resource_name = format!("{}: {}({})", self.api_name, self.name, self.ip);
+                    err_details.set_error_info(err.to_string(), domain, metadata);
+                    err_details.set_request_info(
+                        vec.id,
+                        String::from_utf8(req.encode_to_vec())
+                            .unwrap_or_else(|_| "<invalid UTF-8>".to_string()),
+                    );
+                    err_details.set_bad_request(vec![FieldViolation::new(
+                        "vector dimension size",
+                        err.to_string(),
+                    )]);
+                    err_details.set_resource_info(resource_type, resource_name, "", "");
+                    let status = Status::with_error_details(
+                        Code::InvalidArgument,
+                        "MultiInsert API Incombatible Dimension Size detedted",
+                        err_details,
+                    );
+                    warn!("{:?}", status);
+                    return Err(status);
+                }
+                uuids.push(vec.id.clone());
+                vmap.insert(vec.id, vec.vector);
+            }
+            let result = s.insert_multiple(vmap);
+            match result {
+                Err(err) => {
+                    let metadata = HashMap::new();
+                    let resource_type = self.resource_type.clone() + "/qbg.MultiInsert";
+                    let resource_name = format!("{}: {}({})", self.api_name, self.name, self.ip);
+                    let status = match err {
+                        Error::FlushingIsInProgress {} => {
+                            let mut err_details = ErrorDetails::new();
+                            err_details.set_error_info(err.to_string(), domain, metadata);
+                            err_details.set_request_info(
+                                uuids.join(", "),
+                                String::from_utf8(mreq.encode_to_vec())
+                                    .unwrap_or_else(|_| "<invalid UTF-8>".to_string()),
+                            );
+                            err_details.set_resource_info(resource_type, resource_name, "", "");
+                            let status = Status::with_error_details(Code::Aborted, "MultiInsert API aborted to process insert request due to flushing indices is in progress", err_details);
+                            warn!("{:?}", status);
+                            status
+                        }
+                        Error::UUIDAlreadyExists { ref uuid } => {
+                            let mut err_details = ErrorDetails::new();
+                            err_details.set_error_info(err.to_string(), domain, metadata);
+                            err_details.set_request_info(
+                                uuid,
+                                String::from_utf8(mreq.encode_to_vec())
+                                    .unwrap_or_else(|_| "<invalid UTF-8>".to_string()),
+                            );
+                            err_details.set_resource_info(resource_type, resource_name, "", "");
+                            let uuids = Error::split_uuids(uuid.to_string());
+                            let status = Status::with_error_details(
+                                Code::AlreadyExists,
+                                format!("MultiInsert API uuids {:?} already exists", uuids),
+                                err_details,
+                            );
+                            warn!("{:?}", status);
+                            status
+                        }
+                        Error::UUIDNotFound { uuid: _ } => {
+                            let mut err_details = ErrorDetails::new();
+                            err_details.set_error_info(err.to_string(), domain, metadata);
+                            err_details.set_request_info(
+                                uuids.join(", "),
+                                String::from_utf8(mreq.encode_to_vec())
+                                    .unwrap_or_else(|_| "<invalid UTF-8>".to_string()),
+                            );
+                            err_details.set_bad_request(vec![FieldViolation::new(
+                                "uuid",
+                                err.to_string(),
+                            )]);
+                            err_details.set_resource_info(resource_type, resource_name, "", "");
+                            let status = Status::with_error_details(
+                                Code::InvalidArgument,
+                                format!("MultiInsert API invalid uuids \"{:?}\" detected", uuids),
+                                err_details,
+                            );
+                            warn!("{:?}", status);
+                            status
+                        }
+                        _ => {
+                            let mut err_details = ErrorDetails::new();
+                            err_details.set_error_info(err.to_string(), domain, metadata);
+                            err_details.set_request_info(
+                                uuids.join(", "),
+                                String::from_utf8(mreq.encode_to_vec())
+                                    .unwrap_or_else(|_| "<invalid UTF-8>".to_string()),
+                            );
+                            err_details.set_resource_info(resource_type, resource_name, "", "");
+                            let status = Status::with_error_details(
+                                Code::Internal,
+                                "MultiInsert API failed",
+                                err_details,
+                            );
+                            error!("{:?}", status);
+                            status
+                        }
+                    };
+                    Err(status)
+                }
+                Ok(()) => Ok(tonic::Response::new(object::Locations {
+                    locations: uuids
+                        .iter()
+                        .map(|x| object::Location {
+                            name: self.name.clone(),
+                            uuid: x.to_string(),
+                            ips: vec![self.ip.clone()],
+                        })
+                        .collect(),
+                })),
+            }
+        }
     }
 }
