2de88c12d627bf5a7157efae1c2df3b4a841a704
Matts966 <matts966@vdaas.org>
2025-07-29 16:58:45 +0900

    Use log.Errorf instead of fmt.Fprintf

--- tests/v2/e2e/crud/grpc_test.go
+++ tests/v2/e2e/crud/grpc_test.go
@@ -117,14 +117,14 @@ func handleGRPCWithStatusCode(
 	for _, expect := range plan.Expect {
 		if expect.StatusCode != "" && !expect.StatusCode.Equals(code.String()) {
 			err := fmt.Errorf("unexpected gRPC response received expected: %s, got: %s", expect.StatusCode, code)
-			fmt.Fprintf(os.Stderr, "❌ assert failed, err: %v\n", err)
+			log.Errorf("❌ assert failed, err: %v\n", err)
 			errs = append(errs, err)
 			continue
 		}
 		if expect.Value != nil {
 			val, err := jsonpath.JSONPathEval(protoJSON, expect.Path)
 			if err != nil {
-				fmt.Fprintf(os.Stderr, "❌ assert failed, err: %v\n", err)
+				log.Errorf("❌ assert failed, err: %v\n", err)
 				errs = append(errs, fmt.Errorf("failed to evaluate JSONPath: %s, JSON: %s, err: %s", expect.Path, protoJSON, err))
 				continue
 			}
@@ -168,13 +168,13 @@ func handleGRPCWithStatusCode(
 				errs = append(errs, fmt.Errorf("unsupported operator '%s' for JSONPath %s", expect.Op, expect.Path))
 				continue
 			}
-			fmt.Fprintf(os.Stderr, "✅ assert_%v passed, expected: %v actual: %v\n", expect.Op, expect.Value, val)
+			log.Errorf("✅ assert_%v passed, expected: %v actual: %v\n", expect.Op, expect.Value, val)
 		}
 		return nil
 	}
 
 	err = errors.Join(errs...)
-	fmt.Fprintf(os.Stderr, "❌ assert failed, err: %v\n", err)
+	log.Errorf("❌ assert failed, err: %v\n", err)
 	return err
 }
 
