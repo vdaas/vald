73d94f1592a6d0466a136de4fc69e152ebf7e7ce
Matts966 <matts966@vdaas.org>
2025-08-28 20:14:30 +0900

    Fix envvars

--- hack/docker/gen/main.go
+++ hack/docker/gen/main.go
@@ -422,8 +422,8 @@ var (
 		"RUSTC_WRAPPER": "/usr/bin/sccache",
 	}
 	clangDefaultEnvironments = map[string]string{
-		"CC":  "sccache gcc",
-		"CXX": "sccache g++",
+		"CC":  "\"sccache gcc\"",
+		"CXX": "\"sccache g++\"",
 	}
 	goInstallCommands = []string{
 		"make GOPATH=\"${GOPATH}\" GOROOT=\"${GOROOT}\" GO_VERSION=\"${GO_VERSION}\" go/install",
