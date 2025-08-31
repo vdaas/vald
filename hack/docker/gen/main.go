3bff20f02ac21e3bc955a0f7bc08a2a791d51a2d
Matts966 <matts966@vdaas.org>
2025-08-31 20:54:16 +0900

    Revert "Try to use CARGO_HOME"
    This reverts commit 7546368bc3c6fbd3fded9281783147a07a70b98e.

--- hack/docker/gen/main.go
+++ hack/docker/gen/main.go
@@ -459,8 +459,8 @@ var (
 		"--mount=type=cache,target=/_cache/sccache,sharing=locked,id=sccache-${TARGETARCH}",
 		"--mount=type=cache,target=\"/go/pkg\",id=\"go-pkg-${TARGETARCH}\"",
 		"--mount=type=cache,target=\"/root/.cache/go-build\",id=\"go-build-${TARGETARCH}\"",
-		"--mount=type=cache,target=\"${CARGO_HOME}/registry\",sharing=locked,id=\"cargo-registry-${TARGETARCH}\"",
-		"--mount=type=cache,target=\"${CARGO_HOME}/git\",sharing=locked,id=\"cargo-git-${TARGETARCH}\"",
+		"--mount=type=cache,target=\"/usr/local/lib/rust/cargo/registry\",sharing=locked,id=\"cargo-registry-${TARGETARCH}\"",
+		"--mount=type=cache,target=\"/usr/local/lib/rust/cargo/git\",sharing=locked,id=\"cargo-git-${TARGETARCH}\"",
 	}
 	goDefaultMounts = []string{
 		"--mount=type=tmpfs,target=\"${GOPATH}/src\"",
