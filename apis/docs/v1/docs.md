c6d2b4e209320becf62f577f749cedd12105ef16
jumpei527 <jumpei5278@gmail.com>
2026-01-25 16:09:27 +0900

    Regenerate proto files for vald.v1.Stats service

--- apis/docs/v1/docs.md
+++ apis/docs/v1/docs.md
@@ -161,6 +161,8 @@
   - [Remove](#vald-v1-Remove)
 - [v1/vald/search.proto](#v1_vald_search-proto)
   - [Search](#vald-v1-Search)
+- [v1/vald/stats.proto](#v1_vald_stats-proto)
+  - [Stats](#vald-v1-Stats)
 - [v1/vald/update.proto](#v1_vald_update-proto)
   - [Update](#vald-v1-Update)
 - [v1/vald/upsert.proto](#v1_vald_upsert-proto)
@@ -1702,10 +1704,9 @@ reached.
 
 Represent the resource stats service.
 
-| Method Name         | Request Type                           | Response Type                                                                            | Description                                         |
-| ------------------- | -------------------------------------- | ---------------------------------------------------------------------------------------- | --------------------------------------------------- |
-| ResourceStats       | [.payload.v1.Empty](#payload-v1-Empty) | [.payload.v1.Info.Stats.ResourceStats](#payload-v1-Info-Stats-ResourceStats)             | Represent the RPC to get the resource stats.        |
-| ResourceStatsDetail | [.payload.v1.Empty](#payload-v1-Empty) | [.payload.v1.Info.Stats.ResourceStatsDetail](#payload-v1-Info-Stats-ResourceStatsDetail) | Represent the RPC to get the resource stats detail. |
+| Method Name   | Request Type                           | Response Type                                                                | Description                                  |
+| ------------- | -------------------------------------- | ---------------------------------------------------------------------------- | -------------------------------------------- |
+| ResourceStats | [.payload.v1.Empty](#payload-v1-Empty) | [.payload.v1.Info.Stats.ResourceStats](#payload-v1-Info-Stats-ResourceStats) | Represent the RPC to get the resource stats. |
 
 <a name="v1_vald_filter-proto"></a>
 
@@ -1976,6 +1977,23 @@ Here are some common reasons and how to resolve each error.
 Here are some common reasons and how to resolve each error.
 
 | name | common reason | how to resolve | | :---------------- | :------------------------------------------------------------------------------------------------------------------------------- | :--------------------------------------------------------------------------------------- | | CANCELLED | Executed cancel() of rpc from client/server-side or network problems between client and server. | Check the code, especially around timeout and connection management, and fix if needed. | | INVALID_ARGUMENT | The Requested vector&#39;s ID is empty, or some request payload is invalid. | Check request payload and fix request payload. | | DEADLINE_EXCEEDED | The RPC timeout setting is too short on the client/server side. | Check the gRPC timeout setting on both the client and server sides and fix it if needed. | | NOT_FOUND | The Requested ID is not inserted on the target Vald cluster, or the search result is insufficient to the required result length. | Send a request with another vector or set min_num to a smaller value. | | INTERNAL | Target Vald cluster or network route has some critical error. | Check target Vald cluster first and check network route including ingress as second. | |
+
+<a name="v1_vald_stats-proto"></a>
+
+<p align="right"><a href="#top">Top</a></p>
+
+## v1/vald/stats.proto
+
+<a name="vald-v1-Stats"></a>
+
+### Stats
+
+Overview
+Represent the resource stats service for gateway.
+
+| Method Name         | Request Type                           | Response Type                                                                            | Description                                                           |
+| ------------------- | -------------------------------------- | ---------------------------------------------------------------------------------------- | --------------------------------------------------------------------- |
+| ResourceStatsDetail | [.payload.v1.Empty](#payload-v1-Empty) | [.payload.v1.Info.Stats.ResourceStatsDetail](#payload-v1-Info-Stats-ResourceStatsDetail) | Overview Represent the RPC to get the resource stats for each agents. |
 
 <a name="v1_vald_update-proto"></a>
 
--- rust/libs/proto/src/rpc/v1/rpc.v1.tonic.rs
+++ rust/libs/proto/src/rpc/v1/rpc.v1.tonic.rs
@@ -129,32 +129,6 @@ pub mod stats_client {
                 .insert(GrpcMethod::new("rpc.v1.Stats", "ResourceStats"));
             self.inner.unary(req, path, codec).await
         }
-        pub async fn resource_stats_detail(
-            &mut self,
-            request: impl tonic::IntoRequest<super::super::super::payload::v1::Empty>,
-        ) -> std::result::Result<
-            tonic::Response<
-                super::super::super::payload::v1::info::stats::ResourceStatsDetail,
-            >,
-            tonic::Status,
-        > {
-            self.inner
-                .ready()
-                .await
-                .map_err(|e| {
-                    tonic::Status::unknown(
-                        format!("Service was not ready: {}", e.into()),
-                    )
-                })?;
-            let codec = tonic_prost::ProstCodec::default();
-            let path = http::uri::PathAndQuery::from_static(
-                "/rpc.v1.Stats/ResourceStatsDetail",
-            );
-            let mut req = request.into_request();
-            req.extensions_mut()
-                .insert(GrpcMethod::new("rpc.v1.Stats", "ResourceStatsDetail"));
-            self.inner.unary(req, path, codec).await
-        }
     }
 }
 /// Generated server implementations.
@@ -176,15 +150,6 @@ pub mod stats_server {
         ) -> std::result::Result<
             tonic::Response<
                 super::super::super::payload::v1::info::stats::ResourceStats,
-            >,
-            tonic::Status,
-        >;
-        async fn resource_stats_detail(
-            &self,
-            request: tonic::Request<super::super::super::payload::v1::Empty>,
-        ) -> std::result::Result<
-            tonic::Response<
-                super::super::super::payload::v1::info::stats::ResourceStatsDetail,
             >,
             tonic::Status,
         >;
@@ -298,54 +263,6 @@ pub mod stats_server {
                     let inner = self.inner.clone();
                     let fut = async move {
                         let method = ResourceStatsSvc(inner);
-                        let codec = tonic_prost::ProstCodec::default();
-                        let mut grpc = tonic::server::Grpc::new(codec)
-                            .apply_compression_config(
-                                accept_compression_encodings,
-                                send_compression_encodings,
-                            )
-                            .apply_max_message_size_config(
-                                max_decoding_message_size,
-                                max_encoding_message_size,
-                            );
-                        let res = grpc.unary(method, req).await;
-                        Ok(res)
-                    };
-                    Box::pin(fut)
-                }
-                "/rpc.v1.Stats/ResourceStatsDetail" => {
-                    #[allow(non_camel_case_types)]
-                    struct ResourceStatsDetailSvc<T: Stats>(pub Arc<T>);
-                    impl<
-                        T: Stats,
-                    > tonic::server::UnaryService<
-                        super::super::super::payload::v1::Empty,
-                    > for ResourceStatsDetailSvc<T> {
-                        type Response = super::super::super::payload::v1::info::stats::ResourceStatsDetail;
-                        type Future = BoxFuture<
-                            tonic::Response<Self::Response>,
-                            tonic::Status,
-                        >;
-                        fn call(
-                            &mut self,
-                            request: tonic::Request<
-                                super::super::super::payload::v1::Empty,
-                            >,
-                        ) -> Self::Future {
-                            let inner = Arc::clone(&self.0);
-                            let fut = async move {
-                                <T as Stats>::resource_stats_detail(&inner, request).await
-                            };
-                            Box::pin(fut)
-                        }
-                    }
-                    let accept_compression_encodings = self.accept_compression_encodings;
-                    let send_compression_encodings = self.send_compression_encodings;
-                    let max_decoding_message_size = self.max_decoding_message_size;
-                    let max_encoding_message_size = self.max_encoding_message_size;
-                    let inner = self.inner.clone();
-                    let fut = async move {
-                        let method = ResourceStatsDetailSvc(inner);
                         let codec = tonic_prost::ProstCodec::default();
                         let mut grpc = tonic::server::Grpc::new(codec)
                             .apply_compression_config(
