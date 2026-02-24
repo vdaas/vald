3c0c7d6379763b6fb2efe1fa8147e8fde6585607
Kosuke Morimoto <ksk@vdaas.org>
2026-02-24 22:29:15 +0900

    fix

--- rust/bin/agent/src/lib.rs
+++ rust/bin/agent/src/lib.rs
@@ -13,7 +13,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 //
-#![cfg_attr(test, allow(missing_docs))]
 
 /// Agent configuration module.
 ///
