72c7794e5b37510daea69295fcc26f5f2c5c7311
Matts966 <matts966@vdaas.org>
2026-08-28 14:20:08 +0900

    fix: pin dev container to Ubuntu 24.04
    Signed-off-by: Matts966 <matts966@vdaas.org>

--- hack/docker/gen/main.go
+++ hack/docker/gen/main.go
@@ -88,6 +88,7 @@ const (
 	defaultBuildStageName = "builder"
 	maintainerKey         = "MAINTAINER"
 	minimumArgumentLength = 2
+	ubuntuVersion         = "24.04"
 
 	yearKey = "YEAR"
 
@@ -831,7 +832,7 @@ func main() {
 		vald + "-" + devContainer: {
 			AppName:       devContainer,
 			BuilderImage:  "ubuntu",
-			BuilderTag:    "latest",
+			BuilderTag:    ubuntuVersion,
 			BuildUser:     defaultBuildUser,
 			RuntimeUser:   defaultBuildUser,
 			ContainerType: DevContainer,
